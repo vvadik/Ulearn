d30548f0added35671f3e1aac302b3c116bef967
Sorokin Ivan <iwan954@mail.ru>
2014-07-08 12:50:40 +0600

    ExpectedOutput

---
+++ src/courses/BasicProgramming/ExpectedOutput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace uLearn.Courses.Linq.Slides
+{
+    public class ExpectedOutputAttribute : Attribute
+    {
+        public ExpectedOutputAttribute(string s)
+        {
+            var newOut = new StringWriter();
+            //null or "" ?!
+            Console.SetOut(newOut);
+            Assert.AreEqual(s, newOut.ToString().Trim());
+        }
+    }
+}
