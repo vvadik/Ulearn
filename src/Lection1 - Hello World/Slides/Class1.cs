e09d971be14d2d046606df9a972dd75baf5d4cc0
Sorokin Ivan <iwan954@mail.ru>
2014-07-08 11:08:14 +0600

    HelloWorld added

---
+++ src/Lection1 - Hello World/Slides/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uLearn;
+
+namespace uLearn.Courses.Linq.Slides
+{
+    public class Class1
+    {
+    }
+}
