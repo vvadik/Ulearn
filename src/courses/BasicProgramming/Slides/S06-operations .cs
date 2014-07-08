ae283425399daa23509cf0542ad933f37b0924a3
Sorokin Ivan <iwan954@mail.ru>
2014-07-08 12:32:59 +0600

    ExpectedOutput added

---
+++ src/courses/BasicProgramming/Slides/S06-operations .cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using uLearn;
+
+namespace uLearn.Courses.Linq.Slides
+{
+    public class S06_Operations
+    {
+        /*
+
+        ##Задача: Исправьте программу так, чтобы она давала корректный ответ. ОБъясните, почему программа не работает.
+        */
+
+        [Exercise(SingleStatement = true)]
+        [Hint("var is very dangerous...")]
+        [ExpectedOutput("5.5")]
+        static public void GetSumOfTwoNumbers()
+        {
+            double a = 5;
+            a += 0.5;
+            Console.WriteLine(a);
+            /*uncomment
+            var a = 5;
+            a += 0.5;
+            Console.WriteLine(a)
+            */
+        }
+
+        //[Test]
+        public void Test()
+        {
+            var newOut = new StringWriter();
+            Console.SetOut(newOut);
+            GetSumOfTwoNumbers();
+            Assert.AreEqual("5,5", newOut.ToString().Trim());
+            }
+
+        /*
+        ### Краткая справка
+        */
+    }
+}
---
+++ src/courses/BasicProgramming/Slides/S07-Methods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using uLearn;
+
+namespace uLearn.Courses.Linq.Slides
+{
+    public class S07_Methods
+    {
+        /*
+
+        ##Задача: Исправьте программу так, чтобы она давала корректный ответ. ОБъясните, почему программа не работает.
+        */
+
+        [Exercise(SingleStatement = true)]
+        [Hint("var is very dangerous...")]
+        public double GetSumOfTwoNumbers()
+        {
+            double a = 5;
+            a += 0.5;
+            return a;
+            /*uncomment
+            var a = 5;
+            a += 0.5;
+            Console.WriteLine(a)
+            */
+        }
+
+        [Test]
+        public void Test()
+        {
+            var number = GetSumOfTwoNumbers();
+            Assert.That(
+                5.5,
+                Is.EqualTo(number));
+        }
+
+        /*
+        ### Краткая справка
+        */
+    }
+}
