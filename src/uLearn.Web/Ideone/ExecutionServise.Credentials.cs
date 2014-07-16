a27b6784d2dc082099f50ee1c463a5306d8f7a01
SilentCatVallis <SilentCatVallis@gmail.com>
2014-07-16 10:58:06 +0600

    delete "ServiSe..."

--- src/uLearn.Web/Ideone/ExecutionServise.Credentials.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace uLearn.Web.Ideone
-{
-	public partial class ExecutionService
-	{
-		private const string DefaultName = "xoposhiy";
-		private const string DefaultPassword = "R4dfhl4r";
-	}
-}
