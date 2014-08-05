c9ddba55bf39a832ca0d47b413363827135d3fc9
Sorokin Ivan <iwan954@mail.ru>
2014-08-05 12:13:13 +0600

    Quizzes improvements

---
+++ src/uLearn.Web/Scripts/slide-quiz.js
@@ -0,0 +1,17 @@
+﻿function submitQuiz(courseId, slideId) {
+    $.ajax(
+{
+    type: "POST",
+    url: $("#quizSub").data("url"),
+    data: {
+        courseId: courseId, slideId: slideId, hintId: index
+    }
+}).success(function (ans) {
+            alert("Submitted");
+        })
+    .fail(function (req) {
+        console.log(req.responseText);
+    })
+    .always(function (ans) {
+    });
+}
