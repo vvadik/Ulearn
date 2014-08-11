48b4bd2d0bd7b2ce2a310670186fe238c2f485dc
Sorokin Ivan <iwan954@mail.ru>
2014-08-11 16:29:17 +0600

    Partial quizBlcocks

---
+++ src/uLearn/QuizInfoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using uLearn.Quizes;
+
+namespace uLearn
+{
+	public class QuizInfoModel
+	{
+		public CoursePageModel CourseModel {get; set;}
+		public QuizBlock CurrentBlock { get; set;}
+		public int BlockIndex { get; set; }
+		public bool Passed { get; set; }
+
+		public QuizInfoModel(CoursePageModel model, QuizBlock currentBlock, int index, bool passed)
+		{
+			CourseModel = model;
+			CurrentBlock = currentBlock;
+			BlockIndex = index;
+			Passed = passed;
+		}
+	}
+
+}
