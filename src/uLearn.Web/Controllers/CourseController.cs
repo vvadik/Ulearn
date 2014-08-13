8615afc43f84e80d99b3b002b7182b52bac1435c
Sorokin Ivan <iwan954@mail.ru>
2014-08-13 19:04:52 +0600

    controller for quiz

--- src/uLearn.Web/Controllers/CourseController.cs
+++ src/uLearn.Web/Controllers/CourseController.cs
@@ -54,10 +54,13 @@ namespace uLearn.Web.Controllers
 				CourseId = course.Id,
 				CourseTitle = course.Title,
 				Slide = course.Slides[slideIndex],
-				LatestAcceptedSolution = solutionsRepo.FindLatestAcceptedSolution(courseId, course.Slides[slideIndex].Id, User.Identity.GetUserId()),
+				LatestAcceptedSolution =
+					solutionsRepo.FindLatestAcceptedSolution(courseId, course.Slides[slideIndex].Id, User.Identity.GetUserId()),
 				Rate = GetRate(course.Id, course.Slides[slideIndex].Id),
 				PassedQuiz = userQuizzesRepo.GetIdOfQuizPassedSlides(courseId, User.Identity.GetUserId()),
-				AnswersToQuizes = userQuizzesRepo.GetAnswersForShowOnSlide(courseId, course.Slides[slideIndex] as QuizSlide, User.Identity.GetUserId())
+				AnswersToQuizes =
+					userQuizzesRepo.GetAnswersForShowOnSlide(courseId, course.Slides[slideIndex] as QuizSlide,
+						User.Identity.GetUserId())
 			};
 			return model;
 		}
@@ -91,7 +94,9 @@ namespace uLearn.Web.Controllers
 			var code = GetUserCode(Request.InputStream);
 			var exerciseSlide = courseManager.GetExerciseSlide(courseId, slideIndex);
 			var result = await CheckSolution(exerciseSlide, code, slideIndex);
-			await SaveUserSolution(courseId, exerciseSlide.Id, code, result.CompilationError, result.ActualOutput, result.IsRightAnswer);
+			await
+				SaveUserSolution(courseId, exerciseSlide.Id, code, result.CompilationError, result.ActualOutput,
+					result.IsRightAnswer);
 			return Json(result);
 		}
 
@@ -112,8 +117,10 @@ namespace uLearn.Web.Controllers
 			return Json(new RunSolutionResult
 			{
 				IsRightAnswer = false,
-				ActualOutput = string.Join("\n", Enumerable.Repeat("Тридцать три корабля лавировали лавировали, да не вылавировали", 21)),
-				ExpectedOutput = string.Join("\n", Enumerable.Repeat("Тридцать три корабля лавировали лавировали, да не вылавировали", 15)),
+				ActualOutput =
+					string.Join("\n", Enumerable.Repeat("Тридцать три корабля лавировали лавировали, да не вылавировали", 21)),
+				ExpectedOutput =
+					string.Join("\n", Enumerable.Repeat("Тридцать три корабля лавировали лавировали, да не вылавировали", 15)),
 			});
 		}
 
@@ -131,7 +138,7 @@ namespace uLearn.Web.Controllers
 		public async Task<string> ApplyRate(string courseId, string slideId, string rate)
 		{
 			var userId = User.Identity.GetUserId();
-			var slideRate = (SlideRates)Enum.Parse(typeof(SlideRates), rate);
+			var slideRate = (SlideRates) Enum.Parse(typeof (SlideRates), rate);
 			return await slideRateRepo.AddRate(courseId, slideId, userId, slideRate);
 		}
 
@@ -174,7 +181,7 @@ namespace uLearn.Web.Controllers
 		public async Task<JsonResult> LikeSolution(int solutionId)
 		{
 			var res = await solutionsRepo.Like(solutionId, User.Identity.GetUserId());
-			return Json(new { likesCount = res.Item1, liked = res.Item2 });
+			return Json(new {likesCount = res.Item1, liked = res.Item2});
 		}
 
 		private string NormalizeString(string s)
@@ -216,7 +223,8 @@ namespace uLearn.Web.Controllers
 			};
 		}
 
-		private async Task SaveUserSolution(string courseId, string slideId, string code, string compilationError, string output,
+		private async Task SaveUserSolution(string courseId, string slideId, string code, string compilationError,
+			string output,
 			bool isRightAnswer)
 		{
 			await solutionsRepo.AddUserSolution(
@@ -237,138 +245,5 @@ namespace uLearn.Web.Controllers
 		{
 			await visitersRepo.AddVisiter(courseId, slideId, User.Identity.GetUserId());
 		}
-
-		[HttpPost]
-		[Authorize]
-		public async Task<string> SubmitQuiz(string courseId, string slideIndex, string answer)
-		{
-			var intSlideIndex = int.Parse(slideIndex);
-			var course = courseManager.GetCourse(courseId);
-			if (userQuizzesRepo.IsQuizSlidePassed(courseId, User.Identity.GetUserId(), course.Slides[intSlideIndex].Id))
-				return "already answered";
-			var time = DateTime.Now;
-			var answers = answer.Split('*').Select(x => x.Split('_').Take(2).ToList()).GroupBy(x => x[0]);
-			var quizBlockWithTaskCount = (course.Slides[intSlideIndex] as QuizSlide).Quiz.Blocks.Count(x => x is FillInBlock || x is IsTrueBlock || x is ChoiceBlock);
-			var incorrectQuizzes = new List<string>();
-			var fillInBlockType = typeof (FillInBlock);
-			var tmpFolder = new List<QuizInfoForDb>();
-			foreach (var ans in answers)
-			{
-				var quizInfo = GetQuizInfo(course, intSlideIndex, ans);
-				foreach (var quizInfoForDb in quizInfo)
-				{
-					tmpFolder.Add(quizInfoForDb);
-					if (!quizInfoForDb.IsRightAnswer && quizInfoForDb.QuizType == fillInBlockType)
-						incorrectQuizzes.Add(ans.Key);
-				}
-			}
-			var blocksInAnswerCount = tmpFolder.Select(x => x.QuizId).Distinct().Count();
-			if (blocksInAnswerCount != quizBlockWithTaskCount)
-				return "has empty bloks";
-			foreach (var quizInfoForDb in tmpFolder)
-				await userQuizzesRepo.AddUserQuiz(courseId, quizInfoForDb.IsRightAnswer, quizInfoForDb.ItemId, quizInfoForDb.QuizId,
-					course.Slides[intSlideIndex].Id, quizInfoForDb.Text, User.Identity.GetUserId(), time);
-			return string.Join("*", incorrectQuizzes.Distinct());
-		}
-
-		[HttpPost]
-		public string GetRightAnswersToQuiz(string courseId, int slideIndex)
-		{
-			var course = courseManager.GetCourse(courseId);
-			var quizSlide = course.Slides[slideIndex];
-			var e = (quizSlide as QuizSlide).RightAnswersToQuiz;
-			return e;
-		}
-
-		private IEnumerable<QuizInfoForDb> GetQuizInfo(Course course, int slideIndex, IGrouping<string, List<string>> answer)
-		{
-			var slide = course.Slides[slideIndex] as QuizSlide;
-			if (slide == null)
-				throw new Exception("Error in func 'GetSlideUsingId' or in 'QuizSlideId'");
-			var block = slide.GetBlockById(answer.Key);
-			var data = answer.ToList();
-			if (block is FillInBlock)
-				return CreateQuizInfoForDb(block as FillInBlock, data);
-			if (block is ChoiceBlock)
-				return CreateQuizInfoForDb(block as ChoiceBlock, answer, data);
-			return CreateQuizInfoForDb(block as IsTrueBlock, data);
-		}
-
-		private IEnumerable<QuizInfoForDb> CreateQuizInfoForDb(IsTrueBlock isTrueBlock, IEnumerable<List<string>> data)
-		{
-			return new List<QuizInfoForDb>
-			{
-				new QuizInfoForDb
-				{
-					QuizId = isTrueBlock.Id,
-					ItemId = null,
-					IsRightAnswer = isTrueBlock.Answer.ToString() == data.First()[1],
-					Text = data.First()[1],
-					QuizType = typeof(IsTrueBlock)
-				}
-			};
-		}
-
-		private IEnumerable<QuizInfoForDb> CreateQuizInfoForDb(ChoiceBlock choiseBlock, IEnumerable<List<string>> answer, List<List<string>> data)
-		{
-			if (!choiseBlock.Multiple)
-				return new List<QuizInfoForDb>
-				{
-					new QuizInfoForDb
-					{
-						QuizId = choiseBlock.Id,
-						ItemId = data.First()[1],
-						IsRightAnswer = choiseBlock.Items.First(x => x.Id == data.First()[1]).IsCorrect,
-						Text = null,
-						QuizType = typeof (ChoiceBlock)
-					}
-				};
-			var ans = answer.ToList()
-				.Select(x => new QuizInfoForDb
-				{
-					QuizId = choiseBlock.Id, 
-					IsRightAnswer = false, 
-					ItemId = x[1], 
-					Text = null, 
-					QuizType = typeof(ChoiceBlock)
-					
-				}).ToList();
-			var correctItems = new HashSet<string>(choiseBlock.Items.Where(x => x.IsCorrect).Select(x => x.Id));
-			var ansItem = new HashSet<string>(ans.Select(x => x.ItemId));
-			var count = ansItem.Count(correctItems.Contains);
-			if (count != correctItems.Count || count != ansItem.Count) return ans;
-			foreach (var info in ans)
-				info.IsRightAnswer = true;
-			return ans;
-		}
-
-		private IEnumerable<QuizInfoForDb> CreateQuizInfoForDb(FillInBlock fillInBlock, IEnumerable<List<string>> data)
-		{
-			return new List<QuizInfoForDb>
-			{
-				new QuizInfoForDb
-				{
-					QuizId = fillInBlock.Id,
-					ItemId = null,
-					IsRightAnswer = fillInBlock.Regexes.Any(regex => Regex.IsMatch(data.First()[1], regex)),
-					Text = data.First()[1],
-					QuizType = typeof(FillInBlock)
-				}
-			};
-		}
-
-		public async Task<string> LikeHint(string courseId, string slideId, int hintId)
-		{
-			return await slideHintRepo.LikeHint(courseId, slideId, hintId, User.Identity.GetUserId());
-		}
-	}
-
-	public class QuizInfoForDb
-	{
-		public string QuizId;
-		public string ItemId;
-		public string Text;
-		public bool IsRightAnswer;
-		public Type QuizType;
 	}
 }
---
+++ src/uLearn.Web/Controllers/QuizController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using uLearn.Quizes;
+using uLearn.Web.DataContexts;
+using uLearn.Web.Ideone;
+using uLearn.Web.Models;
+
+namespace uLearn.Web.Controllers
+{
+	public class QuizController : Controller
+	{
+		private readonly CourseManager courseManager;
+		private readonly UserQuizzesRepo userQuizzesRepo = new UserQuizzesRepo();
+
+		public QuizController()
+			: this(CourseManager.AllCourses)
+		{
+		}
+
+		public QuizController(CourseManager courseManager)
+		{
+			this.courseManager = courseManager;
+		}
+
+		[HttpPost]
+		[Authorize]
+		public async Task<string> SubmitQuiz(string courseId, string slideIndex, string answer)
+		{
+			var intSlideIndex = int.Parse(slideIndex);
+			var course = courseManager.GetCourse(courseId);
+			if (userQuizzesRepo.IsQuizSlidePassed(courseId, User.Identity.GetUserId(), course.Slides[intSlideIndex].Id))
+				return "already answered";
+			var time = DateTime.Now;
+			var answers = answer.Split('*').Select(x => x.Split('_').Take(2).ToList()).GroupBy(x => x[0]);
+			var quizBlockWithTaskCount = (course.Slides[intSlideIndex] as QuizSlide).Quiz.Blocks.Count(x => x is FillInBlock || x is IsTrueBlock || x is ChoiceBlock);
+			var incorrectQuizzes = new List<string>();
+			var fillInBlockType = typeof(FillInBlock);
+			var tmpFolder = new List<QuizInfoForDb>();
+			foreach (var ans in answers)
+			{
+				var quizInfo = GetQuizInfo(course, intSlideIndex, ans);
+				foreach (var quizInfoForDb in quizInfo)
+				{
+					tmpFolder.Add(quizInfoForDb);
+					if (!quizInfoForDb.IsRightAnswer && quizInfoForDb.QuizType == fillInBlockType)
+						incorrectQuizzes.Add(ans.Key);
+				}
+			}
+			var blocksInAnswerCount = tmpFolder.Select(x => x.QuizId).Distinct().Count();
+			if (blocksInAnswerCount != quizBlockWithTaskCount)
+				return "has empty bloks";
+			foreach (var quizInfoForDb in tmpFolder)
+				await userQuizzesRepo.AddUserQuiz(courseId, quizInfoForDb.IsRightAnswer, quizInfoForDb.ItemId, quizInfoForDb.QuizId,
+					course.Slides[intSlideIndex].Id, quizInfoForDb.Text, User.Identity.GetUserId(), time);
+			return string.Join("*", incorrectQuizzes.Distinct());
+		}
+
+		[HttpPost]
+		public string GetRightAnswersToQuiz(string courseId, int slideIndex)
+		{
+			var course = courseManager.GetCourse(courseId);
+			var quizSlide = course.Slides[slideIndex];
+			var e = (quizSlide as QuizSlide).RightAnswersToQuiz;
+			return e;
+		}
+
+		private IEnumerable<QuizInfoForDb> GetQuizInfo(Course course, int slideIndex, IGrouping<string, List<string>> answer)
+		{
+			var slide = course.Slides[slideIndex] as QuizSlide;
+			if (slide == null)
+				throw new Exception("Error in func 'GetSlideUsingId' or in 'QuizSlideId'");
+			var block = slide.GetBlockById(answer.Key);
+			var data = answer.ToList();
+			if (block is FillInBlock)
+				return CreateQuizInfoForDb(block as FillInBlock, data);
+			if (block is ChoiceBlock)
+				return CreateQuizInfoForDb(block as ChoiceBlock, answer, data);
+			return CreateQuizInfoForDb(block as IsTrueBlock, data);
+		}
+
+		private IEnumerable<QuizInfoForDb> CreateQuizInfoForDb(IsTrueBlock isTrueBlock, IEnumerable<List<string>> data)
+		{
+			return new List<QuizInfoForDb>
+			{
+				new QuizInfoForDb
+				{
+					QuizId = isTrueBlock.Id,
+					ItemId = null,
+					IsRightAnswer = isTrueBlock.Answer.ToString() == data.First()[1],
+					Text = data.First()[1],
+					QuizType = typeof(IsTrueBlock)
+				}
+			};
+		}
+
+		private IEnumerable<QuizInfoForDb> CreateQuizInfoForDb(ChoiceBlock choiseBlock, IEnumerable<List<string>> answer, List<List<string>> data)
+		{
+			if (!choiseBlock.Multiple)
+				return new List<QuizInfoForDb>
+				{
+					new QuizInfoForDb
+					{
+						QuizId = choiseBlock.Id,
+						ItemId = data.First()[1],
+						IsRightAnswer = choiseBlock.Items.First(x => x.Id == data.First()[1]).IsCorrect,
+						Text = null,
+						QuizType = typeof (ChoiceBlock)
+					}
+				};
+			var ans = answer.ToList()
+				.Select(x => new QuizInfoForDb
+				{
+					QuizId = choiseBlock.Id,
+					IsRightAnswer = false,
+					ItemId = x[1],
+					Text = null,
+					QuizType = typeof(ChoiceBlock)
+
+				}).ToList();
+			var correctItems = new HashSet<string>(choiseBlock.Items.Where(x => x.IsCorrect).Select(x => x.Id));
+			var ansItem = new HashSet<string>(ans.Select(x => x.ItemId));
+			var count = ansItem.Count(correctItems.Contains);
+			if (count != correctItems.Count || count != ansItem.Count) return ans;
+			foreach (var info in ans)
+				info.IsRightAnswer = true;
+			return ans;
+		}
+
+		private IEnumerable<QuizInfoForDb> CreateQuizInfoForDb(FillInBlock fillInBlock, IEnumerable<List<string>> data)
+		{
+			return new List<QuizInfoForDb>
+			{
+				new QuizInfoForDb
+				{
+					QuizId = fillInBlock.Id,
+					ItemId = null,
+					IsRightAnswer = fillInBlock.Regexes.Any(regex => Regex.IsMatch(data.First()[1], regex)),
+					Text = data.First()[1],
+					QuizType = typeof(FillInBlock)
+				}
+			};
+		}
+	}
+
+	public class QuizInfoForDb
+	{
+		public string QuizId;
+		public string ItemId;
+		public string Text;
+		public bool IsRightAnswer;
+		public Type QuizType;
+	}
+}
