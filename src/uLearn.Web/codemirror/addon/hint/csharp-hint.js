dc3bba52bc45743c504f3e39aa11bc4a8c4da81f
Denisov Sasha <daa@skbkontur.ru>
2014-08-18 16:32:13 +0600

    new clever autocompletion

--- src/uLearn.Web/codemirror/addon/hint/csharp-hint.js
+++ src/uLearn.Web/codemirror/addon/hint/csharp-hint.js
@@ -28,17 +28,55 @@
 	function csharpHint(editor) {
 		var cur = editor.getCursor(), token = editor.getTokenAt(cur), tprop = token;
 		var from = CodeMirror.Pos(cur.line, token.start);
+		var tmpLen = 0;
+		var totalLen = 0;
 		var completionList;
 		if (token.string === ".") {
+			var lastToken = "";
 			from = CodeMirror.Pos(cur.line, token.start + 1);
-			completionList = getCompletions("", true);
+			cur.ch = cur.ch - 1;
+			lastToken = editor.getTokenAt(cur);
+			if (lastToken.string == ')') {
+				totalLen = 0;
+				while (lastToken.string != '(') {
+					cur.ch = cur.ch - lastToken.string.length;
+					totalLen += lastToken.string.length;
+					lastToken = editor.getTokenAt(cur);
+				}
+				cur.ch = cur.ch - 1;
+				lastToken = editor.getTokenAt(cur);
+				cur.ch = cur.ch + totalLen + 1;
+			}
+			cur.ch = cur.ch + 1;
+			completionList = getCompletions("", lastToken.string, true);
 		}
 		else if (!/^[\w@_]*$/.test(token.string)) {
 			from = cur;
-			completionList = getCompletions("", false);
+			completionList = getCompletions("", "", false);
+		} else {
+			var beforeDot = "";
+			cur.ch = cur.ch - token.string.length;
+			var lastDot = editor.getTokenAt(cur);
+			if (lastDot.string == '.') {
+				cur.ch = cur.ch - 1;
+				beforeDot = editor.getTokenAt(cur);
+				if (beforeDot.string == ')') {
+					totalLen = 0;
+					while (beforeDot.string != '(') {
+						cur.ch = cur.ch - beforeDot.string.length;
+						totalLen += beforeDot.string.length;
+						beforeDot = editor.getTokenAt(cur);
+					}
+					cur.ch = cur.ch - 1;
+					beforeDot = editor.getTokenAt(cur);
+					cur.ch = cur.ch + totalLen + 1;
+				}
+				cur.ch = cur.ch + 1;
+			}
+			cur.ch = cur.ch + token.string.length;
+
+			completionList = getCompletions(beforeDot.string, token.string, false);
 		}
-		else
-			completionList = getCompletions(token.string, false);
 
 		completionList = completionList.sort();
 
@@ -55,23 +93,109 @@
 		+ " Where Select SelectMany ToArray ToList ToDictionary ToLookup Join Zip Aggregate GroupBy OrderBy OrderByDescending ThenBy ThenByDescending"
 		+ " "
 		).split(" ");
-	var types = ("Console CultureInfo Enumerable Math Int Double "
+	var types = ("Console CultureInfo Enumerable Math int double string "
 		+ " "
 		).split(" ");
 
-	function getCompletions(start, afterDot) {
+	var dictWithMethods = [];
+	dictWithMethods['int'] = ['CompareTo', 'Equals', 'GetHashCode', 'ToString', 'Parse', 'TryParse', 'GetTypeCode', 'GetType'];
+	dictWithMethods['string'] = ['Join', 'Equals', 'CopyTo', 'ToCharArray', 'IsNullOrEmpty', 'IsNullOrWhiteSpace', 'GetHashCode', 'Split', 'Substring', 'Trim', 'TrimStart', 'TrimEnd', 'IsNormalized', 'Normalize', 'Compare', 'CompareTo', 'CompareOrdinal', 'Contains', 'EndsWith', 'IndexOf', 'IndexOfAny', 'LastIndexOf', 'LastIndexOfAny', 'PadLeft', 'PadRight', 'StartsWith', 'ToLower', 'ToLowerInvariant', 'ToUpper', 'ToUpperInvariant', 'ToString', 'Clone', 'Insert', 'Replace', 'Remove', 'Format', 'Copy', 'Concat', 'Intern', 'IsInterned', 'GetTypeCode', 'GetEnumerator', 'GetType'];
+	dictWithMethods['double'] = ['IsInfinity', 'IsPositiveInfinity', 'IsNegativeInfinity', 'IsNaN', 'CompareTo', 'Equals', 'GetHashCode', 'ToString', 'Parse', 'TryParse', 'GetTypeCode', 'GetType'];
+	dictWithMethods['Console'] = ['Beep', 'Clear', 'ResetColor', 'MoveBufferArea', 'SetBufferSize', 'SetWindowSize', 'SetWindowPosition', 'SetCursorPosition', 'ReadKey', 'OpenStandardError', 'OpenStandardInput', 'OpenStandardOutput', 'SetIn', 'SetOut', 'SetError', 'Read', 'ReadLine', 'WriteLine', 'Write', 'ToString', 'Equals', 'GetHashCode', 'GetType'];
+	dictWithMethods['Math'] = ['Acos', 'Asin', 'Atan', 'Atan2', 'Ceiling', 'Cos', 'Cosh', 'Floor', 'Sin', 'Tan', 'Sinh', 'Tanh', 'Round', 'Truncate', 'Sqrt', 'Log', 'Log10', 'Exp', 'Pow', 'IEEERemainder', 'Abs', 'Max', 'Min', 'Sign', 'BigMul', 'DivRem', 'ToString', 'Equals', 'GetHashCode', 'GetType'];
+
+	var dictWithProperties = [];
+	dictWithProperties['string'] = ['Chars', 'Length'];
+	dictWithProperties['Console'] = ['IsInputRedirected', 'IsOutputRedirected', 'IsErrorRedirected', 'In', 'Out', 'Error', 'InputEncoding', 'OutputEncoding', 'BackgroundColor', 'ForegroundColor', 'BufferHeight', 'BufferWidth', 'WindowHeight', 'WindowWidth', 'LargestWindowWidth', 'LargestWindowHeight', 'WindowLeft', 'WindowTop', 'CursorLeft', 'CursorTop', 'CursorSize', 'CursorVisible', 'Title', 'KeyAvailable', 'NumberLock', 'CapsLock', 'TreatControlCAsInput'];
+
+	var dictWithConstants = [];
+	dictWithConstants['int'] = ['MaxValue', 'MinValue'];
+	dictWithConstants['string'] = ['Empty'];
+	dictWithConstants['double'] = ['MinValue', 'MaxValue', 'Epsilon', 'NegativeInfinity', 'PositiveInfinity', 'NaN'];
+	dictWithConstants['Math'] = ['PI', 'E'];
+
+	var returnTypeDict = [];
+	returnTypeDict['int'] = ['CompareTo', 'GetHashCode', 'Parse', 'Compare', 'CompareOrdinal', 'IndexOf', 'IndexOfAny', 'LastIndexOf', 'LastIndexOfAny', 'Read', 'Abs', 'Max', 'Min', 'Sign', 'DivRem', 'Length', 'BufferHeight', 'BufferWidth', 'WindowHeight', 'WindowWidth', 'LargestWindowWidth', 'LargestWindowHeight', 'WindowLeft', 'WindowTop', 'CursorLeft', 'CursorTop', 'CursorSize', 'MaxValue', 'MinValue'];
+	returnTypeDict['string'] = ['ToString', 'Join', 'Substring', 'Trim', 'TrimStart', 'TrimEnd', 'Normalize', 'PadLeft', 'PadRight', 'ToLower', 'ToLowerInvariant', 'ToUpper', 'ToUpperInvariant', 'Insert', 'Replace', 'Remove', 'Format', 'Copy', 'Concat', 'Intern', 'IsInterned', 'ReadLine', 'Title', 'Empty'];
+	returnTypeDict['double'] = ['Parse', 'Acos', 'Asin', 'Atan', 'Atan2', 'Ceiling', 'Cos', 'Cosh', 'Floor', 'Sin', 'Tan', 'Sinh', 'Tanh', 'Round', 'Truncate', 'Sqrt', 'Log', 'Log10', 'Exp', 'Pow', 'IEEERemainder', 'Abs', 'Max', 'Min', 'MinValue', 'MaxValue', 'Epsilon', 'NegativeInfinity', 'PositiveInfinity', 'NaN', 'PI', 'E'];
+
+	function getCompletions(beforeDot, start, afterDot) {
 		var found = [];
 		console.log("|" + start + "|");
 		function maybeAdd(str) {
 			if (str.lastIndexOf(start, 0) == 0 && !arrayContains(found, str)) found.push(str);
 		}
+		var isWasFound = false;
 		if (afterDot) {
-			forEach(methods, maybeAdd);
-		}
-		else {
-			forEach(types, maybeAdd);
-			forEach(methods, maybeAdd);
-			forEach(keywords, maybeAdd);
+			var wordBeforeDot = start;
+			start = "";
+			if (dictWithConstants[wordBeforeDot] != undefined || dictWithMethods[wordBeforeDot] != undefined || dictWithProperties[wordBeforeDot] != undefined) {
+				if (dictWithConstants[wordBeforeDot] != undefined)
+					forEach(dictWithConstants[wordBeforeDot], maybeAdd);
+				if (dictWithMethods[wordBeforeDot] != undefined)
+					forEach(dictWithMethods[wordBeforeDot], maybeAdd);
+				if (dictWithProperties[wordBeforeDot] != undefined)
+					forEach(dictWithProperties[wordBeforeDot], maybeAdd);
+			} else {
+				isWasFound = false;
+				for (var type in returnTypeDict) {
+					for (var method in returnTypeDict[type]) {
+						if (wordBeforeDot == returnTypeDict[type][method]) {
+							if (dictWithMethods[type] != undefined)
+								forEach(dictWithMethods[type], maybeAdd);
+							if (dictWithProperties[type] != undefined)
+								forEach(dictWithProperties[type], maybeAdd);
+							if (dictWithConstants[type] != undefined)
+								forEach(dictWithConstants[type], maybeAdd);
+							isWasFound = true;
+							break;
+						}
+					}
+					if (isWasFound)
+						break;
+				}
+				if (!isWasFound) {
+					forEach(methods, maybeAdd);
+				}
+			}
+		} else {
+			if (beforeDot != undefined) {
+				if (dictWithConstants[beforeDot] != undefined || dictWithMethods[beforeDot] != undefined || dictWithProperties[beforeDot] != undefined) {
+					if (dictWithConstants[beforeDot] != undefined)
+						forEach(dictWithConstants[beforeDot], maybeAdd);
+					if (dictWithMethods[beforeDot] != undefined)
+						forEach(dictWithMethods[beforeDot], maybeAdd);
+					if (dictWithProperties[beforeDot] != undefined)
+						forEach(dictWithProperties[beforeDot], maybeAdd);
+				} else {
+					isWasFound = false;
+					for (var type2 in returnTypeDict) {
+						for (var method2 in returnTypeDict[type2]) {
+							if (beforeDot == returnTypeDict[type2][method2]) {
+								if (dictWithMethods[type2] != undefined)
+									forEach(dictWithMethods[type2], maybeAdd);
+								if (dictWithProperties[type2] != undefined)
+									forEach(dictWithProperties[type2], maybeAdd);
+								if (dictWithConstants[type2] != undefined)
+									forEach(dictWithConstants[type2], maybeAdd);
+								isWasFound = true;
+								break;
+							}
+						}
+						if (isWasFound)
+							break;
+					}
+					if (!isWasFound) {
+						forEach(types, maybeAdd);
+						forEach(methods, maybeAdd);
+						forEach(keywords, maybeAdd);
+					}
+				}
+			} else {
+				forEach(types, maybeAdd);
+				forEach(methods, maybeAdd);
+				forEach(keywords, maybeAdd);
+			}
 		}
 		return found;
 	}
---
+++ src/uLearn/tests/CreateDictForAutocomplit.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace uLearn.tests
+{
+	class CreateDictForAutocomplit
+	{
+		public static Dictionary<Type, List<string>> ReturnTypeDictionary = new Dictionary<Type, List<string>>();
+
+		public static int TotalWordCount = 0;
+
+		[Explicit]
+		[Test]
+		public static void CreateDict()
+		{
+			var myTypes = new[]
+			{
+				typeof (int),
+				typeof (string),
+				typeof (double),
+				typeof (Console),
+				typeof (Math)
+			};
+
+			WalkThroughTypes(myTypes);
+		}
+
+		private static void WalkThroughTypes(Type[] myTypes)
+		{
+			const string methodDict = "dictWithMethods";
+			VisitTypesElements(methodDict, GetMethods, myTypes);
+
+			const string propertiesDict = "dictWithProperties";
+			VisitTypesElements(propertiesDict, GetProperties, myTypes);
+
+			const string constantDict = "dictWithConstants";
+			VisitTypesElements(constantDict, GetConstants, myTypes);
+			
+			PrintReturnTypeDictionary(myTypes);
+			Console.WriteLine("Total word count in all dictionary: {0}", TotalWordCount);
+		}
+
+		private static void PrintReturnTypeDictionary(IEnumerable<Type> myTypes)
+		{
+			Console.WriteLine("var returnTypeDict = [];\n");
+			foreach (
+				var type in
+					ReturnTypeDictionary.Keys.Where(
+						type => !type.ToString().Contains("TSource") && !type.ToString().Contains("TResult") && myTypes.Contains(type)))
+			{
+				Console.WriteLine("returnTypeDict['{0}'] = [{1}];", ToPrettyString(type),
+					ToDictString(ReturnTypeDictionary[type].Distinct()));
+				Console.WriteLine();
+				TotalWordCount += ReturnTypeDictionary[type].Distinct().Count();
+			}
+		}
+
+		private static void VisitTypesElements(string dictName, Func<Type, IEnumerable<string>> func, IEnumerable<Type> myTypes)
+		{
+			Console.WriteLine("var {0} = [];", dictName);
+			Console.WriteLine();
+			foreach (var myType in myTypes)
+			{
+				var collection = func(myType).ToList();
+				TotalWordCount += collection.Count();
+				if (!collection.Any())
+					continue;
+				Console.WriteLine("{0}['{1}'] = [{2}];", dictName, ToPrettyString(myType), ToDictString(collection));
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+		}
+
+		private static string ToPrettyString(Type myType)
+		{
+			var type = myType.ToString().Replace("System.", "");
+			if (type == "Int32")
+				return "int";
+			if (type == "String")
+				return "string";
+			if (type == "Double")
+				return "double";
+			return type;
+		}
+
+		private static string ToDictString(IEnumerable<string> collection)
+		{
+			return "'" + string.Join("', '", collection) + "'";
+		}
+
+		private static void WriteMyCollection(IEnumerable<KeyValuePair<Type, IEnumerable<string>>> myTypesWithMethods)
+		{
+			foreach (var typeWithMethod in myTypesWithMethods)
+			{
+				Console.WriteLine("методы у {0}: \n", typeWithMethod.Key);
+				foreach (var method in typeWithMethod.Value)
+					Console.WriteLine(method);
+				Console.WriteLine();
+			}
+		}
+
+		private static IEnumerable<string> GetMethods(Type myType)
+		{
+			var methods = myType.GetMethods().Where(y => y.Name[0] != y.Name.ToLower()[0]).ToArray();
+			foreach (var methodInfo in methods)
+			{
+				var type = methodInfo.ReturnType;
+				if (!ReturnTypeDictionary.ContainsKey(type))
+					ReturnTypeDictionary[type] = new List<string>();
+				ReturnTypeDictionary[type].Add(methodInfo.Name);
+			}
+			return methods.Select(x => x.Name).Distinct();
+		}
+
+		private static IEnumerable<string> GetProperties(Type myType)
+		{
+			var properties = myType.GetProperties().Where(y => y.Name[0] != y.Name.ToLower()[0]).ToArray();
+			foreach (var propertyInfo in properties)
+			{
+				var type = propertyInfo.PropertyType;
+				if (!ReturnTypeDictionary.ContainsKey(type))
+					ReturnTypeDictionary[type] = new List<string>();
+				ReturnTypeDictionary[type].Add(propertyInfo.Name);
+			}
+			return properties.Select(x => x.Name).Distinct();
+		}
+
+		private static IEnumerable<string> GetConstants(Type myType)
+		{
+			var fields = myType.GetFields().Where(y => y.Name[0] != y.Name.ToLower()[0]).ToArray();
+			foreach (var fieldInfo in fields)
+			{
+				var type = fieldInfo.FieldType;
+				if (!ReturnTypeDictionary.ContainsKey(type))
+					ReturnTypeDictionary[type] = new List<string>();
+				ReturnTypeDictionary[type].Add(fieldInfo.Name);
+			}
+			return fields.Select(x => x.Name).Distinct();
+		}
+	}
+}
