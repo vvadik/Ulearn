ddad33a0a2be81b63ecef1b91a208547b68663da
Denisov Sasha <daa@skbkontur.ru>
2014-08-21 15:51:17 +0600

    revert ^^

--- src/courses/BasicProgramming/Slides/U03_Cycles/S023_QueenMove.cs
+++ src/courses/BasicProgramming/Slides/U03_Cycles/S023_QueenMove.cs
@@ -5,36 +5,51 @@ using uLearn.CSharp;
 namespace uLearn.Courses.BasicProgramming.Slides
 {
 	[Slide("Ход ферзя", "{EB11DFB6-5629-4D13-819B-6411B9F93DF6}")]
-	class S023_QueenMove
+	internal class S023_QueenMove
 	{
 		/*
-		Вася решил научиться играть в шахматы. Сейчас он разучивает ферзя. Он решает такую задачу:
-		Ему на вход передаются две координаты шахматного поля. Вася должен ответить, является ли такой ход ферзя корректным.
-		Координаты передаются в строковом формате, например, "a1".
+		Вася решил научить своего младшего брата играть в шахматы. Но вот беда, брат никак не может запомнить как ходит ферзь.
+		Как настоящий программист Вася решил автоматизировать ручной труд по обучению начинающих шахматистов.
+
+		Помогите ему написать программу, которая по шахматному ходу определяет корректный ли это ход ферзя.
+		Координаты исходной и конечной позиции хода передаются строкой в обычной шахматной нотации, например, "a1".
 		*/
 
 		[Hint("Найдите с помощью автокомплита функции у строк, позволяющие взять первый и последний символ")]
 		[Hint("Функция взятия модуля числа находится в классе Math и называется Abs")]
-		[Hint(@"Воспользуйтесь знанием, что символы латинских букв в кодировке Unicod стоят в алфавитном порядке, а
+		[Hint(@"Воспользуйтесь знанием, что символы латинских букв в кодировке Unicode стоят в алфавитном порядке, а
  результат вычитания одного char из другого char - разница между их кодами в кодировке")]
-		[ExpectedOutput("True\nFalse\nTrue\nTrue\nFalse\nTrue\nFalse")]
+		[ExpectedOutput(@"
+a1-d4 True
+f4-e7 False
+a1-a4 True
+c4-d4 True
+a1-a1 False
+b7-h1 True
+e3-g7 False
+")]
 		public static void Main()
 		{
-			Console.WriteLine(IsRightStep("a1", "d4"));
-			Console.WriteLine(IsRightStep("f4", "e7"));
-			Console.WriteLine(IsRightStep("a1", "a4"));
-			Console.WriteLine(IsRightStep("c4", "d4"));
-			Console.WriteLine(IsRightStep("a1", "a1"));
-			Console.WriteLine(IsRightStep("b7", "h1"));
-			Console.WriteLine(IsRightStep("e3", "g7"));
+			Run("a1", "d4");
+			Run("f4", "e7");
+			Run("a1", "a4");
+			Run("c4", "d4");
+			Run("a1", "a1");
+			Run("b7", "h1");
+			Run("e3", "g7");
+		}
+
+		public static void Run(string from, string to)
+		{
+			Console.WriteLine("{0}-{1} {2}", from, to, IsRightStep(from, to));
 		}
 
 		[Exercise]
-		private static bool IsRightStep(string lastPosition, string newPosition)
+		private static bool IsRightStep(string from, string to)
 		{
-			return (lastPosition.First() - newPosition.First() == 0 || lastPosition.Last() - newPosition.Last() == 0 ||
-			         (Math.Abs(lastPosition.First() - newPosition.First())) == Math.Abs(lastPosition.Last() - newPosition.Last())) &&
-			        !(lastPosition.First() - newPosition.First() == 0 && lastPosition.Last() - newPosition.Last() == 0);
+			var dx = Math.Abs(to[0] - from[0]);
+			var dy = Math.Abs(to[1] - from[1]);
+			return dx + dy != 0 && (dx == dy || dx*dy == 0);
 			/*uncomment
 			...
 			*/
