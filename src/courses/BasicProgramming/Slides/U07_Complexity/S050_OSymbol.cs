809d0d781be5d3bec5ade103e708fef7cb05ce39
Pavel Egorov <xoposhiy@mail.ru>
2014-10-20 11:52:47 +0600

    Fix small errors in complexity

--- src/courses/BasicProgramming/Slides/U07_Complexity/S050_OSymbol.cs
+++ src/courses/BasicProgramming/Slides/U07_Complexity/S050_OSymbol.cs
@@ -18,9 +18,13 @@
 		###O-большое
 	
 		$$f(n)=O(g(n)) \Leftrightarrow$$
-		$$\exists k>0\ \exists n_0\ \forall n>n_0:\ f(n)<k\cdot g(n) \Leftrightarrow $$
+		$$\exists k>0\ \exists n_0\ \forall n>n_0:\ f(n)<k\cdot g(n) \Leftarrow $$
 		$$\lim_{n\rightarrow \infty} \frac{f(n)}{g(n)}<\infty $$
 		$$f(n)\preceq g(n) $$
+
+		Предел может не существовать, поэтому импликация только в одну сторону. 
+
+		__На видео, импликация ошибочно показана в обе стороны.__
 
 		###Θ-большое
 	
@@ -28,6 +32,8 @@
 		$$\exists k_1,k_2>0\ \exists n_{0}\ \forall n>n_{0}:\ k_1\cdot g(n)<f(n)<k_2 \cdot g(n) \Leftarrow $$
 		$$\Leftarrow \lim_{n\rightarrow \infty} \frac{f(n)}{g(n)}=c>0 $$
 		$$f(n) \approx g(n) $$
+
+		Предел может не существовать, поэтому импликация только в одну сторону. 
 		*/
 	}
 }
