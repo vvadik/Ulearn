9c6bfbaf3dae0a678e83ff2f33d6cb9431095f80
Yury Pliner <yury.pliner@gmail.com>
2014-10-25 13:52:00 +0600

    Слайд для ацкий автокомплит

--- src/courses/OOP/Slides/U03_DataStructure/S060_LimitedAutocomplete.cs
+++
@@ -1,9 +0,0 @@
-﻿using uLearn;
-
-namespace OOP.Slides.U03_DataStructure
-{
-    [Slide("Задача. Автодополнение", "{2315E50E-3EA0-4943-8113-E6025325EFA0}")]
-    public class S060_LimitedAutocomplete
-    {
-    }
-}
