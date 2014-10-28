8c1aa5955b9d6e9c8ad582cf4c5e315cbf11d668
Pavel Egorov <xoposhiy@mail.ru>
2014-10-28 11:55:12 +0500

    Fix first slides to Java

--- src/courses/OOP/Slides/U03_DataStructure/S050_Generics.cs
+++ src/courses/OOP/Slides/U03_DataStructure/S050_Generics.cs
@@ -7,55 +7,30 @@ namespace OOP.Slides.U03_DataStructure
     public class S050_Generics
     {
         /*
-        Решая предыдущие задачи(ассоциативный массив, куча) можно было заметить, что для данных структур данных не важно, 
+        Решая две предыдущие задачи можно было заметить, что для данных структур данных не важно, 
         над какими типами они работают. Ассоциативный массив, в котором ключами и значениями являются строки, ничем не отличается от ассоциативного массива,
-        в котором ключами и значениями являются целые числа. Соответственно, не хочется иметь отдельные реализации под каждую пару типов ключ-значение, 
-        а иметь одную общую реализацию ассоциативного массива, для которого можно задавать необходимые типы для ключа и значения.
+        в котором ключами и значениями являются целые числа. Вместо отдельной реализации под каждую пару типов ключ-значение, 
+        желательно иметь одну общую реализацию ассоциативного массива, для которого можно задавать необходимые типы для ключа и значения.
         
-        Данную проблему решает техника, которая называется обобщенным программированием(https://en.wikipedia.org/wiki/Generic_programming).
-        Эта техника позволяет описать ассоциативный массив следующим образом(здесь TKey - тип ключа, а TValue - тип значения):
+        Для решения этой проблемы в языках программирования есть так называемые обобщённые типы (Generic types).
+        Они позволяют описать ассоциативный массив следующим образом (здесь TKey — тип ключа, а TValue — тип значения):
         */
 
-        public interface IAssociativeArray<TKey, TValue>
+        public interface SimpleMap<TKey, TValue>
         {
-            TValue Find(TKey key);
-            TValue Insert(TKey key, TValue value);
-            TValue Remove(TKey key);
+            TValue get(TKey key);
+            TValue put(TKey key, TValue value);
+            TValue remove(TKey key);
         }
 
-        public class AssociativeArray<TKey, TValue> : IAssociativeArray<TKey, TValue>
-        {
-            public TValue Find(TKey key)
-            {
-                return default(TValue);
-            }
-
-            public TValue Insert(TKey key, TValue value)
-            {
-                return default(TValue);
-            }
-
-            public TValue Remove(TKey key)
-            {
-                return default(TValue);
-            }
-        }
-
-        public class AssociativeArrayUsage
-        {
-            public static void Main(string[] args)
-            {
-                IAssociativeArray<string, int> wordsCount = new AssociativeArray<string, int>();
-                wordsCount.Insert("lalala", 1);
-                wordsCount.Insert("bububu", 2);
-            }
-        }
-        /*
-        В данном примере TKey и TValue называются параметризуемыми типами, IAssociativeArray<TKey, TValue> - обобщенным типом.
+		/*
+        В данном примере TKey и TValue называются типами-параметрами, а SimpleMap<TKey, TValue> — обобщенным типом.
         
-        При реализации обобщенных типов часто возникает необходимость наложения ограничений на параметризуемые типы.
+        При реализации обобщенных типов часто возникает необходимость наложения ограничений на типы-параметры.
         Например, при реализации ассоциативного массива с помощью самобалансирующегося дерева поиска, необходимо 
-        чтобы экземпляры TKey был упорядочиваемыми. Одна из возможностей сделать в С# тип упорядочиваемым - реализовать в типе интерфейс
+        чтобы экземпляры TKey был упорядочиваемыми. 
+		
+		Одна из возможностей сделать в С# тип упорядочиваемым — реализовать в типе интерфейс
         IComparable(http://msdn.microsoft.com/ru-ru/library/system.icomparable.aspx). 
         Значит, необходимо наложить на TKey следующее ограничение: он должен реализовывать интерфейс IComparable<TKey>:
         */
