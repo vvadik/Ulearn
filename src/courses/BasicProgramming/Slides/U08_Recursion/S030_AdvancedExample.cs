2815ce1cc3cf39d011c01366dc6b3a12ced0c23c
Pavel Egorov <xoposhiy@mail.ru>
2014-11-03 00:33:08 +0500

    remove trash

--- src/courses/BasicProgramming/Slides/U08_Recursion/S030_AdvancedExample.cs
+++ src/courses/BasicProgramming/Slides/U08_Recursion/S030_AdvancedExample.cs
@@ -21,28 +21,6 @@ namespace uLearn.Courses.BasicProgramming.Slides.U08_Recursion
 			return Make(x - 1, y) + Make(x, y - 1);
 		}
 
-		static int F(int x, int y)
-		{
-			if (y == 0)
-				return x;
-			else
-				return F(y, x % y);
-		}
-
-
-		[TestCase(1, Result=1)]
-		[TestCase(0, Result=0)]
-		[TestCase(10, Result=0)]
-		[TestCase(101, Result = 1)]
-		[TestCase(10101, Result = 1)]
-		[TestCase(10121, Result = 3)]
-		[TestCase(121, Result = 3)]
-		public static int F(int x)
-		{
-			if (x % 10 == 0) return 0;
-			return 1 + F(x / 10);
-		}
-
 		/*
 		![Карта памяти](_30_map.png)
 
