d35a4a04fc9a3e17436d4915b60e2112fc00752a
DenisovAlexander <vallis@olympus.ru>
2014-11-08 23:57:23 +0500

    wrong_commit

---
+++ src/Selenium/SELENIUM_TESTS_1/DB_test_code.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Data.Entity;
+//using Microsoft.AspNet.Identity.EntityFramework;
+using NUnit.Framework;
+using uLearn.Web.DataContexts;
+using uLearn.Web.Migrations;
+using uLearn.Web.Models;
+
+namespace Selenium.SELENIUM_TESTS_1
+{
+	[TestFixture]
+	class DB_test_code
+	{
+		[Test]
+		public void TestCreateDB()
+		{
+			var db = new uLearn.Web.DataContexts.ULearnDb();// ULearnDb();
+			var b = db.Users.Count();
+			//var a = db.AspNetUsers.Count();
+			Console.Write(b);
+		}
+	}
+}
