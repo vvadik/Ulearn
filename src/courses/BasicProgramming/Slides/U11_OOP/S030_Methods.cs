d13b6a6c96c711768da6f2401f174601ac48d9b6
Pavel Egorov <xoposhiy@bk.ru>
2014-11-12 08:38:03 +0500

    OOP unit: add samples to slide

--- src/courses/BasicProgramming/Slides/U11_OOP/S030_Methods.cs
+++ src/courses/BasicProgramming/Slides/U11_OOP/S030_Methods.cs
@@ -1,4 +1,6 @@
-﻿using uLearn;
+﻿using System;
+using NUnit.Framework;
+using uLearn;
 
 namespace uLearn.Courses.BasicProgramming.Slides.U11_OOP
 {
@@ -9,5 +11,27 @@ namespace uLearn.Courses.BasicProgramming.Slides.U11_OOP
 		/*
 		## Заметки по лекции
 		*/
+		class Point
+		{
+			public int X;
+			public int Y;
+			public void Print()
+			{
+				Console.WriteLine("{0} {1}", X, Y);
+			}
+
+			public static void PrintPoint(Point point)
+			{
+				Console.WriteLine("{0} {1}", point.X, point.Y);
+			}
+		}
+
+		[Test]
+		public static void Main()
+		{
+			var point = new Point { X = 1, Y = 2 };
+			point.Print();
+			Point.PrintPoint(point);
+		}
 	}
 }
