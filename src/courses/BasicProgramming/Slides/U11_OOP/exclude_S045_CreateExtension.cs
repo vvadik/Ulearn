78fb5aeb8f6e128009ae1b9a48982199c1872cf5
okulovsky <yuri.okulovsky@gmail.com>
2014-11-12 16:09:19 +0500

    Tried to add task for extension method, failed.

---
+++ src/courses/BasicProgramming/Slides/U11_OOP/exclude_S045_CreateExtension.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+
+//namespace uLearn.Courses.BasicProgramming.Slides.U11_OOP
+//{
+//    [Slide("Создание классов", "{06AA4E3E-C1F8-4895-BA1F-B7D5DF22BB28}")]
+//    public class S045_CreateExtension : SlideTestBase
+//    {
+//        /*
+         
+//         Сделайте так, чтобы заработало. 
+//         */
+
+//        [ExpectedOutput("15")]
+//        [Hint("Создайте extension-метод, который бы присоединялся к типу массива int, и вычислял бы сумму элементов этого массива.")]
+//        [Hint("Extension методы - статические, определяются в статических классах, имеют первый аргумент с ключевым словом this")]
+//        static void Main()
+//        {
+//            var array = new int[] { 1, 2, 3, 4, 5 };
+//            Console.Write(array.Sum());
+//        }
+
+//        [HideOnSlide]
+//        [ExcludeFromSolution]
+//        public static class ArrayExtensionHidden
+//        {
+//            public static int Sum(this int[] array)
+//            {
+//                var sum = 0;
+//                foreach (var e in array)
+//                    sum += e;
+//                return sum;
+//            }
+//        }
+
+//    }
+//}
