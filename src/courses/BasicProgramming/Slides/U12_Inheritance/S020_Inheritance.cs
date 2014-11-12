6864d75cc3e6af3f3bfae1cdf88ff31ecd7ef0d8
okulovsky <yuri.okulovsky@gmail.com>
2014-11-12 17:39:43 +0500

    Лекции и квизы на наследование

--- src/courses/BasicProgramming/Slides/U12_Inheritance/S020_Inheritance.cs
+++ src/courses/BasicProgramming/Slides/U12_Inheritance/S020_Inheritance.cs
@@ -2,12 +2,47 @@
 
 namespace uLearn.Courses.BasicProgramming.Slides.U11_Inheritance
 {
-	[Slide("Наследование", "FD1D51EF-DDD1-4CA0-872F-3A9C89CE06E4")]
-	public class S020_Inheritance
-	{
-		//#video u8jnHaW_FP0
-		/*
-		## Заметки по лекции
-		*/
-	}
+    [Slide("Наследование", "FD1D51EF-DDD1-4CA0-872F-3A9C89CE06E4")]
+    public class S020_Inheritance
+    {
+        //#video u8jnHaW_FP0
+        /*
+        ## Заметки по лекции
+        */
+        class Transport
+        {
+            public double Velocity;
+            public double KilometerPrice;
+            public int Capacity;
+        }
+
+        class CombustionEngineTransport : Transport
+        // это двоеточие обозначает наследование
+        {
+            public double EngineVolume;
+            public double EnginePower;
+        }
+
+        enum ControlType
+        {
+            Forward,
+            Backward
+        }
+
+        class Car : CombustionEngineTransport
+        {
+            public ControlType Control;
+        }
+
+        class Program
+        {
+            public static void Main()
+            {
+                var c = new Car();
+                c.Control = ControlType.Backward; //это собственное поле класса Car
+                c.EnginePower = 100; // это поле унаследовано от CombustionEngineTransport
+                c.Capacity = 4; // это поле унаследовано от Transport
+            }
+        }
+    }
 }
