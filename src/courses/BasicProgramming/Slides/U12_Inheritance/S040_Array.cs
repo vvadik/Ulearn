e7b9186b3a4c4a7d3944b6246d8f11d413964432
okulovsky <yuri.okulovsky@gmail.com>
2014-11-13 13:43:26 +0500

    +2 tasks for Array/object, one is not yet working

--- src/courses/BasicProgramming/Slides/U12_Inheritance/S040_Array.cs
+++ src/courses/BasicProgramming/Slides/U12_Inheritance/S040_Array.cs
@@ -1,4 +1,5 @@
-﻿using uLearn;
+﻿using System;
+using uLearn;
 
 namespace uLearn.Courses.BasicProgramming.Slides.U11_Inheritance
 {
@@ -9,5 +10,25 @@ namespace uLearn.Courses.BasicProgramming.Slides.U11_Inheritance
 		/*
 		## Заметки по лекции
 		*/
+
+
+        static void Main()
+        {
+            var strings = new string[] { "B", "A", "C" };
+            strings.Swap(0, 1);
+
+            var ints = new int[] { 1, 3, 2 };
+            ints.Swap(1, 2);
+        }
 	}
+    
+    public static class ArrayExtensions
+    {
+        public static void Swap(this Array array, int i, int j)
+        {
+            object temporary = array.GetValue(i);
+            array.SetValue(array.GetValue(i), j);
+            array.SetValue(temporary, j);
+        }
+    }
 }
