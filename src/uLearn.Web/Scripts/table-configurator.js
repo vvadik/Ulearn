974cfbcb2f76b667693bec271a3e0b5227da9360
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2014-11-17 13:19:34 +0500

    remove default-value and deprecated options in table-configurator.js

--- src/uLearn.Web/Scripts/table-configurator.js
+++ src/uLearn.Web/Scripts/table-configurator.js
@@ -1,16 +1,6 @@
 ﻿$('.tablesorter').tablesorter({
 	widthFixed: true,
 	widgets: ['zebra'],
-	dateFormat: "ddmmyyyy",
-
-	sortInitialOrder: "asc",
-	sortForce: null,
-	sortList: [],
-	sortAppend: null,
-	sortLocaleCompare: false,
-	sortReset: false,
-	sortRestart: false,
-	sortMultiSortKey: "shiftKey",
 
 	onRenderHeader: function () {
 		$(this).find('span').addClass('headerSpan');
@@ -18,24 +8,13 @@
 	selectorHeaders: 'thead th',
 
 	cssAsc: "headerSortUp",
-	cssChildRow: "expand-child",
 	cssDesc: "headerSortDown",
 	cssHeader: "header",
 	tableClass: 'tablesorter',
 
-	widgetColumns: { css: ["primary", "secondary", "tertiary"] },
-	widgetUitheme: {
-		css: [
-			"ui-icon-arrowthick-2-n-s",
-			"ui-icon-arrowthick-1-s",
-			"ui-icon-arrowthick-1-n"
-		]
+	widgetOptions: {
+		columns: ["primary", "secondary", "tertiary"],
 	},
-	widgetZebra: { css: ["ui-widget-content", "ui-state-default"] },
-	
-	cancelSelection: true,
-	
-	debug: false,
-	
+
 	textAttribute: 'data-sort-value'
 });
