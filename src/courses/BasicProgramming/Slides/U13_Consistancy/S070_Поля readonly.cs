89035ce2bdb6e1b3bfb9ac5f4d71803c2492e30b
okulovsky <yuri.okulovsky@gmail.com>
2014-11-20 14:38:13 +0500

    + code samples for lecture 13

--- src/courses/BasicProgramming/Slides/U13_Consistancy/S070_Поля readonly.cs
+++ src/courses/BasicProgramming/Slides/U13_Consistancy/S070_Поля readonly.cs
@@ -3,16 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using uLearn; 
+using uLearn;
 
 namespace U13_Consistancy
 {
-	[Slide("Поля readonly", "ac9cb965-c780-4117-abae-8f1ad41a08c9")]
-	class S070_Поля_readonly
-	{
-		//#video cNSd8kDKK0E
-		/*
-		## Заметки по лекции
-		*/
+    [Slide("Поля readonly", "ac9cb965-c780-4117-abae-8f1ad41a08c9")]
+    class S070_Поля_readonly
+    {
+        //#video cNSd8kDKK0E
+        /*
+        ## Заметки по лекции
+        */
+        public class TournirInfo
+        {
+            //readonly поле - это еще более сильное ограничение целостности
+            //такие поля можно присваивать только в конструкторе
+            public readonly int TeamsCount;
+            public readonly string[] TeamsNames;
+            public readonly double[,] Scores;
+            public TournirInfo(int teamsCount)
+            {
+                TeamsCount = teamsCount;
+                TeamsNames = new string[teamsCount];
+                Scores = new double[teamsCount, teamsCount];
+            }
+
+            public void SomeMethod()
+            {
+                // TeamsCount = 4; //так писать нельзя, хотя мы внутри класса
+            }
+        }
+
+        public class Program
+        {
+            public static void Main()
+            {
+                var info = new TournirInfo(4);
+                // info.TeamsCount = 5; //так тоже нельзя, хотя поле public
+            }
+        }
+
+        //Разница между readonly полями и константами заключается в том,
+        //что readonly поля присваиваются при вызове конструктора (возможно, неявно),
+        //а константы - во время компиляции программы.
+
+        class Test
+        {
+            public readonly DateTime Time = DateTime.Now; 
+            //Вызов Now произойдет при создании объекта
+            //в неявно созданном конструкторе
+        }
+
+
     }
 }
--- src/courses/BasicProgramming/Slides/U13_Consistancy/S080_Статические конструкторы.cs
+++ src/courses/BasicProgramming/Slides/U13_Consistancy/S080_Статические конструкторы.cs
@@ -14,5 +14,30 @@ namespace U13_Consistancy
 		/*
 		## Заметки по лекции
 		*/
+        class Test
+        {
+            public static readonly DateTime Readonly;
+            public readonly int Number;
+            //Статические конструкторы всегда без параметров
+            static Test()
+            {
+                Readonly = DateTime.Now;
+            }
+            //это динамический конструктор
+            public Test()
+            {
+                Number = 3;
+            }
+        }
+
+        class Program
+        {
+            public static void Main()
+            {
+                var test = new Test();
+                //сначала вызовется статический конструктор (настройка типа)
+                //и только после этого - динамический
+            }
+        }
     }
 }
