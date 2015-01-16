67aca657ea684dfde855fbc66bd5dd09765ac1a9
Pavel Egorov <xoposhiy@mail.ru>
2015-01-16 20:04:27 +0500

    Move course to git@github.com:urfu-code/BasicProgramming.git

--- src/courses/BasicProgramming/Slides/U01_Mistakes/S070_Style.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace uLearn.Courses.BasicProgramming.Slides.U01_Mistakes
-{
-	[Slide("Стилистические ошибки", "{1FAAA541-C3F5-4D77-9C27-D0FA634FB180}")]
-	class S070_Style
-	{
-		//#video jFcZaDkvzYI
-	}
-}
--- src/courses/BasicProgramming/Slides/U07_Complexity/S010_AlgorithmsMatters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace uLearn.Courses.BasicProgramming.Slides.U07_Complexity
-{
-	[Slide("О важности алгоритмов", "{2CA4334B-CCD3-4EB6-BEEF-5D84CF83E949}")]
-	class S010_AlgorithmsMatters
-	{
-		//#video aQWTrGM6HEg
-	}
-}
