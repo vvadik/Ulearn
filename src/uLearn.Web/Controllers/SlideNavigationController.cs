42d9b4b2627608198679202ce45ddbe852c5bc82
Pavel Egorov <xoposhiy@mail.ru>
2015-04-24 00:53:17 +0500

    Fix Slide Navigation. Again :(

--- src/uLearn.Web/Controllers/SlideNavigationController.cs
+++ src/uLearn.Web/Controllers/SlideNavigationController.cs
@@ -62,7 +62,7 @@ namespace uLearn.Web.Controllers
 			var course = courseManager.GetCourse(courseId);
 			var slide = course.Slides[slideIndex];
 			var userId = User.Identity.GetUserId();
-			var nextIsAcceptedSolutions = slide is ExerciseSlide && visitersRepo.IsSkippedOrPassed(slide.Id, userId);
+			var nextIsAcceptedSolutions = !onSolutionsSlide && slide is ExerciseSlide && visitersRepo.IsSkippedOrPassed(slide.Id, userId);
 			var visibleUnits = unitsRepo.GetVisibleUnits(courseId, User);
 			var nextSlide = course.Slides.FirstOrDefault(s => s.Index > slideIndex && visibleUnits.Contains(s.Info.UnitName));
 			var prevSlide = course.Slides.LastOrDefault(s => s.Index < slideIndex && visibleUnits.Contains(s.Info.UnitName));
