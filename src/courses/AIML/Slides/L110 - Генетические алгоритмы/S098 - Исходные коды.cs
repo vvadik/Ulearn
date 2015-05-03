a244d407a29604a4528f917455d7654f8c1fd7f6
okulovsky <yuri.okulovsky@gmail.com>
2015-05-03 19:02:08 +0500

    + GA Lectures

---
+++ src/courses/AIML/Slides/L110 - Генетические алгоритмы/S098 - Исходные коды.cs
@@ -0,0 +1,13 @@
+﻿
+using uLearn;
+namespace AIML
+{
+    [Slide(@"Исходные коды программ", "{559DA071-91BD-482E-BC6E-5063DD139A90}")]
+	public class Исходные_коды_программ_5
+	{
+		/*
+		[Скачать исходные коды программ](GeneticAlgorithms.zip)
+		*/
+
+	}
+}
