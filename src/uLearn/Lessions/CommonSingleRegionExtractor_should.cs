1d64faa184afd2ab06d114f5f97140c28a656d90
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2015-05-26 13:45:39 +0500

    some tests for CommonSingleRegionExtractor

---
+++ src/uLearn/Lessions/CommonSingleRegionExtractor_should.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using uLearn.CSharp;
+
+namespace uLearn.Lessions
+{
+	[TestFixture]
+	public class CommonSingleRegionExtractor_should
+	{
+		private readonly FileSystem fs = new FileSystem(new DirectoryInfo("tests"));
+
+		private string GetRegion(string region, string fileName = "OverloadedMethods.cs")
+		{
+			var code = fs.GetContent(fileName);
+			var extractor = new CommonSingleRegionExtractor(code);
+			return extractor.GetRegion(new Label { Name = region });
+		}
+
+		[Test, UseReporter(typeof(DiffReporter))]
+		public void ExtractAllFile()
+		{
+			Approvals.Verify(GetRegion("all"));
+		}
+
+		[Test, UseReporter(typeof(DiffReporter))]
+		public void ExtractOverlap1()
+		{
+			Approvals.Verify(GetRegion("methods_1_and_2"));
+		}
+
+		[Test, UseReporter(typeof(DiffReporter))]
+		public void ExtractOverlap2()
+		{
+			Approvals.Verify(GetRegion("methods_2_and_3"));
+		}
+	}
+}
--- src/uLearn/Lessions/RegionsExtractor.cs
+++ src/uLearn/Lessions/RegionsExtractor.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using uLearn.CSharp;
 
-namespace uLearn
+namespace uLearn.Lessions
 {
 	public interface ISingleRegionExtractor
 	{
@@ -37,26 +37,6 @@ namespace uLearn
 		private static string FixEolns(string arg)
 		{
 			return Regex.Replace(arg.Trim(), "(\t*\r*\n){3,}", "\r\n\r\n");
-		}
-	}
-
-	public class CommonSingleRegionExtractor : ISingleRegionExtractor
-	{
-		private readonly Dictionary<string, RegionsParser.Region> regions;
-		private readonly string code;
-
-		public CommonSingleRegionExtractor(string code)
-		{
-			this.code = code;
-			regions = RegionsParser.GetRegions(code);
-		}
-
-		public string GetRegion(Label label)
-		{
-			var region = regions.Get(label.Name, null);
-			if (region == null)
-				return null;
-			return code.Substring(region.dataStart, region.dataLength).RemoveCommonNesting();
 		}
 	}
 }
