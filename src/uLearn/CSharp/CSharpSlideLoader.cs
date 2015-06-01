e25aa566d0e314b500136a198aa096d5b76d63b0
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2015-06-01 13:52:59 +0500

    extract FileSystem

--- src/uLearn/CSharp/CSharpSlideLoader.cs
+++ src/uLearn/CSharp/CSharpSlideLoader.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 
@@ -22,39 +21,6 @@ namespace uLearn.CSharp
 				SelectMany(d => d.GetFiles("prelude.*", SearchOption.TopDirectoryOnly))
 				.FirstOrDefault(f => f.Exists);
 			return preludeFile == null ? "" : preludeFile.ContentAsUtf8();
-		}
-	}
-
-	
-	public interface IFileSystem
-	{
-		string GetContent(string filepath);
-		string[] GetFilenames(string dirPath);
-	}
-
-	public class FileSystem : IFileSystem
-	{
-		private readonly DirectoryInfo baseDir;
-
-		public FileSystem(DirectoryInfo baseDir)
-		{
-			this.baseDir = baseDir;
-		}
-
-		public string GetContent(string filepath)
-		{
-			var fileInfo = baseDir.GetFile(filepath);
-			if (fileInfo.Exists) return fileInfo.ContentAsUtf8();
-				throw new Exception("No " + filepath + " in " + baseDir.Name);
-		}
-
-		public string[] GetFilenames(string dirPath)
-		{
-			var dir = new DirectoryInfo(Path.Combine(baseDir.FullName, dirPath));
-			if (!dir.Exists)
-				throw new Exception("No " + dirPath + " in " + baseDir.Name);
-			return dir.GetFiles().Select(f => dirPath + "/" + f.Name).ToArray();
-
 		}
 	}
 }
