efa1c81aea4a0a970223335eb1853b1aaed3b915
Pavel Egorov <xoposhiy@mail.ru>
2015-10-13 17:03:14 +0500

    Fix unauthorized redirect address

--- src/uLearn.Web/App_Start/Startup.Auth.cs
+++ src/uLearn.Web/App_Start/Startup.Auth.cs
@@ -16,7 +16,7 @@ namespace uLearn.Web
 			app.UseCookieAuthentication(new CookieAuthenticationOptions
 			{
 				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-				LoginPath = new PathString("/Account/Login")
+				LoginPath = new PathString("/Login/index")
 			});
 			// Use a cookie to temporarily store information about a user logging in with a third party login provider
 			app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
