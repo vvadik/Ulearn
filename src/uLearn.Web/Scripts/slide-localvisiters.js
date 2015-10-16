736a0bc27e8b06c8dda5146f1145a08d9d848d79
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2015-10-16 13:34:45 +0500

    show visited slides for guests

--- src/uLearn.Web/Scripts/slide-localvisiters.js
+++ src/uLearn.Web/Scripts/slide-localvisiters.js
@@ -1,6 +1,6 @@
 ﻿function saveVisiters(url, time) {
 	var obj = {};
-	if (typeof localStorage['visiters'] !== "undefined")
+	if (typeof localStorage['visiters'] !== 'undefined')
 		obj = JSON.parse(localStorage['visiters']);
 	obj[url] = time;
 	localStorage['visiters'] = JSON.stringify(obj);
@@ -20,4 +20,22 @@ function uploadVisiters(url) {
 		.fail(function(req) {
 			console.log(req.responseText);
 		});
+}
+
+function displayVisits(courseId) {
+	if (typeof localStorage['visiters'] === 'undefined')
+		return;
+	var slidesGlyphs = $('.slide-list-item > .glyphicon:not(.navbar-label-privileged)');
+	var visits = JSON.parse(localStorage['visiters']);
+	for (var visit in visits) {
+		var parts = visit.split('/', 2);
+		if (courseId != parts[0] || !$.isNumeric(parts[1]))
+			continue;
+		var ind = parseInt(parts[1]);
+		var glyph = $(slidesGlyphs[ind]);
+		if (glyph.hasClass('glyphicon-none')) {
+			glyph.removeClass('glyphicon-none');
+			glyph.addClass('glyphicon-ok');
+		}
+	}
 }
