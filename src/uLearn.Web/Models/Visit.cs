f18e19b7e49d8078a4b0db9ba4dba9866e8fbeb0
Pavel Egorov <xoposhiy@mail.ru>
2015-12-21 13:36:25 +0500

    fix bug

---
+++ src/uLearn.Web/Models/Visit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace uLearn.Web.Models
+{
+	public class Visit : ISlideAction
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public virtual ApplicationUser User { get; set; }
+
+		[StringLength(64)]
+		[Required]
+		[Index("IDX_Visits_UserAndSlide", 1)]
+		public string UserId { get; set; }
+
+		[Required]
+		[StringLength(64)]
+		public string CourseId { get; set; }
+
+		[Required]
+		[StringLength(64)]
+		[Index("IDX_Visits_UserAndSlide", 2)]
+		[Index("IDX_Visits_SlideAndTime", 1)]
+		public string SlideId { get; set; }
+
+		///<summary>Первый заход на слайд</summary>
+		[Required]
+		[Index("IDX_Visits_SlideAndTime", 2)]
+		public DateTime Timestamp { get; set; }
+
+		public int Score { get; set; }
+		public int AttemptsCount { get; set; }
+		public bool IsSkipped { get; set; }
+		public bool IsPassed { get; set; }
+	}
+}
---
+++ src/uLearn.Web/Scripts/slide-localvisits.js
@@ -0,0 +1,40 @@
+﻿function saveVisits(id, time) {
+	var obj = {};
+	if (typeof localStorage['visits'] !== 'undefined')
+		obj = JSON.parse(localStorage['visits']);
+	obj[id] = time;
+	localStorage['visits'] = JSON.stringify(obj);
+}
+
+function uploadVisits(url) {
+	if (typeof localStorage['visits'] === 'undefined')
+		return;
+	$.ajax({
+			type: 'POST',
+			url: url,
+			data: localStorage['visits']
+		})
+		.success(function(ans) {
+			localStorage.removeItem('visits');
+		})
+		.fail(function(req) {
+			console.log(req.responseText);
+		});
+}
+
+function displayVisits() {
+	if (typeof localStorage['visits'] === 'undefined')
+		return;
+	var visits = JSON.parse(localStorage['visits']);
+	for (var visit in visits) {
+		var glyph = $('[data-slide-id="' + visit + '"] > i');
+		if (glyph.hasClass('glyphicon-none')) {
+			glyph.removeClass('glyphicon-none');
+			glyph.addClass('glyphicon-ok');
+		}
+		else if (!glyph.hasClass('navbar-label-success')) {
+			glyph.removeClass('navbar-label-default');
+			glyph.addClass('navbar-label-danger');
+		}
+	}
+}
