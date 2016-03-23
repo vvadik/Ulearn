0d336bb18cb1ed0eb81ddc2495299109db545a72
Andrew Gein <andgein@yandex.ru>
2016-03-24 00:37:19 +0500

    Fixed SlideHtml.generated.cs
    1. Some Migration makeup

--- src/uLearn.Web/Migrations/201603230005459_SlideIdIsGuid.cs
+++ src/uLearn.Web/Migrations/201603230005459_SlideIdIsGuid.cs
@@ -1,3 +1,5 @@
+using ApprovalUtilities.Utilities;
+
 namespace uLearn.Web.Migrations
 {
     using System;
@@ -5,6 +7,11 @@ namespace uLearn.Web.Migrations
     
     public partial class SlideIdIsGuid : DbMigration
     {
+	    private void InsertHyphensIntoGuids(string tableName)
+	    {
+			Sql("UPDATE {0} SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32".FormatWith(tableName));
+		}
+
         public override void Up()
         {
             DropIndex("dbo.Comments", "IDX_Comment_CommentBySlide");
@@ -16,22 +23,22 @@ namespace uLearn.Web.Migrations
             DropIndex("dbo.UserQuizs", "StatisticsIndex");
             DropIndex("dbo.Visits", "IDX_Visits_UserAndSlide");
             DropIndex("dbo.Visits", "IDX_Visits_SlideAndTime");
-			Sql("UPDATE dbo.Comments SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.Comments");
 			AlterColumn("dbo.Comments", "SlideId", c => c.Guid(nullable: false));
-			Sql("UPDATE dbo.UserQuestions SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.UserQuestions");
 			AlterColumn("dbo.UserQuestions", "SlideId", c => c.Guid(nullable: false));
 			Sql("UPDATE dbo.UserSolutions SET SlideId = '00000000-0000-0000-0000-000000000000' WHERE SlideId = 'runner'");
-			Sql("UPDATE dbo.UserSolutions SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.UserSolutions");
 			AlterColumn("dbo.UserSolutions", "SlideId", c => c.Guid(nullable: false));
-			Sql("UPDATE dbo.SlideHints SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.SlideHints");
 			AlterColumn("dbo.SlideHints", "SlideId", c => c.Guid(nullable: false));
-			Sql("UPDATE dbo.LtiSlideRequests SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.LtiSlideRequests");
 			AlterColumn("dbo.LtiSlideRequests", "SlideId", c => c.Guid(nullable: false));
-			Sql("UPDATE dbo.SlideRates SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.SlideRates");
 			AlterColumn("dbo.SlideRates", "SlideId", c => c.Guid(nullable: false));
-	        Sql("UPDATE dbo.UserQuizs SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.UserQuizs");
 			AlterColumn("dbo.UserQuizs", "SlideId", c => c.Guid(nullable: false));
-			Sql("UPDATE dbo.Visits SET SlideId = SUBSTRING(SlideId, 1, 8) + '-' + SUBSTRING(SlideId, 9, 4) + '-' + SUBSTRING(SlideId, 13, 4) + '-' + SUBSTRING(SlideId, 17, 4) + '-' + SUBSTRING(SlideId, 21, 12) WHERE LEN(SlideId) = 32");
+			InsertHyphensIntoGuids("dbo.Visits");
 			AlterColumn("dbo.Visits", "SlideId", c => c.Guid(nullable: false));
             CreateIndex("dbo.Comments", "SlideId", name: "IDX_Comment_CommentBySlide");
             CreateIndex("dbo.UserSolutions", new[] { "SlideId", "IsRightAnswer", "CodeHash", "Timestamp" }, name: "AcceptedList");
@@ -43,8 +50,12 @@ namespace uLearn.Web.Migrations
             CreateIndex("dbo.Visits", new[] { "UserId", "SlideId" }, name: "IDX_Visits_UserAndSlide");
             CreateIndex("dbo.Visits", new[] { "SlideId", "Timestamp" }, name: "IDX_Visits_SlideAndTime");
         }
-        
-        public override void Down()
+
+		/*
+		For correct migration's down:
+			Replace all slides' guids (in courses files) with version with hyphens (see InsertHyphensIntoGuids())
+		*/
+		public override void Down()
         {
             DropIndex("dbo.Visits", "IDX_Visits_SlideAndTime");
             DropIndex("dbo.Visits", "IDX_Visits_UserAndSlide");
@@ -57,7 +68,6 @@ namespace uLearn.Web.Migrations
             DropIndex("dbo.Comments", "IDX_Comment_CommentBySlide");
             AlterColumn("dbo.Visits", "SlideId", c => c.String(nullable: false, maxLength: 64));
 			AlterColumn("dbo.UserQuizs", "SlideId", c => c.String(nullable: false, maxLength: 64));
-			Sql("UPDATE dbo.UserQuizs SET SlideId = SUBSTRING(SlideId, 1, 8) + SUBSTRING(SlideId, 10, 4) + SUBSTRING(SlideId, 15, 4) + SUBSTRING(SlideId, 20, 4) + SUBSTRING(SlideId, 25, 12)");
 			AlterColumn("dbo.SlideRates", "SlideId", c => c.String(nullable: false, maxLength: 64));
             AlterColumn("dbo.LtiSlideRequests", "SlideId", c => c.String(nullable: false, maxLength: 64));
             AlterColumn("dbo.SlideHints", "SlideId", c => c.String(nullable: false, maxLength: 64));
