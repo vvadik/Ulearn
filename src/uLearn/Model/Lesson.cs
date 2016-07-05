d6f6f704e3df91323ffbfaec4b1223e7fdb039d7
Andrew Gein <andgein@yandex.ru>
2016-07-05 16:18:46 +0500

    Back compability for default-include-code-file and exercise tags

--- src/uLearn/Model/Lesson.cs
+++ src/uLearn/Model/Lesson.cs
@@ -1,8 +1,35 @@
-﻿using System.Xml.Serialization;
+﻿using System.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
 using uLearn.Model.Blocks;
 
 namespace uLearn.Model
 {
+	[XmlType(IncludeInSchema = false)]
+	public enum BlockTypes
+	{
+		[XmlEnum("youtube")]
+		YouTube,
+		[XmlEnum("md")]
+		Md,
+		[XmlEnum("code")]
+		Code,
+		[XmlEnum("tex")]
+		Tex,
+		[XmlEnum("gallery-images")]
+		GalleryImages,
+		[XmlEnum("include-code")]
+		IncludeCode,
+		[XmlEnum("include-md")]
+		IncludeMd,
+		[XmlEnum("gallery")]
+		IncludeImageGalleryBlock,
+		[XmlEnum("exercise")]
+		ExerciseBlock,
+		[XmlEnum("execirse")]
+		ExecirseBlock
+	}
+
 	[XmlRoot("Lesson", IsNullable = false, Namespace = "https://ulearn.azurewebsites.net/lesson")]
 	public class Lesson
 	{
@@ -12,8 +39,15 @@ namespace uLearn.Model
 		[XmlElement("id")]
 		public string Id;
 		
+		[XmlElement("default-include-file")]
+		public string DefaultInclideFile;
+
 		[XmlElement("default-include-code-file")]
-		public string DefaultIncludeCodeFile;
+		public string DefaultIncludeCodeFile
+		{
+			get { return DefaultInclideFile;  }
+			set { DefaultInclideFile = value;  }
+		}
 
 		[XmlElement(typeof(YoutubeBlock))]
 		[XmlElement(typeof(MdBlock))]
@@ -24,7 +58,12 @@ namespace uLearn.Model
 		[XmlElement(typeof(IncludeMdBlock))]
 		[XmlElement(typeof(IncludeImageGalleryBlock))]
 		[XmlElement(typeof(ExerciseBlock))]
+		[XmlElement("execirse", typeof(ExerciseBlock))]
+		[XmlChoiceIdentifier("DefineBlockType")]
 		public SlideBlock[] Blocks;
+
+		[XmlIgnore]
+		public BlockTypes[] DefineBlockType;
 
 		public Lesson()
 		{
