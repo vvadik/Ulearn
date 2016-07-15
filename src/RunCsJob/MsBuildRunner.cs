6af4103f91c36efc617d09de5c070f32d4451390
dubrovin <dubrovin@K1311006>
2016-07-15 20:03:27 +0500

    Add new classes to csjob

---
+++ src/RunCsJob/MsBuildRunner.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Evaluation;
+using RunCsJob.Api;
+
+namespace RunCsJob
+{
+    public static class MsBuildRunner
+    {
+        public static string BuildProject(ProjRunnerSubmition submition)
+        {
+            var path = string.Format(@".\{0}\{1}", submition.Id, submition.ProjectFileName);
+            var proj = new Project(path);
+            //todo возможно добавить логгер
+            proj.Build();
+            return string.Format(@".\{0}\bin\Debug\{1}.exe", submition.Id, submition.ProjectName);
+        }
+    }
+}
---
+++ src/uLearn/Model/Blocks/ProjectInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace uLearn.Model.Blocks
+{
+    ///<summary>Отметка в коде</summary>
+    public class ProjectInfo
+    {
+        [XmlAttribute("project-name")]
+        public string ProjectName { get; set; }
+
+        [XmlAttribute("project-path")]
+        public string Path { get; set; }
+    }
+}
