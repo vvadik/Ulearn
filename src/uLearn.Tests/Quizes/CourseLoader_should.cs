b82103c10fb70d356f60b5cc2d9a734bbd1feaa0
Pavel Egorov <xoposhiy@mail.ru>
2016-08-29 12:57:37 +0500

    Make course loading test explicit

--- src/uLearn.Tests/Quizes/CourseLoader_should.cs
+++ src/uLearn.Tests/Quizes/CourseLoader_should.cs
@@ -11,7 +11,7 @@ namespace uLearn.Quizes
 	[TestFixture]
 	public class CourseLoader_should
 	{
-		[Test]
+		[Test, Explicit("Для профилирования загрузки курса")]
 		public void LoadCourse()
 		{
 			new CourseLoader().LoadCourse(new DirectoryInfo(@"c:\work\edu\BasicProgramming\Part01\BasicProgramming\Slides"));
