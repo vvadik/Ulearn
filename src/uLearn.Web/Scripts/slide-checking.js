0572cd3366a94f59f09e77f0cee141b656a7d105
Andrew Gein <andgein@yandex.ru>
2016-09-23 00:59:31 +0500

    Highlight other score link as active Move submissions'panel buttons Add some hint text

--- src/uLearn.Web/Scripts/slide-checking.js
+++ src/uLearn.Web/Scripts/slide-checking.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
 	var $exerciseScoreForm = $('.exercise__score-form');
 	var $scoreBlock = $('.exercise__score');
+	var $otherScoreLink = $scoreBlock.find('a');
 	var $otherScoreInput = $scoreBlock.find('[name=exerciseScore]');
 
 	$scoreBlock.on('click', 'a', function (e) {
@@ -8,6 +9,7 @@
 		$scoreBlock.find('.btn-group .btn').removeClass('active');
 		$otherScoreInput.show();
 		$otherScoreInput.focus();
+		$otherScoreLink.addClass('active');
 	});
 
 	$scoreBlock.find('.btn-group').on('click', '.btn', function () {
@@ -16,12 +18,14 @@
 		$(this).toggleClass('active', !wasActive);
 
 		$otherScoreInput.hide();
+		$otherScoreLink.removeClass('active');
 		$otherScoreInput.val(wasActive ? "" : $(this).data('value'));
 	});
 
 	$exerciseScoreForm.find('input[type=submit]').click(function() {
 		if ($otherScoreInput.is(':invalid')) {
 			$otherScoreInput.show();
+			$otherScoreLink.addClass('active');
 		}
 	});
 });
