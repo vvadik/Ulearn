a687dca2f7f5e17c54fa203a02a6dddd583f2c70
Andrew Gein <andgein@yandex.ru>
2016-09-22 23:35:41 +0500

    Exercise manual checking: Show other score input on error on form validating Automatically set percentage button active (and show 'other score' input) for already checked exercies

--- src/uLearn.Web/Scripts/slide-checking.js
+++ src/uLearn.Web/Scripts/slide-checking.js
@@ -1,6 +1,8 @@
 ﻿$(document).ready(function () {
+	var $exerciseScoreForm = $('.exercise__score-form');
 	var $scoreBlock = $('.exercise__score');
 	var $otherScoreInput = $scoreBlock.find('[name=exerciseScore]');
+
 	$scoreBlock.on('click', 'a', function (e) {
 		e.preventDefault();
 		$scoreBlock.find('.btn-group .btn').removeClass('active');
@@ -9,11 +11,17 @@
 	});
 
 	$scoreBlock.find('.btn-group').on('click', '.btn', function () {
-		var isActive = $(this).hasClass('active');
+		var wasActive = $(this).hasClass('active');
 		$scoreBlock.find('.btn-group .btn').removeClass('active');
-		$(this).toggleClass('active', !isActive);
+		$(this).toggleClass('active', !wasActive);
 
 		$otherScoreInput.hide();
-		$otherScoreInput.val($(this).data('value'));
+		$otherScoreInput.val(wasActive ? "" : $(this).data('value'));
+	});
+
+	$exerciseScoreForm.find('input[type=submit]').click(function() {
+		if ($otherScoreInput.is(':invalid')) {
+			$otherScoreInput.show();
+		}
 	});
 });
