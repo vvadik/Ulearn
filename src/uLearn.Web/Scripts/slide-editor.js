13dfc200e2c6ef195ccfe32608bf65372035b789
Andrew Gein <andgein@yandex.ru>
2016-10-07 12:25:45 +0500

    Show top 10 code review comments Some style fixes, disable some buttons while action is not completed Fix bug in analytics, fix bug in comment editing

--- src/uLearn.Web/Scripts/slide-editor.js
+++ src/uLearn.Web/Scripts/slide-editor.js
@@ -8,6 +8,7 @@
 		$self.find('.exercise__close-review')
 			.click(function(e) {
 				$self.hide();
+				$self.closest('.exercise').find('.code')[0].codeMirrorEditor.focus();
 				e.preventDefault();
 			});
 		$self.find('.exercise__add-review__comment').on('input', function () {
@@ -18,6 +19,7 @@
 		});
 
 		$self.find('.exercise__add-review__button').click(function () {
+			var $button = $(this);
 			var comment = $self.find('.exercise__add-review__comment').val();
 			var params = {
 				StartLine: editorLastRange.anchor.line,
@@ -26,10 +28,12 @@
 				FinishPosition: editorLastRange.head.ch,
 				Comment: comment,
 			}
+			$button.text('Сохранение..').attr('disabled', 'disabled');
 			$.post(addReviewUrl, params, function (renderedReview) {
 				addExerciseCodeReview(renderedReview);
 				$self.find('.exercise__add-review__comment').val('');
 				$self.hide();
+				$button.text('Сохранить').removeAttr('disabled');
 			});
 		});
 	});
