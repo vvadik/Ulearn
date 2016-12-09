90a00b5b69bb62ce702ff12a24025f8e56bde33b
Andrew Gein <andgein@yandex.ru>
2016-12-09 13:15:11 +0500

    Add real IP-address to logs
    Add comments to Web.config

--- src/uLearn.Web/App_Start/UlearnLogger.cs
+++ src/uLearn.Web/App_Start/UlearnLogger.cs
@@ -1,35 +1,41 @@
+using System.Linq;
 using System.Web;
 using log4net.Config;
 
 namespace uLearn.Web
 {
-	public static class UlearnLogger
-	{
-		public static void ConfigureLogging()
-		{
-			XmlConfigurator.Configure();
-			log4net.GlobalContext.Properties["user"] = new HttpContextUserNameProvider();
-			log4net.GlobalContext.Properties["address"] = new HttpContextAddressProvider();
-		}
-	}
+    public static class UlearnLogger
+    {
+        public static void ConfigureLogging()
+        {
+            XmlConfigurator.Configure();
+            log4net.GlobalContext.Properties["user"] = new HttpContextUserNameProvider();
+            log4net.GlobalContext.Properties["address"] = new HttpContextAddressProvider();
+        }
+    }
 
-	public class HttpContextUserNameProvider
-	{
-		public override string ToString()
-		{
-			var context = HttpContext.Current;
-			if (context?.User != null && context.User.Identity.IsAuthenticated)
-				return context.User.Identity.Name;
-			return "(unknown user)";
-		}
-	}
+    public class HttpContextUserNameProvider
+    {
+        public override string ToString()
+        {
+            var context = HttpContext.Current;
+            if (context?.User != null && context.User.Identity.IsAuthenticated)
+                return context.User.Identity.Name;
+            return "(unknown user)";
+        }
+    }
 
-	public class HttpContextAddressProvider
-	{
-		public override string ToString()
-		{
-			var context = HttpContext.Current;
-			return context?.Request.UserHostAddress ?? "";
-		}
-	}
+    public class HttpContextAddressProvider
+    {
+        private const string xRealIpHeaderName = "X-Real-IP";
+
+        public override string ToString()
+        {
+            var context = HttpContext.Current;
+            var realIp = context?.Request.Headers[xRealIpHeaderName];
+            if (!string.IsNullOrEmpty(realIp))
+                return realIp;
+            return context?.Request.UserHostAddress ?? "";
+        }
+    }
 }
