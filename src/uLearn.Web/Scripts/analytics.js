e0c57e9cd8919c965f3b204db57f572d74ba5dd2
Andrew Gein <andgein@yandex.ru>
2017-02-10 01:54:38 +0500

    Disabled manual checking settings for group if it is set for course Remove 'edit all scores' mode for unit statistics
    Fix some bugs in additional scoring

--- src/uLearn.Web/Scripts/analytics.js
+++ src/uLearn.Web/Scripts/analytics.js
@@ -1,5 +1,4 @@
 ﻿$(document).ready(function () {
-	var editMode = false;
 	var submittingData = false;
 
 	var errorOnUpdateAdditionalScore = function ($input, error) {
@@ -39,10 +38,7 @@
 		if ($input.data('openNext')) {
 			var $openNext = $input.data('openNext');
 			$input.data('openNext', '');
-			if (editMode)
-				$openNext.focus();
-			else
-				$openNext.click();
+			$openNext.click();
 		}
 	}
 
@@ -51,11 +47,6 @@
 		var $link = $input.parent().find('.additional-score-link');
 		$input.hide();
 		$link.show();
-	}
-
-	var updateEditModeControls = function () {
-		$('.additional-scores__edit-mode__link').toggle(!editMode);
-		$('.additional-scores__edit-mode__save-button').toggle(editMode);
 	}
 
 	var saveAdditionalScore = function($input) {
@@ -71,12 +62,6 @@
 					var $link = $input.parent().find('.additional-score-link');
 					$link.text(data.score === '' ? '—' : data.score);
 					hideAdditionalScoreInput($input);
-
-					/* Close edit mode if all scores has been accepted */
-					if (editMode) {
-						editMode = $('.additional-score-input:visible').length > 0;
-						updateEditModeControls();
-					}
 				} else {
 					errorOnUpdateAdditionalScore($input, data.error);
 				}
@@ -125,9 +110,6 @@
 	});
 
 	$('.additional-score-input').blur(function () {
-		if (editMode)
-			return;
-
 		var $self = $(this);
 		if (validateAdditionalScoreInput($self)) {
 			$self.hide();
@@ -140,8 +122,11 @@
 		var $next = undefined;
 		var $studentRow = $self.closest('.student');
 		var $nextStudentRow;
+		var needToSave = false;
 		if (e.which === 9) // Tab or Shift+Tab
 		{
+			needToSave = true;
+
 			var $additionalScore = $self.closest('.additional-score');
 			var $nextAdditionalScore = nextUntilHasChild($additionalScore, '.additional-score-link');
 			if (e.shiftKey)
@@ -163,45 +148,23 @@
 		}
 		if (e.which === 13 || e.which === 38 || e.which === 40) // Enter, Up or Down
 		{
+			needToSave = true;
+
 			var scoringType = $self.data('scoringType');
 			var selector = '.additional-score-link[data-scoring-type=' + scoringType + ']';
 			$nextStudentRow = nextUntilHasChild($studentRow, selector);
-			if (e.which === 38)
+			if (e.which === 38) // Up
 				$nextStudentRow = prevUntilHasChild($studentRow, selector);
-			if ($nextStudentRow.hasClass('student')) {
-				if (editMode) {
-					$next = $nextStudentRow.find('.additional-score-input[data-scoring-type=' + scoringType + ']');
-				} else {
-					$next = $nextStudentRow.find(selector);
-				}
+			if ($nextStudentRow) {
+				$next = $nextStudentRow.find(selector);
 			}
 		}
-		if ($next && $next.length) {
+		if (needToSave) {
 			e.preventDefault();
-			if (editMode)
-				$next.focus();
-			else {
+
+			if ($next && $next.length)
 				$self.data('openNext', $next);
-				$self.blur();
-			}
+			$self.blur();
 		}
-	});
-
-	$('.additional-scores__edit-mode__link').click(function(e) {
-		e.preventDefault();
-		editMode = true;
-		updateEditModeControls();
-
-		$('.additional-score-link').hide();
-		$('.additional-score-input').show().first().focus();
-	});
-
-	$('.additional-scores__edit-mode__save-button').click(function(e) {
-		e.preventDefault();
-		$('.additional-score-input:visible').each(function () {
-			var $input = $(this);
-			if (validateAdditionalScoreInput($input))
-				saveAdditionalScore($input);
-		});
 	});
 });
