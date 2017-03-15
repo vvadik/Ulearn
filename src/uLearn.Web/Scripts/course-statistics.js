209d6da0b7d9a9574f85a9f7fd3bc64570d821a3
Andrew Gein <andgein@yandex.ru>
2017-03-15 12:06:31 +0500

    Sorting course statistics
    Bluring expanded scoring group in course statistics
    Link to course statistics in instructor's menu

--- src/uLearn.Web/Scripts/course-statistics.js
+++ src/uLearn.Web/Scripts/course-statistics.js
@@ -12,6 +12,14 @@
 		var isExpanded = !$scoringGroupTitle.data('expanded');
 		$scoringGroupTitle.data('expanded', isExpanded);
 
+		var $expandIcon = $scoringGroupTitle.find('.expand-icon');
+		$expandIcon.text(isExpanded ? '◀' : '▶');
+		$expandIcon.remove();
+		if (isExpanded)
+			$scoringGroupTitle.find('a').prepend($expandIcon);
+		else
+			$scoringGroupTitle.find('a').append($expandIcon);
+
 		var filterByUnitAndScoringGroup = '[data-unit-id="' + unitId + '"][data-scoring-group="' + scoringGroupId + '"]';
 		$courseStatistics.find('.slide-score' + filterByUnitAndScoringGroup).toggle(isExpanded);
 		$courseStatistics.find('.slide-title' + filterByUnitAndScoringGroup).toggle(isExpanded);
@@ -20,6 +28,9 @@
 
 		var $unitTitleTh = $courseStatistics.find('.unit-title[data-unit-id="' + unitId + '"]').closest('th');
 		addColspan($unitTitleTh, isExpanded ? slidesCount : -slidesCount);
+		
+		$courseStatistics.find(filterByUnitAndScoringGroup).toggleClass('in-expanded-scoring-group', isExpanded);
+		$courseStatistics.toggleClass('with-expanded-scoring-group', $courseStatistics.find('.in-expanded-scoring-group').length > 0);
 	}
 
 	$courseStatistics.find('.expand-scoring-group__link').click(function(e) {
@@ -53,30 +64,98 @@
 		else
 			$('.course-statistics__enable-scoring-group__checkbox').removeAttr('disabled');
 	});
+
+	/* Table sorting */
+
+	/**
+	 * Sort rows in table
+	 * @param {} filter: jquery filter for fetching sorting cell from row
+	 * @param {} order: 'asc' or 'desc'. Default: 'asc'
+	 * @returns {} 
+	 */
+	var sortTable = function ($table, sortingFunctions) {
+		var $rows = $table.find('tbody tr').get();
+
+		$rows.sort(function (first, second) {
+			var $firstRow = $(first);
+			var $secondRow = $(second);
+			for (var idx in sortingFunctions) {
+				if (sortingFunctions.hasOwnProperty(idx)) {
+					var sortingFunction = sortingFunctions[idx];
+					var compare = sortingFunction($firstRow, $secondRow);
+					if (compare !== 0)
+						return compare;
+				}
+			}
+			return 0;
+		});
+
+		$.each($rows, function (index, row) {
+			$table.children('tbody').append(row);
+		});
+	}
+
+	var getCompareFunction = function(filter, order) {
+		return function ($firstRow, $secondRow) {
+			var firstValue = $firstRow.children('td' + filter).text().toUpperCase();
+			var secondValue = $secondRow.children('td' + filter).text().toUpperCase();
+
+			if (firstValue < secondValue)
+				return order === 'asc' ? -1 : 1;
+			if (firstValue > secondValue)
+				return order === 'asc' ? 1 : -1;
+			return 0;
+		}
+	}
+
+	var changeOrder = function(order) {
+		return order === 'desc' ? 'asc' : 'desc';
+	}
+
+	var getSortingFunction = function($th, order) {
+		var unitId = $th.data('unitId');
+		var scoringGroupId = $th.data('scoringGroup');
+		var slideId = $th.data('slideId');
+		var filter = '[data-scoring-group="' + scoringGroupId + '"][data-unit-id="' + unitId + '"]';
+		if (slideId)
+			filter += '[data-slide-id="' + slideId + '"]';
+
+		return getCompareFunction(filter, order);
+	}
 	
-	/*
-	$courseStatistics.tablesorter({
-		widgets: ['zebra', 'stickyHeaders', 'group', 'scroller'],
-		sortInitialOrder: $courseStatistics.data('initial-order') || 'asc',
+	$courseStatistics.find('thead th[data-sorter="true"]').click(function (e) {
+		e.preventDefault();
+		window.getSelection().removeAllRanges();
 
-		selectorHeaders: 'thead th',
+		var $self = $(this);
+		/* Exclude current column from sorting */
+		$self.removeClass('sorted sorted-asc sorted-desc');
 
-		cssAsc: "headerSortUp",
-		cssDesc: "headerSortDown",
-		cssHeader: "header",
-		tableClass: 'tablesorter',
+		var sortingFunctions = [];
+		if (e.shiftKey) {
+			var $oldSortingThs = $courseStatistics.find('thead th[data-sorter="true"].sorted');
+			$oldSortingThs.sort(function(first, second) {
+				var firstValue = $(first).data('sorting-index');
+				var secondValue = $(second).data('sorting-index');
+				if (firstValue < secondValue) return -1;
+				if (firstValue > secondValue) return 1;
+				return 0;
+			});
+			sortingFunctions = $.map($oldSortingThs, function(el) { return getSortingFunction($(el), $(el).data('order')); });
+		} else {
+			$courseStatistics.find('thead th[data-sorter="true"].sorted').data('order', undefined).removeClass('sorted sorted-asc sorted-desc');
+		}
+		/* Find maximum of sorting index of current sorting columns */
+		var maxSortingIndex = 0;
+		$courseStatistics.find('thead th[data-sorter="true"].sorted').each(function() {
+			if ($(this).data('sorting-index') > maxSortingIndex)
+				maxSortingIndex = $(this).data('sorting-index');
+		});
 
-		widgetOptions: {
-			columns: ["primary", "secondary", "tertiary"],
-			stickyHeaders_offset: "#header",
-			group_forceColumn: [1],
-			group_enforceSort: false,
-			//scroller_height : 300,
-			scroller_fixedColumns : 1,
-			scroller_rowHighlight : 'hover',
-		},
+		var order = changeOrder($self.data('order'));
+		sortingFunctions.push(getSortingFunction($self, order));
+		$self.data('order', order).data('sorting-index', maxSortingIndex + 1).addClass('sorted sorted-' + order);
 
-		textAttribute: 'data-sort-value'
+		sortTable($courseStatistics, sortingFunctions);
 	});
-	*/
 });
