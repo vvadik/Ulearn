c40cd6d2ea6ac91f5bf62f522625cfd5acc6b498
Mikhail Vostretsov <boulevardoffakes@gmail.com>
2017-07-17 10:53:02 +0500

    Added message about missing test classes.

---
+++ src/RunCsJob.Api/TestsNotFoundFlag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunCsJob.Api
+{
+	public class TestsNotFoundFlag
+	{
+		public static string Flag
+		{
+			get
+			{
+				if (flag == null)
+					flag = Guid.NewGuid();
+				return flag.ToString();
+			}
+		}
+
+		private static Guid? flag;
+	}
+}
