4bd9956a4f2b46bdda513c17994e0ed6d1f26241
Pavel Egorov <xoposhiy@mail.ru>
2017-08-02 23:58:48 +0500

    fix System.Runtime version in RunCsJob.Service

--- src/RunCsJob/AssemblyCreator.cs
+++ src/RunCsJob/AssemblyCreator.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using log4net;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Emit;
@@ -14,48 +12,45 @@ namespace RunCsJob
 {
 	public static class AssemblyCreator
 	{
-		private static ILog log = LogManager.GetLogger(typeof(AssemblyCreator));
-
-		private static readonly string assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-		private static readonly string systemRuntimeDllPath = Path.Combine(assemblyDirectory, "System.Runtime.dll");
-
-		public static IEnumerable<int> x = Enumerable.Range(1, 1);
-
 		public static CompileResult CreateAssemblyWithRoslyn(FileRunnerSubmission submission, string workingDirectory)
 		{
-			var currentDirectory = Directory.GetCurrentDirectory();
+			string currentDirectory = Directory.GetCurrentDirectory();
 			Directory.SetCurrentDirectory(workingDirectory);
+			try
+			{
+				var syntaxTree = CSharpSyntaxTree.ParseText(submission.Code);
+				string assemblyName = submission.Id;
 
-			IEnumerable<int> x = null;
-			var syntaxTree = CSharpSyntaxTree.ParseText(submission.Code);
-			var assemblyName = submission.Id;
+				var compilation = CSharpCompilation.Create(
+					assemblyName, new[] { syntaxTree },
+					new MetadataReference[]
+					{
+						MetadataReference.CreateFromFile(typeof(object).Assembly.Location), // mscorlib
+						MetadataReference.CreateFromFile(typeof(Uri).Assembly.Location), // System
+						MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location), // System.Core
+						MetadataReference.CreateFromFile(typeof(Point).Assembly.Location), // System.Drawing,
+						MetadataReference.CreateFromFile(typeof(ValueType).Assembly.Location), // System.Runtime
+					}, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
 
-			var compilation = CSharpCompilation.Create(assemblyName, new[] { syntaxTree },
-				new MetadataReference[]
-				{
-					MetadataReference.CreateFromFile(typeof(object).Assembly.Location), // mscorlib
-					MetadataReference.CreateFromFile(typeof(Uri).Assembly.Location), // System
-					MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location), // System.Core
-					MetadataReference.CreateFromFile(typeof(Point).Assembly.Location), // System.Drawing,
-					MetadataReference.CreateFromFile(typeof(ValueType).Assembly.Location), // System.Runtime
-					//MetadataReference.CreateFromFile(systemRuntimeDllPath), // System.Runtime (because typeof(ValueType).Assembly.Location sometimes appeared to be in mscorlib)
-				}, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
-
-			var assemblyFilename = Path.Combine(workingDirectory, assemblyName + ".exe");
-			Directory.SetCurrentDirectory(currentDirectory);
-			return new CompileResult(compilation.Emit(assemblyFilename), assemblyFilename);
+				string assemblyFilename = Path.Combine(workingDirectory, assemblyName + ".exe");
+				return new CompileResult(compilation.Emit(assemblyFilename), assemblyFilename);
+			}
+			finally
+			{
+				Directory.SetCurrentDirectory(currentDirectory);
+			}
 		}
 	}
 
 	public class CompileResult
 	{
+		public readonly EmitResult EmitResult;
+		public readonly string PathToAssembly;
+
 		public CompileResult(EmitResult emitResult, string pathToAssembly)
 		{
 			EmitResult = emitResult;
 			PathToAssembly = pathToAssembly;
 		}
-
-		public readonly EmitResult EmitResult;
-		public readonly string PathToAssembly;
 	}
 }
