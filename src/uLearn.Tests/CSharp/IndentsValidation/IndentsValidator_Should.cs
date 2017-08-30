efa81ffd419397d15ff317c98dd3fe83db1abde7
paatrofimov <epicpumpkin1@gmail.com>
2017-08-30 17:27:50 +0500

    added test helper for indentation validation

--- src/uLearn.Tests/CSharp/IndentsValidation/IndentsValidator_Should.cs
+++ src/uLearn.Tests/CSharp/IndentsValidation/IndentsValidator_Should.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using ApprovalTests;
 using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
@@ -48,7 +51,7 @@ namespace uLearn.CSharp.IndentsValidation
 		[Explicit]
 		public void NotFindErrors_On_Cs_Files_Of_Folder()
 		{
-			var folder = new DirectoryInfo("d:\\BP1_UserCodeFiles\\");
+			var folder = new DirectoryInfo("d:\\_work\\BasicProgramming\\");
 			var filesCode = folder.GetFiles("*.cs", SearchOption.AllDirectories)
 				.Where(f => !f.Name.Equals("Settings.Designer.cs") && !f.Name.Equals("Resources.Designer.cs"))
 				.Select(f => Tuple.Create(f.FullName, f.ContentAsUtf8()));
@@ -67,6 +70,63 @@ namespace uLearn.CSharp.IndentsValidation
 			}
 			if (failed)
 				Assert.Fail();
+		}
+
+		[Test(Description =
+			"Метод запускает валидатор отступов на .cs файлах указанной папки, разбивает найденные ошибки по группам и записывает результаты в html-файлы")]
+		[Explicit]
+		public void BuildHtmlWithErrors()
+		{
+			var folder = new DirectoryInfo("d:\\BP1_UserCodeFiles\\");
+			var filesCode = folder.GetFiles("*.cs", SearchOption.AllDirectories)
+				.Where(f => !f.Name.Equals("Settings.Designer.cs") && !f.Name.Equals("Resources.Designer.cs"))
+				.Select(f => f.ContentAsUtf8());
+			var badCodesWithErrors = new Dictionary<string, List<string>>();
+			const string spanStart = "<span style=\"background-color: coral;\">";
+			const string spanEnd = "</span>";
+			foreach (var code in filesCode)
+			{
+				var errors = new IndentsValidator().FindError(code);
+				if (errors == null)
+					continue;
+				var errorsLines = errors
+					.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries)
+					.Skip(1)
+					.GroupBy(
+						error => Regex.Replace(error, "\\d+", string.Empty),
+						error => int.Parse(Regex.Match(error, "\\d+").Value) - 1)
+					.ToDictionary(group => group.Key, group => new HashSet<int>(group));
+				var codeLines = code.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+				foreach (var errorLines in errorsLines)
+				{
+					var highlightenedCode = new StringBuilder("<pre><code>");
+					for (var line = 0; line < codeLines.Length; line++)
+					{
+						highlightenedCode.AppendLine(errorLines.Value.Contains(line)
+							? $"{spanStart}{codeLines[line].Replace(' ', '.').Replace("\t", "  → ")}{spanEnd}"
+							: $"{codeLines[line].Replace(' ', '.').Replace("\t", "  → ")}");
+					}
+					if (!badCodesWithErrors.ContainsKey(errorLines.Key))
+					{
+						badCodesWithErrors[errorLines.Key] = new List<string>();
+					}
+					highlightenedCode.AppendLine("</pre></code>");
+					badCodesWithErrors[errorLines.Key].Add(highlightenedCode.ToString());
+				}
+			}
+			var i = 0;
+			foreach (var badCodesByError in badCodesWithErrors)
+			{
+				File.WriteAllText(
+					Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"Errors{i++}.html"),
+					$@"<div style=""white-space: pre;"">
+
+{badCodesByError.Key}
+
+{string.Join("\r\n", badCodesByError.Value.Shuffle())}
+</div>",
+					Encoding.UTF8);
+			}
 		}
 	}
 }
