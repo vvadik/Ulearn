ff5b82ebe484b6571d39a3935c3330c5d46ad89e
Andrew Gein <andgein@yandex.ru>
2017-10-14 17:24:40 +0500

    Test for StringExtensions.TruncateHtmlWithEllipsis()

---
+++ src/uLearn.Web/Extensions/StringExtensions_should.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace uLearn.Web.Extensions
+{
+	[TestFixture]
+	public class StringExtensions_should
+	{
+		[Test]
+		public void TruncateHtmlWithEllipsis_shouldAutoCloseTags()
+		{
+			var html = "Text before tag <b>text in tag</b> text after tag";
+			var truncated = html.TruncateHtmlWithEllipsis(25);
+			Assert.True(truncated.EndsWith("</b>..."));
+		}
+	}
+}
