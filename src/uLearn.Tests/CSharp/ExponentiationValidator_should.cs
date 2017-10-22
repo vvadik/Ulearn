5c965530ca32626a74c4b8b7fb7b2e4c0c4a17ec
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2017-10-22 16:49:21 +0500

    Tests for Math.Pow validator

---
+++ src/uLearn.Tests/CSharp/ExponentiationValidator_should.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+namespace uLearn.CSharp
+{
+	[TestFixture]
+	public class ExponentiationValidator_should
+	{
+		[Test]
+		public void ignore_when_no_math_pow()
+		{
+			var errors = FindErrors("int A() { return 2; }");
+			Assert.That(errors, Is.Null);
+		}
+		
+		[Test]
+		public void ignore_when_degree_greater_than_three()
+		{
+			var errors = FindErrors("void A() { var b = Math.Pow(2, 5); }");
+			Assert.That(errors, Is.Null);
+		}
+		
+		[Test]
+		public void warn_when_degree_equals_two()
+		{
+			var errors = FindErrors("void A() { var b = Math.Pow(2, 2); }");
+			Assert.That(errors, Is.Not.Null);
+		}
+		
+		[Test]
+		public void warn_when_degree_equals_three()
+		{
+			var errors = FindErrors("void A() { var b = Math.Pow(2, 3); }");
+			Assert.That(errors, Is.Not.Null);
+		}
+		
+		[Test]
+		public void analyse_code_with_semantic_model_if_possible()
+		{
+			var errors = FindErrors(@"
+using System;
+
+public class C 
+{
+	public int A() 
+	{ 
+		return Math.Pow(2, 3);
+	}
+}");
+			Assert.That(errors, Is.Not.Null);
+		}
+
+		[Test]
+		public void find_all_warnings()
+		{
+			var errors = FindErrors(@"
+void A(){ var b = Math.Pow(2, 3); }
+void B(){ var b = Math.Pow(2, 2); }
+");
+			Assert.That(errors, Does.Contain("Строка 2"));
+			Assert.That(errors, Does.Contain("Строка 3"));
+		}
+
+		private static string FindErrors(string code)
+		{
+			var errors = new ExponentiationValidator().FindError(code);
+			return errors;
+		}
+	}
+}
