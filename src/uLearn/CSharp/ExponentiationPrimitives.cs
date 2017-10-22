38671daf1b8064c9f96e38cfd4abf483611052c4
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2017-10-22 16:48:53 +0500

    Add validator that checks using System.Math.Pow

---
+++ src/uLearn/CSharp/ExponentiationPrimitives.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace uLearn.CSharp
+{
+	public static class ExponentiationPrimitives
+	{
+		public static bool IsMathPow(this InvocationExpressionSyntax invocation, ISymbol symbol = null)
+		{
+			var invokedMethodName = symbol?.ToString() ?? invocation.Expression.ToString();
+			return IsMathPowMethodName(invokedMethodName) 
+				&& invocation.ArgumentList.Arguments.Count == 2;
+		}
+
+		public static bool ContainsForbiddenDegrees(this ArgumentListSyntax argumentListSyntax,
+			HashSet<string> degrees)
+		{
+			return degrees.Contains(argumentListSyntax.Arguments[1].ToString());
+		}
+
+		private static bool IsMathPowMethodName(string methodName)
+		{
+			return methodName == "System.Math.Pow(double, double)"
+					|| methodName == "Math.Pow"
+					|| methodName == "System.Math.Pow";
+		}
+	}
+}
---
+++ src/uLearn/CSharp/ExponentiationValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace uLearn.CSharp
+{
+	public class ExponentiationValidator: BaseStyleValidator
+	{
+		private SemanticModel semanticModel;
+		private static readonly HashSet<string> forbiddenDegrees = 
+			new HashSet<string>{"2", "3"};
+		
+		protected override IEnumerable<string> ReportAllErrors(SyntaxTree userSolution)
+		{
+			var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+			var compilation = CSharpCompilation.Create("MyCompilation",
+				syntaxTrees: new[] { userSolution }, references: new[] { mscorlib });
+			semanticModel = compilation.GetSemanticModel(userSolution);
+			return InspectAll<InvocationExpressionSyntax>(userSolution, InspectMethod);
+		}
+
+		private IEnumerable<string> InspectMethod(InvocationExpressionSyntax methodInvocation)
+		{
+			var symbol = semanticModel.GetSymbolInfo(methodInvocation).Symbol;
+			if (methodInvocation.IsMathPow(symbol) &&
+				methodInvocation.ArgumentList.ContainsForbiddenDegrees(forbiddenDegrees))
+				yield return Report(methodInvocation, "вызов Math.Pow для маленьких степеней неэффективен");
+		}
+	}
+}
