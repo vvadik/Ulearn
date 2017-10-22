bb143b2c11bb6d328777460b263e8ed8eb8e2771
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2017-10-22 19:33:38 +0500

    Change tests in order to using semantic model in validator

--- src/uLearn.Tests/CSharp/ExponentiationValidator_should.cs
+++ src/uLearn.Tests/CSharp/ExponentiationValidator_should.cs
@@ -8,33 +8,53 @@ namespace uLearn.CSharp
 		[Test]
 		public void ignore_when_no_math_pow()
 		{
-			var errors = FindErrors("int A() { return 2; }");
+			var errors = FindErrors(@"
+using System;
+
+public class C 
+{
+	public int A() 
+	{ 
+		return 2;
+	}
+}");
 			Assert.That(errors, Is.Null);
 		}
 		
 		[Test]
 		public void ignore_when_degree_greater_than_three()
 		{
-			var errors = FindErrors("void A() { var b = Math.Pow(2, 5); }");
+			var errors = FindErrors(@"
+using System;
+
+public class C 
+{
+	public int A() 
+	{ 
+		return Math.Pow(2, 5);
+	}
+}");
 			Assert.That(errors, Is.Null);
 		}
 		
 		[Test]
 		public void warn_when_degree_equals_two()
 		{
-			var errors = FindErrors("void A() { var b = Math.Pow(2, 2); }");
+			var errors = FindErrors(@"
+using System;
+
+public class C 
+{
+	public int A() 
+	{ 
+		return Math.Pow(2, 2);
+	}
+}");
 			Assert.That(errors, Is.Not.Null);
 		}
 		
 		[Test]
 		public void warn_when_degree_equals_three()
-		{
-			var errors = FindErrors("void A() { var b = Math.Pow(2, 3); }");
-			Assert.That(errors, Is.Not.Null);
-		}
-		
-		[Test]
-		public void analyse_code_with_semantic_model_if_possible()
 		{
 			var errors = FindErrors(@"
 using System;
@@ -53,11 +73,18 @@ public class C
 		public void find_all_warnings()
 		{
 			var errors = FindErrors(@"
-void A(){ var b = Math.Pow(2, 3); }
-void B(){ var b = Math.Pow(2, 2); }
-");
-			Assert.That(errors, Does.Contain("Строка 2"));
-			Assert.That(errors, Does.Contain("Строка 3"));
+using System;
+
+public class C 
+{
+	public int A() 
+	{ 
+		var b = Math.Pow(2, 2);
+		return Math.Pow(2, 3);
+	}
+}");
+			Assert.That(errors, Does.Contain("Строка 8"));
+			Assert.That(errors, Does.Contain("Строка 9"));
 		}
 
 		private static string FindErrors(string code)
