f92023345ae95ae470958b96ff32c8868c998489
Andrew Gein <andgein@yandex.ru>
2018-02-05 20:46:00 +0500

    Rename Ulearn.CSharpAttributes.Core to Ulearn.CSharpAttributes

--- src/Ulearn.CSharpAttributes.Core/HideExpectedOutputOnErrorAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace uLearn
-{
-	public class HideExpectedOutputOnErrorAttribute : Attribute
-	{
-	}
-}
--- src/Ulearn.CSharpAttributes.Core/SingleStatementMethodAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace uLearn
-{
-	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-	public class SingleStatementMethodAttribute : Attribute
-	{
-	}
-}
