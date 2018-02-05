4497a4cb52d17673d4d232cb7be466018d424a91
Andrew Gein <andgein@yandex.ru>
2018-02-05 15:26:45 +0500

    NuSpec for RunCsJob and PowerShell script for deploying scheduled task
    + small refactoring

--- src/XQueueWatcher/Program.cs
+++ src/XQueueWatcher/Program.cs
@@ -59,13 +59,6 @@ namespace XQueueWatcher
 			}
 		}
 
-		/* We need to create new database connection each time for disabling EF's caching */
-		private static XQueueRepo GetNewXQueueRepo()
-		{
-			var db = new ULearnDb();
-			return new XQueueRepo(db, courseManager);
-		}
-
 		private async Task SafeGetAndProcessSubmissionFromXQueue(Database.Models.XQueueWatcher watcher)
 		{
 			try
@@ -142,6 +135,13 @@ namespace XQueueWatcher
 				graderPayload.SlideId,
 				submission.Body.StudentResponse
 			);
+		}
+		
+		/* We need to create new database connection each time for disabling EF's caching */
+		private static XQueueRepo GetNewXQueueRepo()
+		{
+			var db = new ULearnDb();
+			return new XQueueRepo(db, courseManager);
 		}
 	}
 
