69cdf29604e3581ae523d8f5eb8ae4a822e602ef
Andrew Gein <andgein@yandex.ru>
2018-02-10 21:31:20 +0500

    Fix certificate link

--- src/Database/Models/Notifications.cs
+++ src/Database/Models/Notifications.cs
@@ -730,7 +730,7 @@ namespace Database.Models
 
 		private static string GetCertificateUrl(Certificate certificate, string baseUrl)
 		{
-			return baseUrl + $"/Certificates/{certificate.Id}";
+			return baseUrl + $"/Certificate/{certificate.Id}";
 		}
 
 		public override string GetHtmlMessageForDelivery(NotificationTransport transport, NotificationDelivery delivery, Course course, string baseUrl)
