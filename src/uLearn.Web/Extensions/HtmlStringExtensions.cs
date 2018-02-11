d3b36d45949fc8a2780161782b4a707d8927ab91
Andrew Gein <andgein@yandex.ru>
2018-02-11 23:30:05 +0500

    Legacy HtmlString conversion for correct rendering in quizzes

---
+++ src/uLearn.Web/Extensions/HtmlStringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace uLearn.Web.Extensions
+{
+	public static class HtmlStringExtensions
+	{
+		public static HtmlString ToLegacyHtmlString(this global::Microsoft.AspNetCore.Html.HtmlString aspNetCoreHtmlString)
+		{
+			return new HtmlString(aspNetCoreHtmlString.Value);
+		}
+	}
+}
