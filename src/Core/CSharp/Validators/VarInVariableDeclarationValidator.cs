a5970a7d2ed7619edf5e4714d8b53a028ad6cb9d
Privalov Sergey <smprivalov@skbkontur.ru>
2018-02-17 13:12:53 +0500

    add var validator

---
+++ src/Core/CSharp/Validators/VarInVariableDeclarationValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace uLearn.CSharp.Validators
+{
+    public class VarInVariableDeclarationValidator : BaseStyleValidator
+    {
+        protected override IEnumerable<string> ReportAllErrors(SyntaxTree userSolution, SemanticModel semanticModel)
+        {
+            return InspectAll<VariableDeclarationSyntax>(userSolution, semanticModel, InspectMethodsNames);
+        }
+
+        private IEnumerable<string> InspectMethodsNames(VariableDeclarationSyntax variableDeclarationSyntax, SemanticModel semanticModel)
+        {
+            if (variableDeclarationSyntax.Type.IsVar)
+                yield break;
+
+            foreach (var variable in variableDeclarationSyntax.Variables)
+            {
+                if (variable.Initializer != null)
+                    yield return Report(variable, "Испульзуй var при явной инициализации переменой");
+            }
+        }
+    }
+}
---
+++ src/uLearn.Tests/CSharp/VarInVariableDeclarationValidator_should.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using uLearn.CSharp.Validators;
+
+namespace uLearn.CSharp
+{
+    [TestFixture]
+    public class VarInVariableDeclarationValidator_should
+    {
+        private VarInVariableDeclarationValidator validator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            validator = new VarInVariableDeclarationValidator();
+        }
+
+        
+        [TestCase(@"class A {void SomeMethod(){ int a = 0;}}")]
+        [TestCase(@"class A {void SomeMethod(){ for (int i=0; i<10; ++i){}}}")]
+        [TestCase(@"class A {void SomeMethod(){ for (var i=0; i<10; ++i){for (int j=0; j<10; ++j){}}}}")]
+        public void Warn_declaration_with_type(string code)
+        {
+            var errors = validator.FindError(code);
+
+            errors.Should().Contain("Строка 1");
+        }
+
+
+        [TestCase(@"class A {void SomeMethod(){ int b;}}")]
+        [TestCase(@"class A {void SomeMethod(){ var c = 0;}}")]
+        [TestCase(@"class A {void SomeMethod(){ for (var i=0; i<10; ++i){}}}")]
+        public void ignore_correct_declaration(string code)
+        {
+            validator.FindError(code).Should().BeNullOrEmpty();
+        }
+    }
+}
