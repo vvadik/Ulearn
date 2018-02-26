a2287827ba77fc74efe34c1201af90684c641b6a
Иванаев Станислав Владимирович <ivanaev@skbkontur.ru>
2018-02-26 10:37:12 +0500

    remove redundant files

--- src/uLearn.Tests/CSharp/ArrayGetLengthValidation/ArrayLengthStyleValidator_should.cs
+++ src/uLearn.Tests/CSharp/ArrayGetLengthValidation/ArrayLengthStyleValidator_should.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
-using uLearn.Extensions;
 using Ulearn.Common.Extensions;
 
 namespace uLearn.CSharp.ArrayGetLengthValidation
