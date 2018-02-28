200764f61b874a5f3ed6d5c54985bb5eab564440
Privalov Sergey <smprivalov@skbkontur.ru>
2018-02-28 17:32:43 +0500

    add return statment validation

---
+++ src/Core/CSharp/BracketValidator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace uLearn.CSharp
+{
+    public class BracketValidator : BaseStyleValidator
+    {
+        protected override IEnumerable<string> ReportAllErrors(SyntaxTree userSolution, SemanticModel semanticModel)
+        {
+            return InspectAll<ReturnStatementSyntax>(userSolution, InspectReturnStatement);
+        }
+
+        private IEnumerable<string> InspectReturnStatement(ReturnStatementSyntax returnStatementSyntax)
+        {
+            var expression = returnStatementSyntax.Expression;
+            if (expression == null)
+                yield break;
+
+            var syntaxKinds = expression.DescendantTokens().Select(x => x.Kind()).ToArray();
+            if (!NeedToCheckExpression(syntaxKinds))
+                yield break;
+
+            if (ContainsRedundantBrackets(syntaxKinds))
+                yield return Report(returnStatementSyntax, "Не следует выделять возвращаемое выражение скобками");
+        }
+
+        private bool NeedToCheckExpression(SyntaxKind[] syntaxKinds) =>
+            syntaxKinds.Length > 2 &&
+            syntaxKinds[0] == SyntaxKind.OpenParenToken &&
+            syntaxKinds[syntaxKinds.Length - 1] == SyntaxKind.CloseParenToken;
+
+        private bool ContainsRedundantBrackets(IEnumerable<SyntaxKind> syntaxKinds)
+        {
+            var pairBracketsCount = 0;
+            var brackets = syntaxKinds
+                .Where(x => x == SyntaxKind.OpenParenToken || x == SyntaxKind.CloseParenToken)
+                .ToArray();
+
+            for (var i = 1; i < brackets.Length - 1; i++)
+            {
+                if (brackets[i] == SyntaxKind.OpenParenToken)
+                    pairBracketsCount++;
+                else
+                    pairBracketsCount--;
+
+                if (pairBracketsCount < 0)
+                    return false;
+            }
+
+            return pairBracketsCount == 0;
+        }
+    }
+}
---
+++ src/uLearn.Tests/CSharp/BracketValidator_should.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace uLearn.CSharp
+{
+    public class BracketValidator_should
+    {
+        private BracketValidator validator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            validator = new BracketValidator();
+        }
+
+        [TestCase(@"int SomeMethod(){return (1);}")]
+        [TestCase(@"bool SomeMethod(){return (true);}")]
+        [TestCase(@"int SomeMethod(){return (-1);}")]
+        [TestCase(@"bool SomeMethod(bool a, bool b){return ((a) && (b));}")]
+        public void warn_return_statment_with_extra_brackets(string code)
+        {
+            FindErrors(code).Should().NotBeNullOrEmpty();
+        }
+
+        [TestCase(@"bool SetX(int x){ return true; }")]
+        [TestCase(@"int SetX(int x, int y){ return (x+y)*2; }")]
+        [TestCase(@"int SetX(int x, int y){ return (x+y)*x*(x-y); }")]
+        [TestCase(@"int SetX(int x, int y){ return 2*(x+y); }")]
+        [TestCase(@"int SetX(int x){ return 1; }")]
+        [TestCase(@"void SetX(int x){ return; }")]
+        public void ignore_correct_return_statment(string code)
+        {
+            FindErrors(code).Should().BeNullOrEmpty();
+        }
+
+        private string FindErrors(string code) =>
+            validator.FindError(code);
+    }
+}
