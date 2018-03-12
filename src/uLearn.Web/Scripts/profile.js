88026c1b33c492f14a4baee8ed7ee7fabf7c89ab
Andrew Gein <andgein@yandex.ru>
2018-03-12 13:24:41 +0500

    Recommentation for russian lastname and firstname

--- src/uLearn.Web/Scripts/profile.js
+++ src/uLearn.Web/Scripts/profile.js
@@ -2,11 +2,23 @@
 	$('.profile__change-email__link').click(function (e) {
 		e.preventDefault();
 
-		var $self = $(this);
-		var $formGroup = $self.closest('.form-group');
-		var $emailInput = $('.profile__email-input');
+        let $self = $(this);
+        let $formGroup = $self.closest('.form-group');
+        let $emailInput = $('.profile__email-input');
 
-		$formGroup.hide();
+        $formGroup.hide();
 		$emailInput.show();
 	});
+	
+	$('.login-and-registration-form .recommend-russian').keyup(function () {
+        let $self = $(this);
+        let val = $self.val();
+		let $validationField = $self.next();
+		if (/[a-z]/i.test(val)) {
+            $validationField.removeClass('field-validation-valid').addClass('field-validation-error');
+            $validationField.text('Мы советуем указывать имя и фамилию на русском языке, а не латиницей')
+		} else {
+            $validationField.removeClass('field-validation-error').addClass('field-validation-valid');
+		}
+    });
 });
