8c1c91658b65ea565af23df1a2fa26662804cd66
Andrew Gein <andgein@yandex.ru>
2018-03-20 11:54:12 +0500

    Custom errors for 404 and 500

---
+++ src/uLearn.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace uLearn.Web.Controllers
+{
+	public class ErrorsController : Controller
+	{
+		public ActionResult Error404()
+		{
+			return View("404");
+		}
+		
+		public ActionResult Error500()
+		{
+			return View("Error");
+		}
+	}
+}
