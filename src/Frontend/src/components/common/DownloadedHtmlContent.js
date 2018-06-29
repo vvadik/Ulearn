2bde31424127b797c1d388b01705e67ae502f114
Andrew Gein <andgein@yandex.ru>
2018-06-29 11:53:10 +0500

    React application: eval included scripts, get title from <title> tag if no window.meta
    + certificates doesn't render yandex.metrika

--- src/Frontend/src/components/common/DownloadedHtmlContent.js
+++ src/Frontend/src/components/common/DownloadedHtmlContent.js
@@ -20,6 +20,16 @@ function getUrlParts(url) {
     };
 }
 
+function safeEval(code) {
+    try {
+        console.log('Eval', code);
+// eslint-disable-next-line
+        eval(code)
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 class DownloadedHtmlContent extends Component {
     BASE_URL = '';
 
@@ -29,7 +39,8 @@ class DownloadedHtmlContent extends Component {
         this.state = {
             loading: true,
             body: '',
-            meta: {}
+            meta: {},
+            links: []
         };
     }
 
@@ -50,9 +61,10 @@ class DownloadedHtmlContent extends Component {
 
     fetchContentFromServer(url) {
         let body = document.getElementsByTagName('body')[0];
-        this.setState({
-            loading: true,
-            body: body.innerHTML
+        this.setState(s => {
+            s.loading = true;
+            s.body = body.innerHTML;
+            return s;
         });
         const self = this;
         fetch(this.BASE_URL + url, {credentials: 'include'})
@@ -66,38 +78,43 @@ class DownloadedHtmlContent extends Component {
             .then(data => {
                 let el = document.createElement('html');
                 el.innerHTML = data;
+                let head = el.getElementsByTagName('head')[0];
                 let body = el.getElementsByTagName('body')[0];
+                let links = head.getElementsByTagName('link');
+                let titles = head.getElementsByTagName('title');
 
                 this.setState({
                     loading: false,
-                    body: body.innerHTML
+                    body: body.innerHTML,
+                    links: links
                 });
 
                 /* Run scripts */
                 (window.documentReadyFunctions || []).forEach(f => f());
 
-                let embeddedScripts = Array.from(body.getElementsByTagName('script')).filter(s => !s.src).map(s => s.innerHTML);
-                embeddedScripts.forEach(s => {
-                    try {
-// eslint-disable-next-line
-                        eval(s)
-                    } catch (e) {
-                        console.error(e);
-                    }
+                window.meta = undefined;
+                let allScriptTags = Array.from(body.getElementsByTagName('script'));
+                /* Eval embedded scripts */
+                let embeddedScripts = allScriptTags.filter(s => !s.src).map(s => s.innerHTML);
+                embeddedScripts.forEach(safeEval);
+                /* Eval remote scripts */
+                allScriptTags.filter(s => s.src).map(s => s.src).forEach(url => {
+                    fetch(url).then(r => r.text()).then(safeEval);
                 });
 
                 /* Scroll to top */
                 window.scrollTo(0, 0);
 
                 let meta = window.meta || {
-                    title : 'Ulearn',
+                    title : titles ? titles[0].innerText : 'Ulearn',
                     description: 'Интерактивные учебные онлайн-курсы по программированию',
                     keywords: '',
                     imageUrl: '',
                 };
                 this.setState(s => {
                     s.loading = false;
-                    s.meta = meta
+                    s.meta = meta;
+                    return s;
                 });
             }).catch(function(error) {
                 /* Retry after timeout */
@@ -118,10 +135,11 @@ class DownloadedHtmlContent extends Component {
     }
 
     getContent() {
-        let meta = this.state.meta;
+        let meta = Object.assign({}, this.state.meta);
+        let links = Object.assign({}, this.state.links);
         return (
             <div>
-                <Meta meta={meta} />
+                <Meta meta={meta} links={links}/>
                 <div dangerouslySetInnerHTML={{__html: this.state.body}}/>
             </div>
         )
@@ -141,6 +159,12 @@ class DownloadedHtmlContent extends Component {
 class Meta extends Component {
     render() {
         let meta = this.props.meta;
+        let links = this.props.links;
+        let renderedLinks = [];
+        for (let i = 0; i < links.length; i++) {
+            let link = links[i];
+            renderedLinks.push(<link rel={link.rel} type={link.type} href={link.href} key={i}/>);
+        }
         return (
             <Helmet>
                 <title>{ meta.title }</title>
@@ -154,6 +178,7 @@ class Meta extends Component {
                 <meta name="description" content={ meta.description }/>
                 <meta name="keywords" content={ meta.keywords }/>
                 <link rel="image_src" href={ meta.imageUrl }/>
+                {renderedLinks}
             </Helmet>
         )
     }
