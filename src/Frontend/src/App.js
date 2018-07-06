ca60df4e5639f9570551a06b3c5da63cd7acb09f
Andrew Gein <andgein@yandex.ru>
2018-07-06 09:48:11 +0500

    Add less-loader and example Header component

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 import AnyPage from "./pages/AnyPage";
 import ErrorBoundary from "./components/common/ErrorBoundary";
 import YandexMetrika from "./components/common/YandexMetrika";
+import Header from "./components/common/Header";
 
 class UlearnApp extends Component {
   render() {
     return (
         <ErrorBoundary>
+            <Header isAuthenticated={false}/>
             <Switch>
                 <Route component={AnyPage} />
             </Switch>
--- src/Frontend/src/components/common/Header.js
+++ src/Frontend/src/components/common/Header.js
@@ -1,48 +1,20 @@
 ﻿import React, { Component } from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import * as PropTypes from 'prop-types'
 
-import './Header.css'
+import './Header.less'
 
-class NavbarIcons extends Component {
+class Header extends Component {
+
     render() {
         return (
-            <button type="button" className="navbar-toggle hide-side-bar-button" data-toggle="collapse" data-target={this.props.target}>
-                <span className="icon-bar"/>
-                <span className="icon-bar"/>
-                <span className="icon-bar"/>
-            </button>
+            <div className="header">
+                123123123123
+            </div>
         )
     }
-}
- 
-class Header extends Component {
-    render() {
-        return (
-            <Navbar fixedTop inverse>
-                <NavbarIcons target=".side-bar"/>
-                <div className="container">
-                    <Navbar.Header>
-                        <NavbarIcons target=".greeting-collapse-class"/>
-                        <Navbar.Brand>
-                            <Link to="/">Ulearn</Link>                            
-                        </Navbar.Brand>
-                        <Nav className="notifications__mobile-nav pull-right visible-xs">
-                            <NavDropdown title="Dropdown">
-                                <a href="Feed/NotificationsTopbarPartial?isMobile=true" />
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Header>
-                    <ul className="notifications__mobile-dropdown notifications__dropdown navbar-collapse collapse visible-xs">
 
-                    </ul>
-                    <div className="greeting-collapse-class navbar-collapse collapse">
-                        <ul className="top-navigation nav navbar-nav">
-                        </ul>
-                    </div>
-                </div>
-            </Navbar>            
-        )
+    static propTypes = {
+        isAuthenticated: PropTypes.bool.isRequired
     }
 }
 
