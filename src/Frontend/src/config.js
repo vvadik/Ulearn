63e880f39c351c0cd8f32d9f2f1a3b34835dc71f
Andrew Gein <andgein@yandex.ru>
2018-07-17 10:14:10 +0500

    Replace api:endpoint for production environments

--- src/Frontend/src/config.js
+++ src/Frontend/src/config.js
@@ -1,3 +1,10 @@
 const config = require("./settings.json");
 
+/* Replace api.endpoint to be ready for production environment */
+const hostname = window.location.hostname;
+if (hostname !== 'localhost' && hostname !== '127.0.0.1') {
+    /* Just add "api." to hostname, i.e. ulearn.me → api.ulearn.me */
+    config.api.endpoint = window.location.protocol + '//' + 'api.' + hostname;
+}
+
 export default config;
