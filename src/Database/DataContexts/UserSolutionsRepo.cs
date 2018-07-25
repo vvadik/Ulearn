8c1a42107222a19c24fdf30b0cc8f3cf57e15624
Andrew Gein <andgein@yandex.ru>
2018-07-25 15:35:35 +0500

    Small optmizations

--- src/Database/DataContexts/UserSolutionsRepo.cs
+++ src/Database/DataContexts/UserSolutionsRepo.cs
@@ -265,7 +265,7 @@ namespace Database.DataContexts
 
 		public int GetAcceptedSolutionsCount(string courseId, Guid slideId)
 		{
-			return GetAllAcceptedSubmissions(courseId, new List<Guid> { slideId }).DistinctBy(x => x.UserId).Count();
+			return GetAllAcceptedSubmissions(courseId, new List<Guid> { slideId }).Select(x => x.UserId).Distinct().Count();
 		}
 
 		public bool IsCheckingSubmissionByUser(string courseId, Guid slideId, string userId, DateTime periodStart, DateTime periodFinish)
