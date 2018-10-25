301ae1a8bdb616596a414cfcdda9bc8381cd0746
Andrew Gein <andgein@yandex.ru>
2018-10-25 19:57:58 +0500

    Remove loggerMiddleware from production environment

--- src/Frontend/src/index.js
+++ src/Frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UlearnApp from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <UlearnApp />
