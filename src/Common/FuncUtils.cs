fc0141c77c0f6e5437b857c58235b4ba0ee811ad
Andrew Gein <andgein@yandex.ru>
2018-11-29 11:18:10 +0500

    A little upgrade of FuncUtils.TrySeveralTimes

--- src/Common/FuncUtils.cs
+++ src/Common/FuncUtils.cs
@@ -21,7 +21,7 @@ namespace Ulearn.Common
 				}
 				catch (Exception e) when (exceptionType.IsInstanceOfType(e))
 				{
-					log.Error("Исключение:", e);
+					log.Error($"Исключение (попытка {tryIndex + 1} из {triesCount}):", e);
 					if (tryIndex >= triesCount - 1)
 						throw;
 					log.Warn($"Попробую ещё раз (попытка {tryIndex + 2} из {triesCount})");
