ba5ba309a6356802f0c5571ff272c6f6e5705030
Andrew Gein <andgein@yandex.ru>
2018-11-30 12:57:09 +0500

    Don't send notification about additional score if it is quals to old one

--- src/uLearn.Web/Controllers/AdminController.cs
+++ src/uLearn.Web/Controllers/AdminController.cs
@@ -1485,7 +1485,7 @@ namespace uLearn.Web.Controllers
 			var scoringGroup = unit.Scoring.Groups[scoringGroupId];
 			if (string.IsNullOrEmpty(score))
 			{
-				await additionalScoresRepo.RemoveAdditionalScores(courseId, unitId, userId, scoringGroupId);
+				await additionalScoresRepo.RemoveAdditionalScores(courseId, unitId, userId, scoringGroupId).ConfigureAwait(false);
 				return Json(new { status = "ok", score = "" });
 			}
 
@@ -1494,8 +1494,10 @@ namespace uLearn.Web.Controllers
 			if (scoreInt < 0 || scoreInt > scoringGroup.MaxAdditionalScore)
 				return Json(new { status = "error", error = $"Баллы должны быть от 0 до {scoringGroup.MaxAdditionalScore}" });
 
-			var additionalScore = await additionalScoresRepo.SetAdditionalScore(courseId, unitId, userId, scoringGroupId, scoreInt, User.Identity.GetUserId());
-			await NotifyAboutAdditionalScore(additionalScore);
+			var (additionalScore, oldScore) = 
+				await additionalScoresRepo.SetAdditionalScore(courseId, unitId, userId, scoringGroupId, scoreInt, User.Identity.GetUserId()).ConfigureAwait(false);
+			if (!oldScore.HasValue || oldScore.Value != scoreInt)
+				await NotifyAboutAdditionalScore(additionalScore).ConfigureAwait(false);
 
 			return Json(new { status = "ok", score = scoreInt });
 		}
@@ -1507,7 +1509,7 @@ namespace uLearn.Web.Controllers
 			if (label == null || label.OwnerId != User.Identity.GetUserId())
 				return Json(new { status = "error", message = "Label not found or not owned by you" });
 
-			await groupsRepo.AddLabelToGroup(groupId, labelId);
+			await groupsRepo.AddLabelToGroup(groupId, labelId).ConfigureAwait(false);
 			return Json(new { status = "ok" });
 		}
 		
