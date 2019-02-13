b7a234dded0f6ffdcc8ec47a8e294c2eabdea047
Maria Paradeeva <gentlepudding@gmail.com>
2019-02-13 15:37:32 +0500

    Added some test. Delete test with throw Error.

--- src/Frontend/src/components/comments/CommentSendForm/wrapRangeWithMarkdown.test.js
+++ src/Frontend/src/components/comments/CommentSendForm/wrapRangeWithMarkdown.test.js
@@ -1,37 +1,21 @@
-import CommentSendForm from "../CommentSendForm";
+import CommentSendForm from "./CommentSendForm";
 
 describe('CommentSendForm.wrapRangeWithMarkdown', () => {
 	test.each([
-		["", 0, 0, "bold", "****", 0, 4],
-		["", 0, 0, "italic", "__", 0, 2],
-		["", 0, 0, "code", "``````", 0, 6],
-		["hello", 1, 2, "italic", "h_e_llo", 1, 4],
-		["hello", 1, 2, "bold", "h**e**llo", 1, 6],
-		["hello", 1, 2, "code", "h```e```llo", 1, 8],
+		["", 0, 0, "**", "****", 2, 2],
+		["", 0, 0, "_", "__", 1, 1],
+		["", 0, 0, "```", "``````", 3, 3],
+		["hello", 0, 0, "_", "__hello", 1, 1],
+		["a", 1, 1, "_", "a__", 2, 2],
+		["hello", 1, 2, "_", "h_e_llo", 1, 4],
+		["abc", 0, 3, "_", "_abc_", 0, 5],
+		["hello", 1, 1, "**", "h****ello", 3, 3],
+		["hello", 1, 2, "**", "h**e**llo", 1, 6],
+		["hello", 1, 2, "```", "h```e```llo", 1, 8],
 	])('%s with selection, %i, %i %s -> %s with selection, %i, %i',
-		(text, selectionStart, selectionEnd, markdown, expectedText, expectedSelectionStart, expectedSelectionEnd) => {
-		const result = CommentSendForm.wrapRangeWithMarkdown(text, {start:selectionStart, end:selectionEnd}, markdown);
+		(text, selectionStart, selectionEnd, markup, expectedText, expectedSelectionStart, expectedSelectionEnd) => {
+		const result = CommentSendForm.wrapRangeWithMarkdown(text, {start:selectionStart, end:selectionEnd}, {markup: markup});
 		expect(result).toEqual({finalText: expectedText, finalSelectionRange:{end: expectedSelectionEnd, start: expectedSelectionStart}});
 	});
-});
-
-
-describe('CommentSendForm.wrapRangeWithMarkdown', () => {
-	test.each([
-		[4, 0, 1, "bold", TypeError],
-		[undefined, 0, 0, TypeError],
-		[null, 0, 0, "bold", TypeError],
-		["", undefined, 0, "bold", TypeError],
-		["", 0, null, "bold", TypeError],
-		["", 0, '', "bold", TypeError],
-		["hello", 2, 1, "bold", RangeError],
-		["hello", -1, 0, "bold", RangeError],
-		["hello", 0, 10, "bold", RangeError],
-		["", 0, 0, undefined, TypeError],
-	])('%s with selection, %i, %i %s -> %o with error',
-		(text, selectionStart, selectionEnd, markdown) => {
-			const result = CommentSendForm.wrapRangeWithMarkdown;
-			expect(() => result(text, {start:selectionStart, end:selectionEnd}, markdown)).toThrowError(Error);
-		});
 });
 
