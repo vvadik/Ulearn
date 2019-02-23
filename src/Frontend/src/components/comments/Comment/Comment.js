df934323bccb296a46e92f96d4ce8b6bca62045a
Maria Paradeeva <gentlepudding@gmail.com>
2019-02-23 11:34:31 +0500

    Added actions and conditions render actions depending on the role.

--- src/Frontend/src/components/comments/Comment/Comment.js
+++ src/Frontend/src/components/comments/Comment/Comment.js
@@ -5,11 +5,13 @@ import * as debounce from "debounce";
 import Icon from "@skbkontur/react-icons";
 import Hint from "@skbkontur/react-ui/components/Hint/Hint";
 import Kebab from "@skbkontur/react-ui/components/Kebab/Kebab";
-import Avatar from "../../common/Avatar/Avatar";
 import MenuItem from "@skbkontur/react-ui/components/MenuItem/MenuItem";
+import Avatar from "../../common/Avatar/Avatar";
 import CommentSendForm from "../CommentSendForm/CommentSendForm";
 
 import styles from "./comment.less";
+
+
 
 class Comment extends Component {
 	constructor(props) {
@@ -17,17 +19,17 @@ class Comment extends Component {
 		this.state = {
 			likesCount: props.likesCount,
 			isLiked: props.isLiked,
-			openForm: false,
-			openEditCommentForm: false,
+			showReplyForm: false,
+			showEditForm: false,
 			isCorrectAnswer: false,
 			isApproved: false,
 			isPinnedToTop: false,
 		};
-		this.debouncedSendChangedLike = debounce(this.sendChangedLike, 300);
+		this.debouncedSendData = debounce(this.sendData, 300);
 	};
 
 	render() {
-		const {children, author, publishDate} = this.props;
+		const {children, author, publishDate, userRoles} = this.props;
 		const {likesCount, isLiked} = this.state;
 		return (
 			<React.Fragment>
@@ -36,7 +38,7 @@ class Comment extends Component {
 					<div className={styles.content}>
 						<div className={styles.header}>
 							<h3 className={styles.author}>{author.visibleName}</h3>
-							<div className={styles.likesBlock}>
+							<div>
 								<button className={styles.likesAction} onClick={this.onLikeChanged}>
 									<Icon name='ThumbUp' color={isLiked ? '#D70C17' : '#A0A0A0'} size={16} />
 								</button>
@@ -45,23 +47,37 @@ class Comment extends Component {
 							{ this.state.isApproved && this.renderHiddenCommentMark() }
 							{ this.state.isCorrectAnswer && this.renderCorrectAnswerMark() }
 							{ this.state.isPinnedToTop && this.renderPinnedToTopMark() }
-							<div className={styles.instructorsActions}>
-								<Kebab positions={['bottom right']} size="large">
-									<MenuItem icon={<Icon.Edit size="small"/>} onClick={this.editComment}>Редактировать</MenuItem>
-									<MenuItem icon={<Icon.EyeClosed size="small"/>} onClick={this.hideComment}>{ this.state.isApproved ? 'Опубликовать' : 'Скрыть' }</MenuItem>
-									<MenuItem icon={<Icon.Delete size="small"/>} onClick={this.deleteComment}>Удалить</MenuItem>
-								</Kebab>
+							{ this.canModerateComments(userRoles, 'editPinAndRemoveComments') &&
+								<div className={styles.instructorsActions}>
+									<Kebab positions={['bottom right']} size="large" disableAnimations={false}>
+										<MenuItem
+											icon={<Icon.Edit size="small"/>}
+											onClick={this.editComment}>
+											Редактировать
+										</MenuItem>
+										<MenuItem
+											icon={<Icon.EyeClosed size="small"/>}
+											onClick={this.hideComment}>
+											{ this.state.isApproved ? 'Опубликовать' : 'Скрыть' }
+										</MenuItem>
+										<MenuItem
+											icon={<Icon.Delete size="small"/>}
+											onClick={this.deleteComment}>
+											Удалить
+										</MenuItem>
+									</Kebab>
+								</div>
+							}
 						</div>
-						</div>
-						<Hint pos="bottom" text={publishDate}>
+						<Hint pos="bottom" text={publishDate} disableAnimations={false} useWrapper={false}>
 							<div className={styles.timeSinceAdded}>
 								{moment(publishDate).fromNow()}
 							</div>
 						</Hint>
-						{ this.state.openEditCommentForm ?
+						{ this.state.showEditForm ?
 							this.renderEditCommentForm() :
 							this.renderComment()}
-						{ this.state.openForm && children }
+						{ this.state.showReplyForm && children }
 					</div>
 				</div>
 			</React.Fragment>
@@ -111,7 +127,8 @@ class Comment extends Component {
 		return (
 			<CommentSendForm
 				text={commentText}
-				onSubmit={(text) => console.log(`Saving '${text}' to api`)}
+				autofocus
+				onSubmit={this.onSubmit}
 				submitTitle={'Сохранить'}
 				onCancel={this.handleEditCancel}
 				sending={sending} />
@@ -119,14 +136,26 @@ class Comment extends Component {
 	}
 
 	renderActions() {
+			const { user, author, userRoles, parentCommentId } = this.props;
+
 		return (
 			<div className={styles.actions}>
-				{this.renderButton(this.openCommentSendForm, 'ArrowCorner1', 'Ответить')}
-				{this.renderButton(this.editComment, 'Edit', 'Редактировать')}
-				{this.renderButton(this.deleteComment, 'Delete', 'Удалить')}
-				{this.renderButton(this.seeSolutions, 'DocumentLite', 'Посмотреть решения')}
-				{this.renderButton(this.markAsCorrectAnswer, 'Star2', this.state.isCorrectAnswer ? 'Снять отметку' : 'Отметить правильным')}
-				{this.renderButton(this.pinnedComment, 'Pin', this.state.isPinnedToTop ? 'Открепить' : 'Закрепить')}
+				{ !parentCommentId && this.renderButton(this.showReplyForm, 'ArrowCorner1', 'Ответить') }
+				{ user.id === author.id &&
+					<div>
+						{this.renderButton(this.editComment, 'Edit', 'Редактировать')}
+						{this.renderButton(this.deleteComment, 'Delete', 'Удалить')}
+					</div>
+				}
+				{ this.canModerateComments(userRoles, 'viewAllStudentsSubmissions') && this.renderLink() }
+				{ this.canModerateComments(userRoles, 'editPinAndRemoveComments') &&
+					<div>
+						{ parentCommentId ? this.renderButton(this.markAsCorrectAnswer, 'Star2',
+							this.state.isCorrectAnswer ? 'Снять отметку' : 'Отметить правильным') :
+						 this.renderButton(this.pinComment, 'Pin',
+							this.state.isPinnedToTop ? 'Открепить' : 'Закрепить') }
+					</div>
+				}
 			</div>
 		)
 	}
@@ -138,12 +167,26 @@ class Comment extends Component {
 		</button>;
 	}
 
+	renderLink() {
+		return (
+			<a href={this.props.url} className={styles.sendAnswer}>
+				<Icon name='DocumentLite' />
+				<span className={styles.linkText}>Посмотреть решения</span>
+			</a>
+		)
+	}
+
+	canModerateComments = (role, accesses) => {
+		return role.isSystemAdministrator || role.courseRole === 'CourseAdmin' ||
+			(role.courseRole === 'Instructor' && role.courseAccesses.includes(accesses)) };
+
 	handleEditCancel = () => {
-		this.setState({ openEditCommentForm: false });
+		this.setState({ showEditForm: false });
 	};
 
 	onLikeChanged = () => {
 		const {isLiked, likesCount} = this.state;
+		const { likeChanged } = this.props;
 
 		if (!isLiked) {
 			this.setState({
@@ -157,16 +200,12 @@ class Comment extends Component {
 			})
 		}
 
-		this.debouncedSendChangedLike(isLiked, likesCount);
+		this.debouncedSendData(likeChanged, likesCount, isLiked);
 	};
 
-	sendChangedLike = (isLiked, likesCount) => {
-		return () => this.props.likeChanged(isLiked, likesCount);
-	};
-
-	openCommentSendForm = () => {
+	showReplyForm = () => {
 		this.setState({
-			openForm: true,
+			showReplyForm: true,
 		});
 	};
 
@@ -178,61 +217,93 @@ class Comment extends Component {
 
 	editComment = () => {
 		this.setState({
-			openEditCommentForm: true,
+			showEditForm: true,
 		});
 	};
 
 	markAsCorrectAnswer = () => {
 		const { isCorrectAnswer } = this.state;
+		const { commentId, markAsCorrectAnswer } = this.props;
 
 		this.setState({
 			isCorrectAnswer: !isCorrectAnswer,
 		});
 
-		this.props.markAsCorrectAnswer(isCorrectAnswer);
+		this.debouncedSendData(markAsCorrectAnswer, commentId, isCorrectAnswer);
 	};
 
-	pinnedComment = () => {
+	pinComment = () => {
 		const { isPinnedToTop } = this.state;
-		const { commentId } = this.props;
+		const { commentId, pinComment } = this.props;
 
 		this.setState({
 			isPinnedToTop: !isPinnedToTop,
 		});
 
-		this.props.pinnedComment(commentId, isPinnedToTop);
+		this.debouncedSendData(pinComment, commentId, isPinnedToTop);
 	};
 
 	hideComment = () => {
 		const { isApproved } = this.state;
+		const { commentId, hideComment } = this.props;
+
 		this.setState({
 			isApproved: !isApproved,
 		});
 
-		this.props.hideComment(isApproved);
+		this.debouncedSendData(hideComment, commentId, isApproved);
+	};
+
+	sendData = (action, value, flag) => {
+		return () => action(value, flag);
+	};
+
+	onSubmit = (e, text) => {
+		e.preventDefault();
+
+		this.props.onSubmit(text);
 	}
 }
 
-const accountModel = PropTypes.shape({
-	id: PropTypes.string,
-	url: PropTypes.string,
+const authorModel = PropTypes.shape({
+	id: PropTypes.string.isRequired,
+	visibleName: PropTypes.string.isRequired,
+	avatarUrl: PropTypes.string,
+});
+
+const userModel = PropTypes.shape({
+	id: PropTypes.string.isRequired,
+	visibleName: PropTypes.string.isRequired,
+	avatarUrl: PropTypes.string,
+});
+
+const userRolesModel = PropTypes.shape({
+	isSystemAdministrator: PropTypes.bool,
+	courseRole: PropTypes.string.isRequired,
+	courseAccesses: PropTypes.arrayOf(PropTypes.string),
 });
 
 Comment.propTypes = {
 	/** Идентифицирует комментарий, с которым работает компонент.
 	 * При изменении идентификатора текст в поле ввода очищается. При сохранении того же идентификатора - текст сохраняется. */
+	user: userModel,
+	userRoles: userRolesModel,
+	author: authorModel,
+	parentCommentId: PropTypes.number,
 	isLiked: PropTypes.bool,
+	isApproved: PropTypes.bool,
 	likesCount: PropTypes.number,
 	likeChanged: PropTypes.func,
 	sendFormOpened: PropTypes.bool,
-	author: accountModel,
 	publishDate: PropTypes.string,
 	commentText: PropTypes.string,
 	showReplyButton: PropTypes.bool,
 	commentId: PropTypes.number,
 	hideComment: PropTypes.func,
 	markAsCorrectAnswer: PropTypes.func,
-	pinnedComment: PropTypes.func,
+	pinComment: PropTypes.func,
+	url: PropTypes.string,
+	onSubmit: PropTypes.func,
 };
 
 export default Comment;
--- src/Frontend/src/components/comments/Comment/Comment.story.js
+++ src/Frontend/src/components/comments/Comment/Comment.story.js
@@ -9,22 +9,129 @@ import CommentSendForm from "../CommentSendForm/CommentSendForm";
 import '../../../common.less';
 
 const nameOnlyUser = {
+	"id": "1",
 	"visibleName": "Garold",
 	"avatarUrl": null,
 };
 
 const userWithAvatar = {
+	"id": "2",
 	"visibleName": "Vasiliy Terkin",
 	"avatarUrl": "https://staff.skbkontur.ru/content/images/default-user-woman.png",
 };
 
-
-class WrapperComment extends React.Component {
+class WrapperForCommentsByRoles extends React.Component {
 	state = {
 		commentsList: [
-			{id: 1, text: "Решать эти задачи можно прямо в браузере, а специальная проверяющая система тут же проверит ваше решение."},
-			{id: 2, text: "После успешного решения некоторых задач вы сможете посмотреть решения других обучающихся. "},
-			{id: 3, text: "Если вам что-то непонятно или вы нашли ошибку на слайде — пишите об этом в комментариях под слайдом."}],
+			{id: 1, text: "Решать эти задачи можно прямо в браузере, а специальная проверяющая система тут же проверит ваше решение.",
+			author: {
+				"id": "11",
+				"visibleName": "Louisa",
+				"avatarUrl": null,
+				"isSystemAdministrator": false,
+				"courseRole": "Student",
+				"courseAccesses": ["nothing"],
+			}},
+			{id: 2, text: "После успешного решения некоторых задач вы сможете посмотреть решения других обучающихся.",
+			author: {
+				"id": "2",
+				"visibleName": "Maria",
+				"avatarUrl": 'https://staff.skbkontur.ru/content/images/default-user-woman.png',
+				"isSystemAdministrator": false,
+				"courseRole": "Student",
+				"courseAccesses": ["nothing"],
+			}},
+			{id: 3, text: "Если вам что-то непонятно или вы нашли ошибку на слайде — пишите об этом в комментариях под слайдом.",
+			author: {
+				"id": "33",
+				"visibleName": "Henry",
+				"avatarUrl": null,
+				"isSystemAdministrator": false,
+				"courseRole": "Instructor",
+				"courseAccesses": ["editPinAndRemoveComments"],
+			}},
+			{id: 4, text: "Вторая часть курса Основы программирования на C#.",
+			author: {
+				"id": "3",
+				"visibleName": "Katelin",
+				"avatarUrl": "https://staff.skbkontur.ru/content/images/default-user-woman.png",
+				"isSystemAdministrator": false,
+				"courseRole": "Instructor",
+				"courseAccesses": ["viewAllStudentsSubmissions"],
+			}},
+			{id: 5, text: "Использование этих методов позволят обойтись без циклов, а следовательно делает код понятнее, короче.",
+			author: {
+				"id": "3",
+				"visibleName": "Robin",
+				"avatarUrl": null,
+				"isSystemAdministrator": false,
+				"courseRole": "Instructor",
+				"courseAccesses": ["editPinAndRemoveComments", "viewAllStudentsSubmissions"],
+			}},
+			{id: 6, text: "Качественные имена, стиль именования, комментарии, разбиение кода на методы.",
+			author: {
+				"id": "3",
+				"visibleName": "Richard",
+				"avatarUrl": "https://staff.skbkontur.ru/content/images/default-user-woman.png",
+				"isSystemAdministrator": false,
+				"courseRole": "CourseAdmin",
+				"courseAccesses": ["editPinAndRemoveComments"],
+			}},
+		]};
+
+	render() {
+		return (
+			<React.Fragment>
+				{ this.state.commentsList.map(comment => {
+					return (
+						<div>
+							<h3>{` Автор.id: ${userWithAvatar.id} / Пользователь.id: ${comment.author.id} / Роль: ${comment.author.courseRole} / Права: ${comment.author.courseAccesses}` }</h3>
+							<Comment
+								url={'https://dev.ulearn.me/Course/BasicProgramming/eb894d4d-5854-4684-898b-5480895685e5?CheckQueueItemId=232301&Group=492'}
+								key={comment.id}
+								commentId={comment.id}
+								author={comment.author}
+								user={userWithAvatar}
+								userRoles={comment.author}
+								commentText={comment.text}
+								publishDate={'2019-01-01T01:37:56'}
+								likesCount={10}
+								isLiked={true}
+								sending={false}
+								showReplyButton={true}
+								deleteComment={this.deleteComment}
+								pinComment={this.pinComment}>
+								<CommentSendForm
+									onSubmit={action('sendComment')}
+									text={comment.text}
+									submitTitle={'Отправить'}
+									commentId={'1'}
+									sending={false}
+									autofocus
+									// editComment={() => this.editComment(comment.id, comment.text)}
+									author={userWithAvatar}/>
+							</Comment>
+						</div>
+					)}
+				)}
+			</React.Fragment>
+		)
+	}
+}
+
+class ReplyToComment extends React.Component {
+	state = {
+		commentsList: [
+			{id: 1, text: "Решать эти задачи можно прямо в браузере, а специальная проверяющая система тут же проверит ваше решение.",
+			author: {
+				"id": "2",
+				"visibleName": "Henry",
+				"avatarUrl": null,
+				"isSystemAdministrator": false,
+				"courseRole": "Instructor",
+				"courseAccesses": ["editPinAndRemoveComments", "viewAllStudentsSubmissions"],
+			}},
+		]
 	};
 
 	render() {
@@ -33,40 +140,36 @@ class WrapperComment extends React.Component {
 			<React.Fragment>
 				{ this.state.commentsList.map(comment => {
 					return (
-					<Comment
-						key={comment.id}
-						commentId={comment.id}
-						author={nameOnlyUser}
-						commentText={comment.text}
-						publishDate={'2019-01-01T01:37:56'}
-						likesCount={10}
-						isLiked={true}
-						sending={false}
-						showReplyButton={true}
-						deleteComment={this.deleteComment}
-						pinnedComment={this.pinnedComment}>
-						<CommentSendForm
-							onSubmit={action('sendComment')}
-							text={comment.text}
-							submitTitle={'Отправить'}
-							commentId={'1'}
+						<Comment
+							url={'https://dev.ulearn.me/Course/BasicProgramming/eb894d4d-5854-4684-898b-5480895685e5?CheckQueueItemId=232301&Group=492'}
+							key={comment.id}
+							parentCommentId={2000}
+							commentId={comment.id}
+							author={comment.author}
+							user={nameOnlyUser}
+							userRoles={comment.author}
+							commentText={comment.text}
+							publishDate={'2019-01-01T01:37:56'}
+							likesCount={10}
+							isLiked={true}
 							sending={false}
-							// editComment={() => this.editComment(comment.id, comment.text)}
-							author={userWithAvatar}/>
-					</Comment>
+							showReplyButton={true}
+							deleteComment={this.deleteComment}>
+							<CommentSendForm
+								// editComment={() => this.editComment(comment.id, comment.text)}
+								onSubmit={action('sendComment')}
+								autofocus
+								text={comment.text}
+								submitTitle={'Отправить'}
+								commentId={'1'}
+								sending={false}
+								author={userWithAvatar}/>
+						</Comment>
+					)}
 				)}
-			)}
 			</React.Fragment>
 		)
 	}
-
-	// editComment = (id, text) => {
-	// 	const changedComment = this.state.commentsList.filter(comment => comment.id === id);
-	// 	const commentListWithoutChangedComment = this.state.commentsList.filter(comment => comment.id !== id);
-	// 	this.setState({
-	// 		commentsList: [...commentListWithoutChangedComment, {...changedComment, text: this.state.text}]
-	// 	})
-	// };
 
 	deleteComment = (commentId) => {
 		const newCommentsList = this.state.commentsList
@@ -87,17 +190,19 @@ class WrapperComment extends React.Component {
 		});
 	};
 
-	pinnedComment = (id) => {
-		const pinnedComment = this.state.commentsList.filter(comment => comment.id === id);
-		const commentListWithoutPinnedComment = this.state.commentsList.filter(comment => comment.id !== id);
-
-		this.setState({
-			commentsList: pinnedComment.concat(commentListWithoutPinnedComment),
-		});
-	};
+	// editComment = (id, text) => {
+	// 	const changedComment = this.state.commentsList.filter(comment => comment.id === id);
+	// 	const commentListWithoutChangedComment = this.state.commentsList.filter(comment => comment.id !== id);
+	// 	this.setState({
+	// 		commentsList: [...commentListWithoutChangedComment, {...changedComment, text: this.state.text}]
+	// 	})
+	// };
 }
 
 storiesOf('Comments/Comment', module)
-	.add('comment with actions', () => (
-		<WrapperComment />
+	.add('список комментариев с ролями пользователей', () => (
+		<WrapperForCommentsByRoles />
+	), { viewport: 'desktop' })
+	.add('ответ на комментарий', () => (
+		<ReplyToComment />
 	),{ viewport: 'desktop' } );
