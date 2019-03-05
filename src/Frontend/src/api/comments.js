151cd94068c88762af5863a392c91188daebda30
Maria Paradeeva <gentlepudding@gmail.com>
2019-03-05 16:01:56 +0500

    Added CommentWrapper and CommentPolicySettings components. Added api request.

--- src/Frontend/src/api/comments.js
+++ src/Frontend/src/api/comments.js
@@ -1,17 +1,17 @@
 import api from "../api/";
 
-export function getComments(courseId, slideId, forInstructors, offset = 0, count = 200) {
-	return api.get("/comments/in/" + courseId + "/" + slideId + "?for_instructors="
-	+ encodeURIComponent(forInstructors) + '&offset=' + encodeURIComponent(offset) +
-		'&count=' + encodeURIComponent(count));
-}
-
-export function addComment(courseId, slideId, text, commentId, isForInstructors) {
-	return api.post("/comments/in/" + courseId + "/" + slideId,
-		api.createRequestParams({
-		'text': text,
-		'reply_to': commentId,
-		'for_instructors': isForInstructors,
-		})
-	);
-}
+export const apiRequests = {
+	getComments(courseId, slideId, isForInstructors) {
+		return api.get("/comments/in/" + courseId + "/" + slideId + "?for_instructors="
+			+ encodeURIComponent(isForInstructors));
+	},
+	addComment(courseId, slideId, text, commentId, isForInstructors) {
+		return api.post("/comments/in/" + courseId + "/" + slideId,
+			api.createRequestParams({
+				'text': text,
+				'reply_to': commentId,
+				'for_instructors': isForInstructors,
+			})
+		);
+	},
+};
