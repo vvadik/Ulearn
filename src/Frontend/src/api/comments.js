e9ccd9449888c4f04a6cf64fae4004da7a559e8c
Maria Paradeeva <gentlepudding@gmail.com>
2019-03-13 16:23:08 +0500

    Changed updateComment method.

--- src/Frontend/src/api/comments.js
+++ src/Frontend/src/api/comments.js
@@ -1,12 +1,16 @@
 import api from "./index";
 
 export function getComments(courseId, slideId, isForInstructors) {
-	return api.get("/comments/in/" + courseId + "/" + slideId + "?for_instructors="
+	return api.get("comments?course_id=" + courseId + "&slide_id=" + slideId + "&for_instructors="
 		+ encodeURIComponent(isForInstructors));
 }
 
-export function addComment(courseId, slideId, text, forInstructors, parentId) {
-	return api.post("/comments/in/" + courseId + "/" + slideId,
+export function getComment(commentId) {
+	return api.get("comments/" + commentId);
+}
+
+export function addComment(courseId, slideId, text, parentId, forInstructors) {
+	return api.post("comments?course_id=" + courseId,
 		api.createRequestParams({
 			"slideId": slideId,
 			"text": text,
@@ -17,25 +21,20 @@ export function addComment(courseId, slideId, text, forInstructors, parentId) {
 }
 
 export function deleteComment(commentId) {
-	return api.delete("/comments/" + commentId);
+	return api.delete("comments/" + commentId);
 }
 
-export function updateComment(commentId, text, isApproved, isPinnedToTop, isCorrectAnswer) {
-	return api.patch("/comments/" + commentId,
-		api.createRequestParams({
-			"text": text,
-			"is_approved": isApproved,
-			"is_pinned_to_top": isPinnedToTop,
-			"is_correct_answer": isCorrectAnswer,
-		})
+export function updateComment(commentId, commentSettings) {
+	return api.patch("comments/" + commentId,
+		api.createRequestParams(commentSettings)
 	);
 }
 
 export function likeComment(commentId) {
-	return api.post("/comments/" + commentId + "/like");
+	return api.post("comments/" + commentId + "/like");
 }
 
 export function dislikeComment(commentId) {
-	return api.delete("/comments/" + commentId + "/like");
+	return api.delete("comments/" + commentId + "/like");
 }
 
