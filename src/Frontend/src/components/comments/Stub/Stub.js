904c8c9401c11b52d238c48bb9a6663486111418
Maria Paradeeva <gentlepudding@gmail.com>
2019-03-14 15:19:19 +0500

    Changed layout and styles in Stub.

--- src/Frontend/src/components/comments/Stub/Stub.js
+++ src/Frontend/src/components/comments/Stub/Stub.js
@@ -1,17 +1,33 @@
-import React, { Component, createContext } from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 import Button from "@skbkontur/react-ui/components/Button/Button";
+import Gapped from "@skbkontur/react-ui/components/Gapped/Gapped";
 
 import styles from './Stub.less';
 
 function Stub(props) {
+	const { courseId, slideId } = props;
+	const urlToRegister =`${window.location.origin}/Account/Register?returnUrl=/Course/${courseId}/${slideId}`;
+	const urlToEnter = `${window.location.origin}/Login?returnUrl=/Course/${courseId}/${slideId}`;
+
 	return (
 		<div className={styles.stub}>
 			<span className={styles.stubText}>Оставлять комментарии могут только зарегистрированные пользователи</span>
-			<Button width={200} use="primary" size="large" type="button" align="center">Зарегистрироваться</Button>
-			<Button use="link">Войти</Button>
+			<Gapped gap={10} vertical>
+			<a href={urlToRegister}>
+				<Button width={200} use="primary" size="large" type="button" align="center">
+				Зарегистрироваться
+			</Button>
+			</a>
+			<a href={urlToEnter}><Button use="link">Войти</Button></a>
+			</Gapped>
 		</div>
 	);
 }
+
+Stub.propTypes = {
+	courseId: PropTypes.string,
+	slideId: PropTypes.string,
+};
 
 export default Stub;
