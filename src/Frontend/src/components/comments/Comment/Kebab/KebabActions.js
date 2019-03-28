61a54bf1b9d18851225cf310f7cd1fa05597fba7
Maria Paradeeva <gentlepudding@gmail.com>
2019-03-28 12:15:25 +0500

    Fixed bug in kebab.

--- src/Frontend/src/components/comments/Comment/Kebab/KebabActions.js
+++ src/Frontend/src/components/comments/Comment/Kebab/KebabActions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { comment, userType, userRoles } from "../../commonPropTypes";
+import uuid from "uuid";
 import Kebab from "@skbkontur/react-ui/components/Kebab/Kebab";
 import MenuItem from "@skbkontur/react-ui/components/MenuItem/MenuItem";
 import Icon from "@skbkontur/react-icons";
@@ -10,55 +11,53 @@ import styles from "./KebabActions.less";
 
 export default function KebabActions(props) {
 	const {user, comment, userRoles, url, canModerateComments, actions, slideType} = props;
+	const canModerate = canModerateComments(userRoles, 'editPinAndRemoveComments');
+	const canDeleteAndEdit = (user.id === comment.author.id || canModerate);
+	const canSeeSubmissions = (slideType === 'exercise' && canModerateComments(userRoles, 'viewAllStudentsSubmissions'));
+	const viewInMobile = document.documentElement.clientWidth < 768;
 
 		return (
 			<div className={styles.instructorsActions}>
-		<Kebab positions={['left top']} size="large" disableAnimations={false}>
-			{(user.id === comment.author.id || canModerateComments(userRoles, 'editPinAndRemoveComments')) &&
-				<>
-					<MenuItem
-						icon={<Icon.Delete size="small" />}
-						onClick={() => actions.handleDeleteComment(comment.id)}>
-						Удалить
-					</MenuItem>
-					<Mobile>
+				<Kebab positions={['left top']} size="large" disableAnimations={true}>
+					{canDeleteAndEdit &&
 						<MenuItem
-							icon={<Icon.Edit size="small" />}
-							onClick={() => actions.handleShowEditForm(comment.id)}>
-							Редактировать
-						</MenuItem>
-					</Mobile>
-				</>}
-			{(slideType === 'exercise' && canModerateComments(userRoles, 'viewAllStudentsSubmissions')) &&
-				<Mobile>
-					<MenuItem
-						href={url}
-						icon={<Icon name='DocumentLite' size="small" />}>
-						Посмотеть решения
-					</MenuItem>
-				</Mobile>}
-			{canModerateComments(userRoles, 'editPinAndRemoveComments') ?
-				<>
-				<MenuItem
-					icon={<Icon.EyeClosed size="small" />}
-					onClick={() => actions.handleApprovedMark(comment.id, comment.isApproved)}>
-					{!comment.isApproved ? 'Опубликовать' : 'Скрыть'}
-				</MenuItem>
-				{comment.parentCommentId ?
-				<MenuItem
-					onClick={() => actions.handleCorrectAnswerMark(comment.id, comment.isCorrectAnswer)}
-					icon={<Icon name='Ok' size="small" />}>
-					{comment.isCorrectAnswer ? 'Снять отметку' : 'Отметить правильным'}
-				</MenuItem> :
-				<MenuItem
-					onClick={() => actions.handlePinnedToTopMark(comment.id, comment.isPinnedToTop)}
-					icon={<Icon name='Pin' size="small" />}>
-					{comment.isPinnedToTop ? 'Открепить' : 'Закрепить'}
-				</MenuItem>}
-				</>
-			: null}
-		</Kebab>
-	</div>
+							icon={<Icon.Delete size="small" />}
+							onClick={() => actions.handleDeleteComment(comment.id)}>
+							Удалить
+						</MenuItem>}
+					{(canDeleteAndEdit && viewInMobile) &&
+							<MenuItem
+								icon={<Icon.Edit size="small" />}
+								onClick={() => actions.handleShowEditForm(comment.id)}>
+								Редактировать
+							</MenuItem>}
+					{(canSeeSubmissions && viewInMobile) &&
+							<MenuItem
+								href={url}
+								icon={<Icon name='DocumentLite' size="small" />}>
+								Посмотеть решения
+							</MenuItem>}
+					{canModerate &&
+						<MenuItem
+							icon={<Icon.EyeClosed size="small" />}
+							onClick={() => actions.handleApprovedMark(comment.id, comment.isApproved)}>
+							{!comment.isApproved ? 'Опубликовать' : 'Скрыть'}
+						</MenuItem>}
+						{canModerate ?
+							comment.parentCommentId ?
+							<MenuItem
+								onClick={() => actions.handleCorrectAnswerMark(comment.id, comment.isCorrectAnswer)}
+								icon={<Icon name='Ok' size="small" />}>
+								{comment.isCorrectAnswer ? 'Снять отметку' : 'Отметить правильным'}
+							</MenuItem> :
+							<MenuItem
+								onClick={() => actions.handlePinnedToTopMark(comment.id, comment.isPinnedToTop)}
+								icon={<Icon name='Pin' size="small" />}>
+								{comment.isPinnedToTop ? 'Открепить' : 'Закрепить'}
+							</MenuItem>
+					: null}
+				</Kebab>
+			</div>
 		)
 }
 
