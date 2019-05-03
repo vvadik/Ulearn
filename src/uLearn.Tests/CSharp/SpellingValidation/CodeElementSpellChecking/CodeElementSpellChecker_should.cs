7288fa551d50a5e0f17bd73d199b9a86ca6aff5c
PhonkX <stas74u@mail.ru>
2019-05-03 21:03:31 +0500

    add tests for CodeElementSpellChecker

---
+++ src/uLearn.Tests/CSharp/SpellingValidation/CodeElementSpellChecking/CodeElementSpellChecker_should.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using uLearn.CSharp.Validators.SpellingValidator;
+
+namespace uLearn.CSharp.SpellingValidation.CodeElementSpellChecking
+{
+	[TestFixture]
+	public class CodeElementSpellChecker_should
+	{
+		private readonly CodeElementSpellChecker spellChecker = new CodeElementSpellChecker();
+		
+		[TestCase("wildWildWest", ExpectedResult = false)]
+		[TestCase("Filename", ExpectedResult = false)]
+		[TestCase("Heatmap", ExpectedResult = false)]
+		[TestCase("Heatmapdata", ExpectedResult = false)]
+		[TestCase("wildWildEwst", ExpectedResult = true)]
+		[TestCase("filenaem", ExpectedResult = false)] // разбивается на f, ile, na, em, которые (не беря во внимание f) есть в латинском словаре
+		[TestCase("sw", ExpectedResult = false)]
+		[TestCase("stri", "String", ExpectedResult = false)]
+		[TestCase("csl", "CSharpLanguage", ExpectedResult = false)]
+		[TestCase("Heapify", "CSharpLanguage", ExpectedResult = false)]
+		[TestCase("strings", "CSharpLanguage", ExpectedResult = false)]
+		[TestCase("xlabel", "CSharpLanguage", ExpectedResult = false)]
+		public bool Test_CheckIdentifierNameForSpellingErrors(string identifierName, string type = null)
+		{
+			return spellChecker.CheckIdentifierNameForSpellingErrors(identifierName, type);
+		}
+	}
+}
