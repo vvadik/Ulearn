a3700bb4cb7ccb8a45d3f50c60dd8d684c0fa9c8
Maria Paradeeva <gentlepudding@gmail.com>
2019-06-18 21:01:29 +0500

    Style changes

--- src/Frontend/src/components/comments/Comment/Marks/Marks.js
+++ src/Frontend/src/components/comments/Comment/Marks/Marks.js
@@ -4,11 +4,9 @@ import { comment, group } from "../../commonPropTypes";
 import Icon from "@skbkontur/react-icons";
 import TooltipMenu from "@skbkontur/react-ui/components/TooltipMenu/TooltipMenu";
 import MenuItem from "@skbkontur/react-ui/components/MenuItem/MenuItem";
-import MenuSeparator from "@skbkontur/react-ui/components/MenuSeparator/MenuSeparator";
 import MenuHeader from "@skbkontur/react-ui/components/MenuHeader/MenuHeader";
 
 import styles from "./Marks.less";
-import Hint from "@skbkontur/react-ui/components/Hint/Hint";
 
 export default function Marks({courseId, comment, canViewStudentsGroup, authorGroups}) {
 	const windowUrl = `${window.location.origin}/${courseId}`;
@@ -49,49 +47,56 @@ const PinnedToTopMark = () => (
 	</div>
 );
 
-const GroupMark = ({url, groups}) => (
+export function GroupMark({url, groups}) {
+	const groupsNumber = groups.length;
+
+	return (
 		<>
 			<div className={styles.visibleOnDesktopAndTablet}>
 				<div className={styles.groupList}>
-					{groups.map(group =>
-						<Hint pos="right middle" text={group.name}>
-							<div
-								key={group.id}
+					{groupsNumber < 3 ?
+						groups.map(group =>
+							<div key={group.id}
 								 className={`${styles.mark} ${styles.group} ${group.isArchived && styles.archiveGroup}`}>
-									<Icon name="People" size={15} />
-									<a href={group.apiUrl && `${url}${group.apiUrl}`}
-									   className={`${styles.text} ${styles.groupName}`}>
-										{group.name}
-									</a>
-							</div>
-						</Hint>)}
+								<Icon name="People" size={15} />
+								<a href={group.apiUrl && `${url}${group.apiUrl}`}
+								   className={`${styles.text} ${styles.groupName}`}>
+									{group.name}
+								</a>
+							</div>) :
+						<GroupsMenu url={url} groups={groups} />}
 				</div>
 			</div>
 			<div className={styles.visibleOnPhone}>
-				<TooltipMenu
-					menuWidth="150px"
-					positions={["bottom right"]}
-					caption={<div className={styles.groupMarkOnPhone}>
-						<Icon name="People" color="#fff" size={15} />
-					</div>}>
-					<MenuHeader>Группы</MenuHeader>
-					<MenuSeparator />
-					{groups.map(group => !group.isArchived &&
-						<MenuItem
-							key={group.id}
-							href={group.apiUrl && `${url}${group.apiUrl}`}>
-							{group.name}
-						</MenuItem>)}
-					<MenuSeparator />
-					{groups.map(group => group.isArchived &&
-						<MenuItem
-							key={group.id}
-							href={group.apiUrl && `${url}${group.apiUrl}`}>
-							{group.name}
-						</MenuItem>)}
-				</TooltipMenu>
+				<GroupsMenu url={url} groups={groups} />
 			</div>
 		</>
+	)
+};
+
+const GroupsMenu = ({url, groups, label = "Все группы"}) => (
+	<TooltipMenu
+		menuWidth="150px"
+		positions={["bottom right"]}
+		caption={
+			<div className={styles.groupMarkOnPhone}>
+				<Icon name="People" color="#fff" size={15} />
+				{label}
+			</div>}>
+		<MenuHeader>Группы</MenuHeader>
+		{groups.map(group => !group.isArchived &&
+			<MenuItem
+				key={group.id}
+				href={group.apiUrl && `${url}${group.apiUrl}`}>
+				{group.name}
+			</MenuItem>)}
+		{groups.map(group => group.isArchived &&
+			<MenuItem
+				key={group.id}
+				href={group.apiUrl && `${url}${group.apiUrl}`}>
+				{group.name}
+			</MenuItem>)}
+	</TooltipMenu>
 );
 
 Marks.propTypes = {
