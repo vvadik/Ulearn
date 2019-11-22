faf7bbca346a76ad6e9ea5de258fd8076759244a
Rozentor <36932390+Rozentor@users.noreply.github.com>
2019-11-22 18:19:20 +0500

    Added accepted solutions to routes

--- src/Frontend/src/consts/routes.js
+++ src/Frontend/src/consts/routes.js
@@ -1,6 +1,7 @@
 export const coursePath = "course";
 export const flashcards = "flashcards";
 export const courseStatistics = "/analytics/courseStatistics";
+export const acceptedSolutions = "acceptedsolutions";
 
 export function constructPathToSlide(courseId, slideId) {
 	return `/${ coursePath }/${ courseId }/${ slideId }`;
