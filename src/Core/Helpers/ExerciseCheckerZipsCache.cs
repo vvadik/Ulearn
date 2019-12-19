397ce57e7517f2282352fa21170fee4afc163539
vorkulsky <vorkulsky@skbkontur.ru>
2019-12-19 17:54:58 +0500

    Write to file operation is not necessary

--- src/Core/Helpers/ExerciseCheckerZipsCache.cs
+++ src/Core/Helpers/ExerciseCheckerZipsCache.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using Ionic.Zip;
 using log4net;
@@ -39,32 +40,57 @@ namespace Ulearn.Core.Helpers
 			var slide = zipBuilder.Slide;
 			var courseDirectory = cacheDirectory.GetSubdirectory(courseId);
 			var zipFile = courseDirectory.GetFile($"{slide.Id}.zip");
+			byte[] zipBytes = null;
 			if (!zipFile.Exists)
 			{
 				courseDirectory.EnsureExists();
 				log.Info($"Собираю zip-архив с упражнением: курс {courseId}, слайд «{slide.Title}» ({slide.Id}), файл {zipFile.FullName}");
-				var bytes = zipBuilder.GetZipBytesForChecker();
-				using(var fileStream = zipFile.OpenWrite())
-					fileStream.Write(bytes, 0, bytes.Length);
+				zipBytes = zipBuilder.GetZipBytesForChecker();
+				SaveFileOnDisk(zipFile, zipBytes);
 			}
 
-			return AddUserCodeToZip(zipFile, userCodeFilePath, userCodeFileContent, courseId, slide);
+			MemoryStream zipMemoryStream;
+			if (zipBytes != null)
+				zipMemoryStream = new MemoryStream(zipBytes);
+			else
+			{
+				using (var stream = zipFile.Open(FileMode.Open, FileAccess.Read, FileShare.Read))
+				{
+					zipMemoryStream = new MemoryStream();
+					stream.CopyTo(zipMemoryStream);
+				}
+				zipMemoryStream.Position = 0;
+			}
+			return AddUserCodeToZip(zipMemoryStream, userCodeFilePath, userCodeFileContent, courseId, slide);
 		}
 
-		private static byte[] AddUserCodeToZip(FileInfo zipFile, string userCodeFilePath, byte[] userCodeFileContent, string courseId, Slide slide)
+		private static void SaveFileOnDisk(FileInfo zipFile, byte[] zipBytes)
+		{
+			try
+			{
+				using (var fileStream = zipFile.Open(FileMode.CreateNew, FileAccess.Write, FileShare.None))
+					fileStream.Write(zipBytes, 0, zipBytes.Length);
+			}
+			catch (Exception ex)
+			{
+				// ignored
+			}
+		}
+
+		private static byte[] AddUserCodeToZip(MemoryStream inputStream, string userCodeFilePath, byte[] userCodeFileContent, string courseId, Slide slide)
 		{
 			var sw = Stopwatch.StartNew();
-			var ms = new MemoryStream();
-			using (var zip = ZipFile.Read(zipFile.Open(FileMode.Open, FileAccess.Read, FileShare.Read)))
+			var resultStream = new MemoryStream();
+			using (var zip = ZipFile.Read(inputStream))
 			{
 				if(zip.ContainsEntry(userCodeFilePath))
 					zip.UpdateEntry(userCodeFilePath, userCodeFileContent);
 				else
 					zip.AddEntry(userCodeFilePath, userCodeFileContent);
-				zip.Save(ms);
+				zip.Save(resultStream);
 			}
-			var result = ms.ToArray();
-			log.Info($"Добавил код студента в zip-архив с упражнением: курс {courseId}, слайд «{slide.Title}» ({slide.Id}), файл {zipFile.FullName} elapsed {sw.ElapsedMilliseconds} ms");
+			var result = resultStream.ToArray();
+			log.Info($"Добавил код студента в zip-архив с упражнением: курс {courseId}, слайд «{slide.Title}» ({slide.Id}) elapsed {sw.ElapsedMilliseconds} ms");
 			return result;
 		}
 
