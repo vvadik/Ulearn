6f3e094cfd8de6d2e4671be0aa0acd4480d72804
Rozentor <36932390+Rozentor@users.noreply.github.com>
2020-03-10 15:01:57 +0500

    Added DomElement.remove() polyfill

--- src/Frontend/config/polyfills.js
+++ src/Frontend/config/polyfills.js
@@ -7,3 +7,20 @@ import 'whatwg-fetch';
 if (process.env.NODE_ENV === 'test') {
 	require('raf').polyfill(global);
 }
+
+// Adding Element.remove() for DOM elements, suggested in the Mozilla documentation. https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+(function (arr) {
+	arr.forEach(function (item) {
+		if (item.hasOwnProperty('remove')) {
+			return;
+		}
+		Object.defineProperty(item, 'remove', {
+			configurable: true,
+			enumerable: true,
+			writable: true,
+			value: function remove() {
+				this.parentNode.removeChild(this);
+			}
+		});
+	});
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
