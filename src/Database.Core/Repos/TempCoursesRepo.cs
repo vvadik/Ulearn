34a40c5b528c50f3355ccb8ab5e2835b45c0a4b3
eliyy4hu <eliyy4hu@outlook.com>
2020-04-19 21:37:32 +0500

    add TempCourse repos

---
+++ src/Database.Core/Repos/TempCoursesRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Database.Models;
+using System.Linq;
+
+namespace Database.Repos
+{
+	public class TempCoursesRepo
+	{
+		private readonly UlearnDb db;
+
+
+		public TempCoursesRepo(UlearnDb db)
+		{
+			this.db = db;
+		}
+
+		public TempCourse Find(string courseId)
+		{
+			return db.TempCourses.SingleOrDefault(course => course.CourseId == courseId);
+		}
+
+		public async Task<TempCourse> AddTempCourse(string courseId, string authorId)
+		{
+			var tempCourse = new TempCourse()
+			{
+				CourseId = courseId,
+				AuthorId = authorId,
+				LoadingTime = DateTime.Now
+			};
+			db.TempCourses.Add(tempCourse);
+			await db.SaveChangesAsync();
+			return tempCourse;
+		}
+
+		public async Task UpdateTempCourseLoadingTime(string courseId)
+		{
+			var course = db.TempCourses.Find(courseId);
+			if (course == null)
+				return;
+
+			course.LoadingTime = DateTime.Now;
+			await db.SaveChangesAsync();
+		}
+	}
+}
