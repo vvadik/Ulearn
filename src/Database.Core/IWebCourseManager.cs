fc4143eb6f5fae18175248dd45cd6d416bd10bbe
eliyy4hu <eliyy4hu@outlook.com>
2020-05-03 16:28:11 +0500

    add CopyTempCourse to courseManager

--- src/Database.Core/IWebCourseManager.cs
+++ src/Database.Core/IWebCourseManager.cs
@@ -36,6 +36,7 @@ namespace Database
 		void ReleaseCourse(string courseId);
 		void WaitWhileCourseIsLocked(string courseId);
 		void MoveCourse(Course course, DirectoryInfo sourceDirectory, DirectoryInfo destinationDirectory);
+		void CopyTempCourse(Course course, DirectoryInfo sourceDirectory, DirectoryInfo destinationDirectory);
 		void ReloadCourse(string courseId);
 	}
 }
