f3c9d8c4be29b39dbae7426a2725bcf9a4739cf0
hukopo <hukopo@yandex.ru>
2020-06-10 02:16:11 +0500

    Теперь логгер меняет log4net.Config и пишет логи рядом с exe

--- src/CourseToolHotReloader/Log/Logger.cs
+++ src/CourseToolHotReloader/Log/Logger.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Xml;
 using log4net;
 using log4net.Config;
 
@@ -15,9 +18,31 @@ namespace CourseToolHotReloader.Log
 		{
 			var repository = LogManager.GetRepository(Assembly.GetCallingAssembly());
 
-			var fileInfo = new FileInfo($"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\log4net.config");
+			var path = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\log4net.config";
+
+			ChangeLogsOutputDirectory(path);
+
+			var fileInfo = new FileInfo(path);
 
 			XmlConfigurator.Configure(repository, fileInfo);
+		}
+
+		private static void ChangeLogsOutputDirectory(string path)
+		{
+			var xDoc = new XmlDocument();
+			xDoc.Load(path);
+			
+			var xRoot = xDoc.DocumentElement;
+			foreach (var xnode in xRoot.Cast<XmlNode>().Where(xnode => xnode.Name.Equals("appender", StringComparison.OrdinalIgnoreCase)))
+				xnode["file"].SetAttribute("value", NewPath(xnode["file"].Attributes["value"].Value));
+			
+			xDoc.Save(path);
+		}
+
+		private static string NewPath(string oldPath)
+		{
+			var fileName= Path.GetFileName(oldPath);
+			return $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\logs\\{fileName}";
 		}
 	}
 }
