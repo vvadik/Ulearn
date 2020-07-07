73d1dff421559c50a702f54714d06cb1c46cfe3f
Rozentor <36932390+Rozentor@users.noreply.github.com>
2020-07-07 23:45:31 +0500

    Implemented student mode toggle + refactor in course js
    Added showHiddenBlock prop to slide to determine if we need to show hidden blocks

--- src/Frontend/src/components/course/Course/Course.js
+++ src/Frontend/src/components/course/Course/Course.js
@@ -13,10 +13,11 @@ import { UrlError } from "src/components/common/Error/NotFoundErrorBoundary";
 import Error404 from "src/components/common/Error/Error404";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import { Edit } from "icons";
+import { Edit, } from "icons";
+import { Toggle } from "ui";
 
 import { flashcards, constructPathToSlide } from 'src/consts/routes';
-import { SLIDETYPE } from 'src/consts/general';
+import { ROLES, SLIDETYPE } from 'src/consts/general';
 import { SCORING_GROUP_IDS } from 'src/consts/scoringGroup';
 
 import classnames from 'classnames';
@@ -42,6 +43,7 @@ class Course extends Component {
 			currentSlideInfo: null,
 			currentCourseId: null,
 			navigationOpened: this.props.navigationOpened,
+			showForStudents: false,
 			meta: {
 				title: 'Ulearn',
 				description: 'Интерактивные учебные онлайн-курсы по программированию',
@@ -73,7 +75,7 @@ class Course extends Component {
 
 	componentDidUpdate(prevProps, prevState) {
 		const { loadUserProgress, courseId, loadCourse, user, courseInfo, } = this.props;
-		const { title } = this.state;
+		const { title, currentSlideInfo, showForStudents, currentSlideId, } = this.state;
 		const { isAuthenticated } = user;
 
 		if(isAuthenticated !== prevProps.user.isAuthenticated) {
@@ -84,6 +86,13 @@ class Course extends Component {
 
 		if(title !== prevState.title) {
 			this.updateWindowMeta(title, courseInfo.title);
+		}
+		if((currentSlideId !== prevState.currentSlideId || showForStudents !== prevState.showForStudents) && currentSlideInfo.current.type === SLIDETYPE.exercise) {
+			if(showForStudents) {
+				this.props.history.push('?version=-1'); //prevent showing task solution
+			} else if(this.props.history.location.search === '?version=-1') {
+				this.props.history.replace();
+			}
 		}
 	}
 
@@ -197,12 +206,13 @@ class Course extends Component {
 	}
 
 	renderSlide() {
-		const { isNavMenuVisible, progress, } = this.props;
-		const { currentSlideInfo, currentSlideId, currentCourseId, Page, title, } = this.state;
+		const { isNavMenuVisible, progress, user, courseId, } = this.props;
+		const { currentSlideInfo, currentSlideId, currentCourseId, Page, title, showForStudents, } = this.state;
 
 		const wrapperClassName = classnames(
 			styles.rootWrapper,
 			{ [styles.withoutNavigation]: !isNavMenuVisible }, // TODO remove isNavMenuVisible flag
+			{ [styles.forStudents]: showForStudents },
 		);
 
 		const slideInfo = currentSlideInfo
@@ -216,16 +226,18 @@ class Course extends Component {
 			}
 			: null;
 
+		const { isSystemAdministrator, accessesByCourse, roleByCourse } = user;
+		const courseAccesses = accessesByCourse[courseId] ? accessesByCourse[courseId] : [];
+		const courseRole = roleByCourse[courseId] ? roleByCourse[courseId] : '';
+		const userRoles = { isSystemAdministrator, courseRole, courseAccesses, };
+
 		return (
 			<main className={ wrapperClassName }>
+				{ this.isInstructor(userRoles) && this.renderShowForStudentToggle() }
 				{ isNavMenuVisible && title &&
 				<h1 className={ styles.title }>
 					{ title }
-					{ slideInfo && slideInfo.gitEditLink &&
-					<a className={ styles.gitEditLink } rel='noopener noreferrer' target='_blank'
-					   href={ slideInfo.gitEditLink }>
-						<Edit/>
-					</a> }
+					{ slideInfo && slideInfo.gitEditLink && this.renderGitEditLink(slideInfo) }
 				</h1> }
 				<div className={ styles.slide }>
 					{
@@ -233,6 +245,7 @@ class Course extends Component {
 							? <Slide
 								slideId={ currentSlideId }
 								courseId={ currentCourseId }
+								showHiddenBlocks={ !showForStudents }
 							/>
 							: <BlocksWrapper score={ score }>
 								<Page match={ this.props.match }/>
@@ -241,10 +254,47 @@ class Course extends Component {
 
 				</div>
 				{ currentSlideInfo && isNavMenuVisible && this.renderNavigationButtons(currentSlideInfo) }
-				{ currentSlideInfo && isNavMenuVisible && this.renderComments(currentSlideInfo.current) }
+				{ currentSlideInfo && isNavMenuVisible && this.renderComments(currentSlideInfo.current, userRoles) }
 				{ isNavMenuVisible && this.renderFooter() }
 			</main>
 		);
+	}
+
+	renderGitEditLink = (slideInfo) => {
+		return (
+			<a className={ styles.gitEditLink } rel='noopener noreferrer' target='_blank'
+			   href={ slideInfo.gitEditLink }>
+				<Edit/>
+			</a>
+		);
+	}
+
+	isCourseAdmin(userRoles) {
+		return userRoles.isSystemAdministrator ||
+			userRoles.courseRole === ROLES.courseAdmin;
+	}
+
+	isInstructor(userRoles) {
+		return this.isCourseAdmin(userRoles) ||
+			userRoles.courseRole === ROLES.instructor;
+	}
+
+	renderShowForStudentToggle = () => {
+		return (
+			<div className={ styles.toggleContainer }>
+				<span className={ styles.toggleText }> Режим студента </span>
+				<Toggle
+					checked={ this.state.showForStudents }
+					onValueChange={ this.showForStudentToggleChanged }
+				/>
+			</div>
+		)
+	}
+
+	showForStudentToggleChanged = (value) => {
+		this.setState({
+			showForStudents: value,
+		})
 	}
 
 	renderNavigationButtons(slideInfo) {
@@ -291,12 +341,8 @@ class Course extends Component {
 		this.props.history.push(href);
 	};
 
-	renderComments(currentSlide) {
+	renderComments(currentSlide, userRoles,) {
 		const { user, courseId, isSlideReady, } = this.props;
-		const { isSystemAdministrator, accessesByCourse, roleByCourse } = user;
-		const courseAccesses = accessesByCourse[courseId] ? accessesByCourse[courseId] : [];
-		const courseRole = roleByCourse[courseId] ? roleByCourse[courseId] : '';
-		const userRoles = { isSystemAdministrator, courseRole, courseAccesses, };
 
 		return (
 			<BlocksWrapper className={ styles.commentsWrapper }>
