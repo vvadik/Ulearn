eb06bca1f5d04bc0dab8f8e063bc02768b950d1f
vorkulsky <vorkulsky@skbkontur.ru>
2020-09-17 16:48:55 +0500

    AddStyleErrorSettings Database.Core migration

--- src/Database.Core/Migrations/20200917114641_AddStyleErrorSettings.Designer.cs
+++ src/Database.Core/Migrations/20200917114641_AddStyleErrorSettings.Designer.cs
@@ -4,14 +4,16 @@ using Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Database.Migrations
 {
     [DbContext(typeof(UlearnDb))]
-    partial class UlearnDbModelSnapshot : ModelSnapshot
+    [Migration("20200917114641_AddStyleErrorSettings")]
+    partial class AddStyleErrorSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Database.Core/Migrations/20200917114641_AddStyleErrorSettings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class AddStyleErrorSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StyleErrorSettings",
+                columns: table => new
+                {
+                    ErrorType = table.Column<int>(nullable: false),
+                    IsEnabled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StyleErrorSettings", x => x.ErrorType);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StyleErrorSettings");
+        }
+    }
+}
