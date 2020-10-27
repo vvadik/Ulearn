27cf64af0d4259e5a1be36bb6cb54ae5a299ce11
Rozentor <36932390+Rozentor@users.noreply.github.com>
2020-10-28 00:59:53 +0500

    changed exercise consts to lower case

--- src/Frontend/src/consts/exercise.js
+++ src/Frontend/src/consts/exercise.js
@@ -1,14 +1,14 @@
 export const processStatuses = {
-	SERVER_ERROR: 'serverError', // Ошибка на сервере или статус SandoxError в чеккере
+	SERVER_ERROR: 'servererror', // Ошибка на сервере или статус SandoxError в чеккере
 	DONE: 'done', // Проверена
 	WAITING: 'waiting', // В очереди на проверку
 	RUNNING: 'running', // Проверяется
-	WAITING_TIME_LIMIT_EXCEEDED: 'waitingTimeLimitExceeded', // Задача выкинута из очереди, потому что её никто не взял на проверку
+	WAITING_TIME_LIMIT_EXCEEDED: 'waitingtimelimitexceeded', // Задача выкинута из очереди, потому что её никто не взял на проверку
 };
 
 export const checkingResults = {
-	NOT_CHECKED: 'notChecked',
-	COMPILATION_ERROR: 'compilationError',
-	RIGHT_ANSWER: 'rightAnswer',
-	WRONG_ANSWER: 'wrongAnswer',
+	NOT_CHECKED: 'notchecked',
+	COMPILATION_ERROR: 'compilationerror',
+	RIGHT_ANSWER: 'rightanswer',
+	WRONG_ANSWER: 'wronganswer',
 };
