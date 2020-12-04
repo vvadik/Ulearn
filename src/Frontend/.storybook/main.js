a3d35373bc08411b0abc7f8c841f8948acacd41d
Rozentor <36932390+Rozentor@users.noreply.github.com>
2020-12-04 15:08:25 +0500

    Removed sb addons which are added by addon-essentials

--- src/Frontend/.storybook/main.js
+++ src/Frontend/.storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
 	stories: ['../src/**/**.story.@(js|jsx|tsx)'],
 	addons: [
 		'@storybook/addon-essentials',
-		'@storybook/addon-controls',
 	],
 	webpackFinal: async (config, { configType }) => {
 		config = merge(base, config);
