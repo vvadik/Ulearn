09b545633c703a4defe4b7d16be50bbfc93a42a2
vorkulsky <vorkulsky@skbkontur.ru>
2020-12-29 13:33:50 +0500

    MsBuildLocationHelper buildTools path

--- src/Core/Helpers/MsBuildLocationHelper.cs
+++ src/Core/Helpers/MsBuildLocationHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.IO;
 using Microsoft.Build.Utilities;
+using Vostok.Logging.Abstractions;
 
 namespace Ulearn.Core.Helpers
 {
 	public static class MsBuildLocationHelper
 	{
 		private static string pathToMsBuild;
+		private static ILog log => LogProvider.Get().ForContext(typeof(MsBuildLocationHelper));
 
 		public static void InitPathToMsBuild()
 		{
@@ -16,8 +18,17 @@ namespace Ulearn.Core.Helpers
 			var path = ToolLocationHelper.GetPathToBuildTools(version);
 			var assembly = Path.GetDirectoryName(System.Reflection.Assembly.GetAssembly(typeof(ProjModifier)).Location);
 			if (path == assembly)
-				Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe");
+			{
+				// TODO использовать PowerShell module to locate MSBuild: vssetup.powershell. Get-VSSetupInstance
+				var buildToolsMsBuildDirectory = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin";
+				var vsCommunityMsBuildDirectory = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin";
+				if (Directory.Exists(buildToolsMsBuildDirectory))
+					Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", Path.Combine(buildToolsMsBuildDirectory, "MSBuild.exe"));
+				else if (Directory.Exists(vsCommunityMsBuildDirectory))
+					Environment.SetEnvironmentVariable("MSBUILD_EXE_PATH", Path.Combine(vsCommunityMsBuildDirectory, "MSBuild.exe"));
+			}
 			pathToMsBuild = ToolLocationHelper.GetPathToBuildTools(version);
+			log.Info("PathToMsBuild {PathToMsBuild}", pathToMsBuild);
 		}
 	}
 }
