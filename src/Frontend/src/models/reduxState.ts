1026e8858fc11a39218c49dde663e85376a47772
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-01-13 14:02:10 +0500

    updated models

--- src/Frontend/src/models/reduxState.ts
+++ src/Frontend/src/models/reduxState.ts
@@ -1,7 +1,7 @@
 import { SlideUserProgress } from "./userProgress";
 import { CourseInfo } from "./course";
-import { SubmissionInfo } from "./exercise";
-import {CourseAccessType, CourseRoleType, SystemAccessType} from "../consts/accessType";
+import { RunSolutionResponse, SubmissionInfo } from "./exercise";
+import { CourseAccessType, CourseRoleType, SystemAccessType } from "../consts/accessType";
 import { AccountProblemType } from "../consts/accountProblemType";
 
 interface RootState {
@@ -22,7 +22,9 @@ interface CourseState {
 }
 
 interface SlidesState {
-	submissionsByCourses: { [courseId: string]: { [slideId: string]: { [submissionId: number]: SubmissionInfo } } }
+	submissionsByCourses: { [courseId: string]: { [slideId: string]: { [submissionId: number]: SubmissionInfo } } },
+	submissionError: string,
+	lastCheckingResponse: RunSolutionResponse,
 	// TODO не все поля
 }
 
