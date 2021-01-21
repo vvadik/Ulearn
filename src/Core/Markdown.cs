88a82a4429546d6baaf8e8f73396e78cee34a8e6
vorkulsky <vorkulsky@skbkontur.ru>
2021-01-21 13:49:25 +0500

    OnPrepareLink download tag

--- src/Core/Markdown.cs
+++ src/Core/Markdown.cs
@@ -106,6 +106,15 @@ namespace Ulearn.Core
 
 		private class ExtendedMarkdownDeep : MarkdownDeep.Markdown
 		{
+			private readonly Regex fileLinkRegex = new Regex(@".*\.(zip|odp|pptx|docx|xlsx|pdf|mmap|xmind)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+			public override void OnPrepareLink(HtmlTag tag)
+			{
+				base.OnPrepareLink(tag);
+				var isFileLink = fileLinkRegex.IsMatch(tag.attributes["href"]);
+				if (isFileLink)
+					tag.attributes["download"] = "";
+			}
+
 			public override void OnPrepareImage(HtmlTag tag, bool TitledImage)
 			{
 				base.OnPrepareImage(tag, TitledImage);
