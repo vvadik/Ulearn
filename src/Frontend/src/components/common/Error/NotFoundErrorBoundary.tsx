a0a8d3a01472a8e288fbf8f68c60c7c01d73de1a
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-01-27 13:54:27 +0500

    Refactored some common components to tsx

---
+++ src/Frontend/src/components/common/Error/NotFoundErrorBoundary.tsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Error404 from "./Error404";
+
+import { RouteComponentProps, withRouter } from "react-router-dom";
+
+export class UrlError extends Error {
+	private response?: string;
+
+	constructor(message?: string, response?: string) {
+		super(message);
+		this.response = response;
+	}
+}
+
+interface State {
+	error: Error | null,
+}
+
+class NotFoundErrorBoundary extends Component<RouteComponentProps, State> {
+	state: State = {
+		error: null,
+	};
+
+	componentDidUpdate(prevProps: RouteComponentProps) {
+		const { error, } = this.state;
+		const { location, } = this.props;
+
+		if(error && (prevProps.location.pathname !== location.pathname)) {
+			this.setState({
+				error: null,
+			});
+		}
+	}
+
+	componentDidCatch(error: Error) {
+		if(error instanceof UrlError) {
+			this.setState({ error });
+		} else {
+			Error();
+		}
+	}
+
+	render() {
+		if(this.state.error) {
+			return (
+				<Error404/>
+			);
+		}
+		return this.props.children;
+	}
+}
+
+export default withRouter(NotFoundErrorBoundary);
---
+++ src/Frontend/src/components/common/ErrorBoundary.tsx
@@ -0,0 +1,46 @@
+import React, { Component, ErrorInfo } from "react";
+
+import { Toast } from "ui";
+import Raven from 'raven-js';
+
+interface State {
+	error: Error | null;
+}
+
+class ErrorBoundary extends Component<unknown, State> {
+	constructor(props: unknown) {
+		super(props);
+		this.state = { error: null };
+	}
+
+	componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+		this.setState({ error });
+		Raven.captureException(error, { extra: errorInfo });
+	}
+
+	render(): React.ReactNode {
+		if(this.state.error) {
+			/* render fallback UI */
+			return (
+				<React.Fragment>
+					<div
+						onClick={ this.onClick }>
+						<p>We're sorry — something's gone wrong.</p>
+						<p>Our team has been notified, but click here fill out a report.</p>
+					</div>
+					{ Toast.push('Произошла ошибка. Попробуйте перезагрузить страницу.') }
+				</React.Fragment>
+			);
+		}
+		/* when there's not an error, render children untouched */
+		return this.props.children;
+	}
+
+	onClick = (): void => {
+		if(Raven.lastEventId()) {
+			Raven.showReportDialog();
+		}
+	};
+}
+
+export default ErrorBoundary;
