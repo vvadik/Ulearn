511f974ab9dc11466cecf452a3c617a823dc1b5c
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-03-04 15:18:03 +0500

    Fixed flashcards in navigation

--- src/Frontend/src/components/course/Navigation/Course/Flashcards/Flashcards.tsx
+++ src/Frontend/src/components/course/Navigation/Course/Flashcards/Flashcards.tsx
@@ -1,29 +1,39 @@
 import React from "react";
+import classnames from 'classnames';
 
 import { Link } from "react-router-dom";
 import { flashcards, constructPathToSlide } from 'src/consts/routes';
 
-import classnames from 'classnames';
+import { FlashcardsStatistics } from "../../types";
 
 import styles from './Flashcards.less';
+import ProgressBarCircle from "../../ProgressBar/ProgressBarCircle";
 
 
 interface Props {
 	courseId: string;
+	statistics: FlashcardsStatistics;
+
 	isActive: boolean;
 
 	toggleNavigation: () => void;
 }
 
 
-function Flashcards({ courseId, isActive, toggleNavigation }: Props): React.ReactElement {
+function Flashcards({ courseId, isActive, toggleNavigation, statistics, }: Props): React.ReactElement {
 	return (
 		<Link to={ constructPathToSlide(courseId, flashcards) }
 			  className={ classnames(styles.root, { [styles.isActive]: isActive }) }
 			  onClick={ toggleNavigation }
 		>
 			<h5 className={ styles.header }>Карточки</h5>
-			<span className={ styles.text }>Вопросы для самопроверки</span>
+			<span className={ styles.text }>
+				Вопросы для самопроверки
+				<ProgressBarCircle
+					active={ isActive }
+					successValue={ 1 - statistics.unratedCount / statistics.count }
+					inProgressValue={ 0 }/>
+			</span>
 		</Link>
 	);
 }
