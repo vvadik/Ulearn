a7af51d807e8b1b85730a7b6c7a0b845f094e859
vorkulsky <vorkulsky@skbkontur.ru>
2021-03-26 16:47:53 +0500

    prepare_db GRANT CREATE ON DATABASE

--- src/Database.Core/create_database/02_prepare_db.sql
+++ src/Database.Core/create_database/02_prepare_db.sql
@@ -6,5 +6,7 @@ CREATE EXTENSION IF NOT EXISTS "pg_trgm"; --gin индекс с триграмм
 CREATE OR REPLACE FUNCTION immutable_concat_ws(text, VARIADIC text[])
 RETURNS text AS 'text_concat_ws' LANGUAGE internal IMMUTABLE PARALLEL SAFE;
 
+GRANT CREATE ON DATABASE ulearn TO ulearn; -- иначе при миграции выполняет CREATE SCHEMA IF NOT EXISTS antiplagiarism и не хватает прав 
+
 CREATE SCHEMA IF NOT EXISTS antiplagiarism;
 ALTER SCHEMA antiplagiarism OWNER TO ulearn;
