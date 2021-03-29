43f1a5e9c4559e199986ff73d99325ba62f61365
vorkulsky <vorkulsky@skbkontur.ru>
2021-03-29 10:35:20 +0500

    Notifications can be disabled (for dev.ulearn)

--- src/Core/Configuration/UlearnConfiguration.cs
+++ src/Core/Configuration/UlearnConfiguration.cs
@@ -132,6 +132,8 @@ namespace Ulearn.Core.Configuration
 
 		[CanBeNull] public VideoAnnotationsClientConfiguration VideoAnnotationsClient { get; set; }
 		
+		[CanBeNull] public NotificationsConfiguration Notifications { get; set; }
+		
 		public bool DisableKonturServices { get; set; }
 	}
 
@@ -145,6 +147,11 @@ namespace Ulearn.Core.Configuration
 		public bool Enabled { get; set; }
 		public string Endpoint { get; set; }
 		public string Token { get; set; }
+	}
+
+	public class NotificationsConfiguration
+	{
+		public bool Enabled { get; set; }
 	}
 
 	public class HerculesSinkConfiguration
