1ca01eb00b10c15a47a71493a5358d39785fdd49
vorkulsky <vorkulsky@skbkontur.ru>
2021-04-05 09:36:47 +0500

    OnDeleteSetNullNotificationAdditionalScore

---
+++ src/Database.Core/Migrations/20210405043608_OnDeleteSetNullNotificationAdditionalScore.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class OnDeleteSetNullNotificationAdditionalScore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AdditionalScores_ScoreId",
+                table: "Notifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AdditionalScores_ScoreId",
+                table: "Notifications",
+                column: "ScoreId",
+                principalTable: "AdditionalScores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AdditionalScores_ScoreId",
+                table: "Notifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AdditionalScores_ScoreId",
+                table: "Notifications",
+                column: "ScoreId",
+                principalTable: "AdditionalScores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
