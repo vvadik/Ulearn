8f4ece0bbe919b832c9b41adab1daf487de5668d
vorkulsky <vorkulsky@skbkontur.ru>
2021-04-29 13:15:14 +0500

    Comment Passed gender

--- src/Frontend/src/components/comments/Comment/Comment.tsx
+++ src/Frontend/src/components/comments/Comment/Comment.tsx
@@ -70,7 +70,7 @@ class Comment extends Component<Props, unknown> {
 	};
 
 	render(): React.ReactNode {
-		const { children, commentEditing, comment, user,} = this.props;
+		const { children, commentEditing, comment, user, } = this.props;
 		const canViewProfiles = (user.systemAccesses.includes(SystemAccessType.viewAllProfiles)) ||
 			user.isSystemAdministrator;
 
@@ -110,7 +110,8 @@ class Comment extends Component<Props, unknown> {
 				canViewProfiles={ canViewProfiles }
 				name={ comment.author.visibleName }>
 				<Passed
-					isPassed={comment.isPassed} />
+					isPassed={ comment.isPassed }
+					gender={ comment.author.gender }/>
 				<Like
 					canLike={ user.isAuthenticated }
 					isLiked={ comment.isLiked }
--- src/Frontend/src/components/comments/Comment/Passed/Passed.tsx
+++ src/Frontend/src/components/comments/Comment/Passed/Passed.tsx
@@ -3,14 +3,20 @@ import { Hint } from "ui";
 import { Ok } from "icons";
 
 import styles from "./Passed.less";
+import { Gender } from "src/models/users";
 
 interface Props {
 	isPassed: boolean,
+	gender?: Gender,
 }
 
-export default function Passed({ isPassed }: Props): React.ReactElement {
+export default function Passed({ isPassed, gender }: Props): React.ReactElement {
 	if(!isPassed) {
 		return <React.Fragment/>;
 	}
-	return <div className={ `${ styles.wrapper }` }><Hint text="Решил задачу"><Ok size={ 15 }/></Hint></div>
+	return <div className={ `${ styles.wrapper }` }>
+		<Hint text={ (gender === Gender.Female ? "Решила" : "Решил") + " задачу" }>
+			<Ok size={ 15 }/>
+		</Hint>
+	</div>;
 }
