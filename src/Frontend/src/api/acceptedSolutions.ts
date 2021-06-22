d422866f661b0c32cb8d66e04c80975e8b7be960
vorkulsky <vorkulsky@skbkontur.ru>
2021-06-22 15:52:11 +0500

    AcceptedSolutions front model and api

---
+++ src/Frontend/src/api/acceptedSolutions.ts
@@ -0,0 +1,30 @@
+﻿import { AcceptedSolutionsResponse, LikedAcceptedSolutionsResponse } from "../models/acceptedSolutions";
+import api from "./index";
+import { buildQuery } from "../utils";
+
+export function getAcceptedSolutions(courseId: string, slideId: string): Promise<AcceptedSolutionsResponse> {
+	return api.get(`accepted-solutions?courseId=${ courseId }&slideId=${ slideId }`);
+}
+
+export function getLikedAcceptedSolutions(courseId: string, slideId: string, offset: number,
+	count: number
+): Promise<LikedAcceptedSolutionsResponse> {
+	const query = buildQuery({ courseId, slideId, offset, count });
+	return api.get(`accepted-solutions/liked` + query);
+}
+
+export function likeAcceptedSolution(solutionId: string): Promise<Response> {
+	return api.put(`accepted-solutions/like?solutionId=${ solutionId }`);
+}
+
+export function dislikeAcceptedSolution(solutionId: string): Promise<Response> {
+	return api.delete(`accepted-solutions/like?solutionId=${ solutionId }`);
+}
+
+export function promoteAcceptedSolution(courseId: string, solutionId: string): Promise<Response> {
+	return api.put(`accepted-solutions/promote?courseId=${ courseId }&solutionId=${ solutionId }`);
+}
+
+export function unpromoteAcceptedSolution(courseId: string, solutionId: string): Promise<Response> {
+	return api.delete(`accepted-solutions/promote?courseId=${ courseId }&solutionId=${ solutionId }`);
+}
---
+++ src/Frontend/src/models/acceptedSolutions.ts
@@ -0,0 +1,19 @@
+﻿import { ShortUserInfo } from "./users";
+
+export interface AcceptedSolutionsResponse {
+	promotedSolutions: AcceptedSolution[],
+	randomLikedSolutions: AcceptedSolution[],
+	newestSolutions: AcceptedSolution[],
+}
+
+export interface LikedAcceptedSolutionsResponse {
+	likedSolutions: AcceptedSolution[],
+}
+
+export interface AcceptedSolution {
+	submissionId: number,
+	code: string,
+	likesCount: number | null,
+	likedByMe: boolean | null,
+	promotedBy?: ShortUserInfo,
+}
