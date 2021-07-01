08643218f5cf8dfd0a946d62706cbbe1a2ad3c2d
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-07-01 14:02:52 +0500

    Fixed code style in interfaces of comments

--- src/Frontend/src/models/comments.ts
+++ src/Frontend/src/models/comments.ts
@@ -1,8 +1,8 @@
 import { ShortUserInfo } from "src/models/users";
 
 export interface SlideComments {
-	topLevelComments: Comment[],
-	pagination: Pagination,
+	topLevelComments: Comment[];
+	pagination: Pagination;
 }
 
 export type SlideCommentsResponse = SlideComments & Response;
@@ -13,49 +13,49 @@ export enum ResponseStatus {
 }
 
 export interface Response {
-	status: ResponseStatus,
+	status: ResponseStatus;
 }
 
 export interface Pagination {
-	offset: number,
-	count: number,
-	totalCount: number,
+	offset: number;
+	count: number;
+	totalCount: number;
 }
 
 export interface ShortGroupInfo {
-	id: number,
-	courseId: string,
-	name: string,
-	isArchived: boolean,
-	apiUrl: string,
+	id: number;
+	courseId: string;
+	name: string;
+	isArchived: boolean;
+	apiUrl: string;
 }
 
 export interface Comment {
-	id: number,
-	author: ShortUserInfo,
-	authorGroups: ShortGroupInfo[] | null,
-	text: string,
-	renderedText: string,
-	publishTime: string,
-	isApproved: boolean,
-	isCorrectAnswer?: boolean,
-	isPinnedToTop?: boolean,
-	isLiked: boolean,
-	likesCount: number,
-	replies: Comment[],
-	parentCommentId?: number,
+	id: number;
+	author: ShortUserInfo;
+	authorGroups: ShortGroupInfo[] | null;
+	text: string;
+	renderedText: string;
+	publishTime: string;
+	isApproved: boolean;
+	isCorrectAnswer?: boolean;
+	isPinnedToTop?: boolean;
+	isLiked: boolean;
+	likesCount: number;
+	replies: Comment[];
+	parentCommentId?: number;
 	isPassed: boolean
 }
 
 export interface CommentPolicy {
-	areCommentsEnabled: boolean,
-	moderationPolicy: string,
-	onlyInstructorsCanReply: boolean,
+	areCommentsEnabled: boolean;
+	moderationPolicy: string;
+	onlyInstructorsCanReply: boolean;
 }
 
 export interface CommentPolicyResponse {
-	areCommentsEnabled: boolean,
-	moderationPolicy: string,
-	onlyInstructorsCanReply: boolean,
-	status: ResponseStatus,
+	areCommentsEnabled: boolean;
+	moderationPolicy: string;
+	onlyInstructorsCanReply: boolean;
+	status: ResponseStatus;
 }
