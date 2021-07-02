d9a945ef8a335b343d3f22c1ea854ffa7b7c327c
vorkulsky <vorkulsky@skbkontur.ru>
2021-07-02 17:29:28 +0500

    ApiCourseManager

---
+++ src/Web.Api/Models/ApiCourseManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Database.Repos;
+using Microsoft.Extensions.DependencyInjection;
+using Ulearn.Core.Courses;
+
+namespace Ulearn.Web.Api.Models
+{
+	public class ApiCourseManager : ICourseUpdater
+	{
+		private readonly IServiceScopeFactory serviceScopeFactory;
+		
+		public ApiCourseManager(IServiceScopeFactory serviceScopeFactory)
+		{
+			this.serviceScopeFactory = serviceScopeFactory;
+		} 
+
+		public async Task UpdateCourses()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public async Task UpdateTempCourses()
+		{
+			using (var scope = serviceScopeFactory.CreateScope())
+			{
+				var tempCoursesRepo = (TempCoursesRepo)scope.ServiceProvider.GetService(typeof(ITempCoursesRepo));
+				var tempCourses = await tempCoursesRepo!.GetTempCoursesAsync();
+			}
+		}
+	}
+}
