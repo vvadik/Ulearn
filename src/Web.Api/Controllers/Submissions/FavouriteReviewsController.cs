d0988705729618b5d3cf5dbc67deecf948458993
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-07-15 19:10:02 +0500

    Added mocked favourite reviews controller

---
+++ src/Web.Api/Controllers/Submissions/FavouriteReviewsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Database;
+using Database.Models;
+using Database.Repos;
+using Database.Repos.Users;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+using Ulearn.Common.Api.Models.Responses;
+using Ulearn.Web.Api.Models.Responses.Submissions;
+
+namespace Ulearn.Web.Api.Controllers.Submissions
+{
+	[Route("/favourite-reviews")]
+	public class FavouriteReviewsController : BaseController
+	{
+		public FavouriteReviewsController(
+			IWebCourseManager courseManager,
+			UlearnDb db,
+			IUsersRepo usersRepo)
+			: base(courseManager, db, usersRepo)
+		{
+		}
+
+		[HttpGet]
+		[Authorize(Policy = "Instructors")]
+		[SwaggerResponse((int)HttpStatusCode.Forbidden, "You don't have access to view submissions")]
+		public async Task<ActionResult<FavouriteReviewsResponse>> GetFavouriteReviews([FromQuery] string courseId, [FromQuery] Guid slideId)
+		{
+			var reviews = new List<FavouriteReview>
+			{
+				new()
+				{
+					Id = 0,
+					Text = "**bold** __italic__ ```code```",
+					IsFavourite = false,
+					RenderedText = "<b>bold</b> <i>italic</i> <code>code</code>",
+					UseCount = 10
+				},
+				new()
+				{
+					Id = 1,
+					Text = "Ой! Наш робот нашёл решения других студентов, подозрительно похожие на ваше. Так может быть, если вы позаимствовали части программы, взяли их из открытых источников либо сами поделились своим кодом.Выполняйте задания самостоятельно.",
+					IsFavourite = false,
+					RenderedText = "Ой! Наш робот нашёл решения других студентов, подозрительно похожие на ваше. Так может быть, если вы позаимствовали части программы, взяли их из открытых источников либо сами поделились своим кодом.Выполняйте задания самостоятельно.",
+					UseCount = 100
+				},
+				new()
+				{
+					Id = 2,
+					Text = "Так делать не стоит из-за сложности в O(N^3). Есть более оптимизированные алгоритмы",
+					IsFavourite = true,
+					RenderedText = "Так делать не стоит из-за сложности в O(N^3). Есть более оптимизированные алгоритмы",
+					UseCount = 5
+				}
+			};
+			
+			await Task.Delay(100); //todo MOCKED
+			
+			return new FavouriteReviewsResponse
+			{
+				FavouriteReviews = reviews
+			};
+		}
+	}
+}
---
+++ src/Web.Api/Models/Responses/Submissions/FavouriteReviewsResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Ulearn.Web.Api.Models.Responses.Submissions
+{
+	[DataContract]
+	public class FavouriteReviewsResponse
+	{
+		public List<FavouriteReview> FavouriteReviews;
+	}
+
+	[DataContract]
+	public class FavouriteReview
+	{
+		[DataMember]
+		public string Text { get; set; }
+
+		[DataMember]
+		public string RenderedText { get; set; }
+
+		[DataMember]
+		public bool IsFavourite { get; set; }
+
+		[DataMember]
+		public int Id { get; set; }
+
+		[DataMember]
+		public int UseCount { get; set; }
+	}
+}
