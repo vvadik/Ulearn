49eaa9cedc767715e97756c8bf5e9156f7cde903
vorkulsky <vorkulsky@skbkontur.ru>
2021-07-22 10:05:06 +0500

    Notifications, XQueueWatcher — UpdateCoursesWorker

--- src/Notifications/EntryPoint.cs
+++ src/Notifications/EntryPoint.cs
@@ -1,6 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Ulearn.Common.Api;
+using Ulearn.Core.Courses.Manager;
+using Vostok.Applications.Scheduled;
 using Vostok.Hosting;
+using Vostok.Hosting.Abstractions.Composite;
 
 namespace Notifications
 {
@@ -9,9 +12,12 @@ namespace Notifications
 		public static async Task Main(string[] args)
 		{
 			var isOneTimeSend = args.Length > 0 && args[0] == "send";
-			var application = new NotificationsApplication(isOneTimeSend);
+			var mainApplication = new NotificationsApplication(isOneTimeSend);
+			var compositeApplication = new CompositeApplication(builder => builder
+				.AddApplication(mainApplication)
+				.AddScheduled(mainApplication.SetupScheduled<UpdateCoursesWorker>));
 			var setupBuilder = new EnvironmentSetupBuilder("notifications", args);
-			var hostSettings = new VostokHostSettings(application, setupBuilder.EnvironmentSetup);
+			var hostSettings = new VostokHostSettings(compositeApplication, setupBuilder.EnvironmentSetup);
 			var host = new VostokHost(hostSettings);
 			await host.WithConsoleCancellation().RunAsync();
 		}
