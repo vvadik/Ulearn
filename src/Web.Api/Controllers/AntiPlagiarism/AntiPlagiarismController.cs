08c15bd1081d5e92428b3cac512c1cb6165aa51b
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-07-23 16:49:15 +0500

    Implemented AntiPlagiarism Controller

---
+++ src/Web.Api/Controllers/AntiPlagiarism/AntiPlagiarismController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using AntiPlagiarism.Api;
+using AntiPlagiarism.Api.Models.Parameters;
+using AntiPlagiarism.Api.Models.Results;
+using Database;
+using Database.Models;
+using Database.Repos;
+using Database.Repos.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Ulearn.Core.Configuration;
+using Ulearn.Core.Courses.Manager;
+using Ulearn.Core.Courses.Slides.Exercises;
+using Ulearn.Web.Api.Models.Responses.AntiPlagiarism;
+
+namespace Ulearn.Web.Api.Controllers.AntiPlagiarism
+{
+	[Route("/antiplagiarism")]
+	public class AntiPlagiarismController : BaseController
+	{
+		private readonly IUserSolutionsRepo userSolutionsRepo;
+		private static readonly IAntiPlagiarismClient antiPlagiarismClient;
+
+		static AntiPlagiarismController()
+		{
+			var antiplagiarismClientConfiguration = ApplicationConfiguration.Read<UlearnConfiguration>().AntiplagiarismClient;
+			antiPlagiarismClient = new AntiPlagiarismClient(antiplagiarismClientConfiguration.Endpoint, antiplagiarismClientConfiguration.Token);
+		}
+
+		public AntiPlagiarismController(
+			ICourseStorage courseStorage,
+			UlearnDb db,
+			IUsersRepo usersRepo,
+			IUserSolutionsRepo userSolutionsRepo
+		)
+			: base(courseStorage, db, usersRepo)
+		{
+			this.userSolutionsRepo = userSolutionsRepo;
+		}
+
+		[Authorize(Policy = "Instructors")]
+		[HttpGet("/{submissionId}")]
+		public async Task<ActionResult<AntiPlagiarismInfoResponse>> Info([FromRoute] string submissionId, [FromQuery] string courseId)
+		{
+			var submission = await userSolutionsRepo.FindSubmissionById(submissionId);
+			if (courseStorage.FindCourse(courseId)?.FindSlideByIdNotSafe(submission.SlideId) is not ExerciseSlide slide)
+				return NotFound();
+
+			if (!slide.Exercise.CheckForPlagiarism)
+				return new AntiPlagiarismInfoResponse
+				{
+					Status = "not_checked",
+				};
+
+			var antiPlagiarismsResult = await GetAuthorPlagiarismsAsync(submission);
+
+			var info = new AntiPlagiarismInfoResponse
+			{
+				Status = "checked",
+				SuspicionLevel = SuspicionLevel.None,
+				SuspiciousAuthorsCount = 0,
+			};
+			var faintSuspicionAuthorsIds = new HashSet<Guid>();
+			var strongSuspicionAuthorsIds = new HashSet<Guid>();
+			foreach (var researchedSubmission in antiPlagiarismsResult.ResearchedSubmissions)
+			{
+				foreach (var plagiarism in researchedSubmission.Plagiarisms)
+				{
+					if (plagiarism.Weight >= antiPlagiarismsResult.SuspicionLevels.StrongSuspicion)
+					{
+						strongSuspicionAuthorsIds.Add(plagiarism.SubmissionInfo.AuthorId);
+						info.SuspicionLevel = SuspicionLevel.Strong;
+					}
+					else if (plagiarism.Weight >= antiPlagiarismsResult.SuspicionLevels.FaintSuspicion && info.SuspicionLevel != SuspicionLevel.Strong)
+					{
+						faintSuspicionAuthorsIds.Add(plagiarism.SubmissionInfo.AuthorId);
+						info.SuspicionLevel = SuspicionLevel.Faint;
+					}
+				}
+			}
+
+			info.SuspiciousAuthorsCount = info.SuspicionLevel == SuspicionLevel.Faint ? faintSuspicionAuthorsIds.Count : strongSuspicionAuthorsIds.Count;
+
+			return info;
+		}
+		
+
+		private static readonly ConcurrentDictionary<Tuple<Guid, Guid>, Tuple<DateTime, GetAuthorPlagiarismsResponse>> plagiarismsCache 
+			= new ConcurrentDictionary<Tuple<Guid, Guid>, Tuple<DateTime, GetAuthorPlagiarismsResponse>>();
+		private static readonly TimeSpan cacheLifeTime = TimeSpan.FromMinutes(10);
+
+		private static async Task<GetAuthorPlagiarismsResponse> GetAuthorPlagiarismsAsync(UserExerciseSubmission submission)
+		{
+			RemoveOldValuesFromCache();
+			var userId = Guid.Parse(submission.UserId);
+			var taskId = submission.SlideId;
+			var cacheKey = Tuple.Create(userId, taskId);
+			if (plagiarismsCache.TryGetValue(cacheKey, out var cachedValue))
+				return cachedValue.Item2;
+
+			var value = await antiPlagiarismClient.GetAuthorPlagiarismsAsync(new GetAuthorPlagiarismsParameters
+			{
+				AuthorId = userId,
+				TaskId = taskId,
+				Language = submission.Language
+			});
+			
+			plagiarismsCache.AddOrUpdate(cacheKey,
+				key => Tuple.Create(DateTime.Now, value),
+				(key, old) => Tuple.Create(DateTime.Now, value));
+			
+			return value;
+		}
+
+		private static void RemoveOldValuesFromCache()
+		{
+			foreach (var key in plagiarismsCache.Keys.ToList())
+			{
+				if (!plagiarismsCache.TryGetValue(key, out var cachedValue))
+					continue;
+				/* Remove cached value if it is too old */
+				if (DateTime.Now.Subtract(cachedValue.Item1) > cacheLifeTime)
+					plagiarismsCache.TryRemove(key, out _);
+			}
+		}
+	}
+}
---
+++ src/Web.Api/Models/Responses/AntiPlagiarism/AntiPlagiarismInfoResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Ulearn.Web.Api.Models.Responses.AntiPlagiarism
+{
+	[JsonConverter(typeof(StringEnumConverter), true)]
+	public enum SuspicionLevel : short
+	{
+		None = 0,
+		Faint = 1,
+		Strong = 2,
+	}
+
+	[DataContract]
+	public class AntiPlagiarismInfoResponse
+	{
+		[DataMember(Name = "status")]
+		public string Status { get; set; }
+
+		[DataMember(Name = "suspicionLevel")]
+		public SuspicionLevel SuspicionLevel { get; set; }
+
+		[DataMember(Name = "suspiciousAuthorsCount")]
+		public int SuspiciousAuthorsCount { get; set; }
+	}
+}
+
