f9c2249bb9a4c67dfc2330160a382981e81d5027
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-07-23 16:45:41 +0500

    implemented SubmissionsController

--- src/Web.Api/Controllers/Submissions/SubmissionsController.cs
+++ src/Web.Api/Controllers/Submissions/SubmissionsController.cs
@@ -5,13 +5,15 @@ using System.Threading.Tasks;
 using Database;
 using Database.Models;
 using Database.Repos;
+using Database.Repos.Groups;
 using Database.Repos.Users;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Swashbuckle.AspNetCore.Annotations;
 using Ulearn.Common.Api.Models.Responses;
+using Ulearn.Common.Extensions;
+using Ulearn.Core.Courses.Manager;
 using Ulearn.Web.Api.Models.Responses.Submissions;
 
 namespace Ulearn.Web.Api.Controllers.Submissions
@@ -22,23 +24,35 @@ namespace Ulearn.Web.Api.Controllers.Submissions
 		private readonly IUserSolutionsRepo userSolutionsRepo;
 		private readonly ISlideCheckingsRepo slideCheckingsRepo;
 		private readonly ICourseRolesRepo courseRolesRepo;
+		private readonly IVisitsRepo visitsRepo;
+		private readonly IUnitsRepo unitsRepo;
+		private readonly IGroupAccessesRepo groupAccessesRepo;
+		private readonly INotificationsRepo notificationsRepo;
 
 		public SubmissionsController(
-			IWebCourseManager courseManager, UlearnDb db,
+			ICourseStorage courseStorage,
+			UlearnDb db,
 			IUsersRepo usersRepo,
 			IUserSolutionsRepo userSolutionsRepo,
 			ICourseRolesRepo courseRolesRepo,
-			ISlideCheckingsRepo slideCheckingsRepo)
-			: base(courseManager, db, usersRepo)
+			ISlideCheckingsRepo slideCheckingsRepo,
+			IGroupAccessesRepo groupAccessesRepo,
+			IVisitsRepo visitsRepo,
+			INotificationsRepo notificationsRepo,
+			IUnitsRepo unitsRepo)
+			: base(courseStorage, db, usersRepo)
 		{
 			this.courseRolesRepo = courseRolesRepo;
 			this.userSolutionsRepo = userSolutionsRepo;
 			this.slideCheckingsRepo = slideCheckingsRepo;
+			this.unitsRepo = unitsRepo;
+			this.groupAccessesRepo = groupAccessesRepo;
+			this.visitsRepo = visitsRepo;
+			this.notificationsRepo = notificationsRepo;
 		}
 
 		[HttpGet]
 		[Authorize]
-		[SwaggerResponse((int)HttpStatusCode.Forbidden, "You don't have access to view submissions")]
 		public async Task<ActionResult<SubmissionsResponse>> GetSubmissions([FromQuery] [CanBeNull] string userId, [FromQuery] string courseId, [FromQuery] Guid slideId)
 		{
 			var isCourseAdmin = await courseRolesRepo.HasUserAccessToCourse(UserId, courseId, CourseRoleType.CourseAdmin);
@@ -59,8 +73,58 @@ namespace Ulearn.Web.Api.Controllers.Submissions
 			var reviewId2Comments = codeReviewComments
 				?.GroupBy(c => c.ReviewId)
 				.ToDictionary(g => g.Key, g => g.AsEnumerable());
-			
+
 			return SubmissionsResponse.Build(submissions, submissionsScores, reviewId2Comments, isCourseAdmin);
+		}
+
+		[HttpPost("/{submissionId}/score")]
+		[Authorize]
+		public async Task<ActionResult> Score([FromRoute] string submissionId, [FromBody] int score)
+		{
+			var submission = await userSolutionsRepo.FindSubmissionById(submissionId);
+			var checking = submission.ManualChecking;
+
+			if (!await groupAccessesRepo.CanInstructorViewStudentAsync(User.GetUserId(), submission.UserId))
+				return StatusCode((int)HttpStatusCode.Forbidden, "You don't have access to view this submission");
+
+			await using (var transaction = await db.Database.BeginTransactionAsync())
+			{
+				var course = courseStorage.GetCourse(submission.CourseId);
+				var slide = course.FindSlideByIdNotSafe(submission.SlideId);
+
+				/* Invalid form: score isn't from range 0..100 */
+				if (score is < 0 or > 100)
+				{
+					return StatusCode((int)HttpStatusCode.BadRequest, $"Неверное количество процентов: {score}");
+				}
+
+				await slideCheckingsRepo.MarkManualExerciseCheckingAsChecked(checking, score);
+				await slideCheckingsRepo.MarkManualExerciseCheckingAsCheckedBeforeThis(checking);
+				await visitsRepo.UpdateScoreForVisit(checking.CourseId, slide, checking.UserId);
+
+				var unit = course.FindUnitBySlideIdNotSafe(checking.SlideId, true);
+				if (unit != null && await unitsRepo.IsUnitVisibleForStudents(course, unit.Id))
+					await NotifyAboutManualExerciseChecking(checking);
+
+				await transaction.CommitAsync();
+			}
+
+			return Ok();
+		}
+		
+		private async Task NotifyAboutManualExerciseChecking(ManualExerciseChecking checking)
+		{
+			var isRecheck = (await notificationsRepo
+				.FindNotifications<PassedManualExerciseCheckingNotification>(n => n.CheckingId == checking.Id))
+				.Any();
+			
+			var notification = new PassedManualExerciseCheckingNotification
+			{
+				Checking = checking,
+				IsRecheck = isRecheck,
+			};
+			
+			await notificationsRepo.AddNotification(checking.CourseId, notification, UserId);
 		}
 	}
 }
